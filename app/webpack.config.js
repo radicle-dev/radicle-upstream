ab43705f768fe1718c55fabce5b9af028bd97e9b
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-08-14 10:34:59 +0200

    Serve static assets via Webpack in development mode

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const outputDir = path.join(__dirname, 'build/');
+const assetsDir = path.join(__dirname, 'assets/');
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -19,7 +20,7 @@ module.exports = {
   ],
   devServer: {
     compress: true,
-    contentBase: outputDir,
+    contentBase: [outputDir, assetsDir],
     port: process.env.PORT || 8000,
     historyApiFallback: true
   }
