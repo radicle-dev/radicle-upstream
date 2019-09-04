713b0e265b32b47d74639bdb00cd5100bf85f5dd
Alexander Simmerl <a.simmerl@gmail.com>
2019-09-04 14:18:52 +0200

    Set up warp for http

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -8,7 +8,13 @@
 extern crate log;
 extern crate pretty_env_logger;
 
+use warp::{self, path, Filter};
+
 fn main() {
     pretty_env_logger::init();
     info!("Proxy started");
+
+    let projects = path!("projects").map(|| "not implemented".to_string());
+
+    warp::serve(projects).run(([127, 0, 0, 1], 3030));
 }
