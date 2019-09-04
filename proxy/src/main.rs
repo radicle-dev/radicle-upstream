540abdb7c46e364471964ab725712fadb5e1f5ab
Alexander Simmerl <a.simmerl@gmail.com>
2019-09-04 14:01:59 +0200

    Set up logging

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,3 +1,14 @@
+//! Proxy to serve a specialised HTTP to the oscoin MVP.
+
+#![deny(clippy::all, clippy::pedantic)]
+#![deny(missing_docs)]
+#![deny(warnings)]
+
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+    info!("Proxy started");
 }
