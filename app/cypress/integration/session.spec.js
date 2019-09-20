3f37b9369686d8d12d09077968fb6226acca6305
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-09-20 13:15:21 +0200

    Make spec more cypress-y

--- app/cypress/integration/session.spec.js
+++ app/cypress/integration/session.spec.js
@@ -3,16 +3,16 @@ context('session', () => {
     cy.visit('/')
   })
 
-  let signIn = (username) => {
+  Cypress.Commands.add('signIn', (username) => {
     cy.get('button').contains('Join the network').click()
 
     cy.get('input[placeholder="Enter your name"]').type(username)
     cy.get('button').contains('Join the network').click()
-  }
+  })
 
   context('happy path', () => {
     it('signs in the user', () => {
-      signIn("Rudolfs")
+      cy.signIn("Rudolfs")
 
       cy.get('header').contains('Rudolfs').should('exist')
       cy.contains('Welcome Rudolfs').should('exist')
@@ -25,7 +25,7 @@ context('session', () => {
     })
 
     it('does not lose the session over a browser reload', () => {
-      signIn("Ange")
+      cy.signIn("Ange")
 
       cy.get('header').contains('Ange').should('exist')
 
@@ -34,13 +34,13 @@ context('session', () => {
     })
 
     it('signs out the user when localStorage is cleared', () => {
-      signIn("Ange")
+      cy.signIn("Merle")
 
-      cy.get('header').contains('Ange').should('exist')
+      cy.get('header').contains('Merle').should('exist')
 
       cy.clearLocalStorage()
       cy.reload()
-      cy.get('header').contains('Ange').should('not.exist')
+      cy.get('header').contains('Merle').should('not.exist')
     })
   })
 })
