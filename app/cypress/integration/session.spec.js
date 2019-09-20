861a6e366c1219d54c09b42240e12b3746d8c665
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-09-20 09:57:13 +0200

    Add more session specs

--- app/cypress/integration/session.spec.js
+++ app/cypress/integration/session.spec.js
@@ -3,13 +3,44 @@ context('session', () => {
     cy.visit('/')
   })
 
-  it('signs in the user', () => {
+  let signIn = (username) => {
     cy.get('button').contains('Join the network').click()
 
-    cy.get('input[placeholder="Enter your name"]').type("Rudolfs")
+    cy.get('input[placeholder="Enter your name"]').type(username)
     cy.get('button').contains('Join the network').click()
+  }
 
-    cy.get('header').contains('Rudolfs').should('exist')
-    cy.contains('Welcome Rudolfs').should('exist')
+  context('happy path', () => {
+    it('signs in the user', () => {
+      signIn("Rudolfs")
+
+      cy.get('header').contains('Rudolfs').should('exist')
+      cy.contains('Welcome Rudolfs').should('exist')
+    })
+
+    it('forces user to sign in before registering a project', () => {
+      cy.get('button').contains('Register project').click()
+
+      cy.get('h2').contains('Join the network').should('exist')
+    })
+
+    it('does not lose the session over a browser reload', () => {
+      signIn("Ange")
+
+      cy.get('header').contains('Ange').should('exist')
+
+      cy.reload()
+      cy.get('header').contains('Ange').should('exist')
+    })
+
+    it('signs out the user when localStorage is cleared', () => {
+      signIn("Ange")
+
+      cy.get('header').contains('Ange').should('exist')
+
+      cy.clearLocalStorage()
+      cy.reload()
+      cy.get('header').contains('Ange').should('not.exist')
+    })
   })
 })
