047c81fe8f390d7168f45b51f66f737d7b05d7de
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-10-09 12:06:32 +0200

    Fixup

--- desktop/src/screens/ProjectScreen.tsx
+++ desktop/src/screens/ProjectScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ProjectScreen: React.SFC<{id: string}> = ({id}) => {
+const ProjectScreen: React.FC<{id: string}> = ({id}) => {
   return(<h1>ProjectScreen: {id}</h1>)
 }
 
