d8de60aea3c5e22be31f22f7bbc20b6848fa0305
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-10-09 12:08:44 +0200

    Add projects query

--- desktop/src/screens/ProjectsScreen.tsx
+++ desktop/src/screens/ProjectsScreen.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const PROJECTS = gql`
+  {
+    projects {
+      address
+      description
+      name
+      imgUrl
+    }
+  }
+`;
 
 const ProjectsScreen = () => {
-  return(<h1>Projects</h1>)
+  const { loading, error, data } = useQuery(PROJECTS);
+
+  console.log(error)
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error:(</p>;
+
+  return data.projects.map(({ address, description, name, imgUrl }: any) => (
+    <div key={address}>
+      <p>
+        {address} {description} {name} {imgUrl}
+      </p>
+    </div>
+  ));
 }
 
 export default ProjectsScreen;
