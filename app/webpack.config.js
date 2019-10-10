2ce6c3417cff46d631d86811acb49c5bd6209f19
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-10-10 14:12:05 +0200

    Move bucklescript compile output to lib/js

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -7,7 +7,7 @@ const assetsDir = path.join(__dirname, 'assets/');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/renderer/Index.bs.js',
+  entry: './lib/js/src/renderer/Index.bs.js',
   mode: isProd ? 'production' : 'development',
   output: {
     path: outputDir,
