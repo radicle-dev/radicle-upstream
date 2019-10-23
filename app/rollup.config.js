51e4dd70bef2777a285b82d85ac4bf5746b19d22
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-10-23 11:46:17 +0200

    Auto-format rollup config

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -1,18 +1,18 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import {terser} from 'rollup-plugin-terser';
+import svelte from "rollup-plugin-svelte";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/main.js',
+  input: "src/main.js",
   output: {
     sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/bundle.js'
+    format: "iife",
+    name: "app",
+    file: "public/bundle.js"
   },
   plugins: [
     svelte({
@@ -21,7 +21,7 @@ export default {
       // we'll extract any component CSS out into
       // a separate file — better for performance
       css: css => {
-        css.write('public/bundle.css');
+        css.write("public/bundle.css");
       }
     }),
 
@@ -32,13 +32,14 @@ export default {
     // https://github.com/rollup/rollup-plugin-commonjs
     resolve({
       browser: true,
-      dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+      dedupe: importee =>
+        importee === "svelte" || importee.startsWith("svelte/")
     }),
     commonjs(),
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
-    !production && livereload('public'),
+    !production && livereload("public"),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
