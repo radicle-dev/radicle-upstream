661befccbe06cc0d72c987c4eaf542d3929ac0a3
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-10-24 16:24:20 +0200

    Add a list of all colors

--- app/src/pages/DesignSystem.svelte
+++ app/src/pages/DesignSystem.svelte
@@ -1,9 +1,39 @@
 <script>
   import { Text, Title, Numeric, Caption } from "../DesignSystem";
   import { link } from "svelte-spa-router";
+
+  let colors = Array.from(document.styleSheets)
+    .filter(
+      sheet =>
+        sheet.href === null || sheet.href.startsWith(window.location.origin)
+    )
+    .reduce(
+      (acc, sheet) =>
+        (acc = [
+          ...acc,
+          ...Array.from(sheet.cssRules).reduce(
+            (def, rule) =>
+              (def =
+                rule.selectorText === ":root"
+                  ? [
+                      ...def,
+                      ...Array.from(rule.style).filter(name =>
+                        name.startsWith("--color")
+                      )
+                    ]
+                  : def),
+            []
+          )
+        ]),
+      []
+    );
 </script>
 
 <a href="/" use:link>back</a>
+
+{#each colors as color}
+  <div style="background-color: var({color})">{color}</div>
+{/each}
 
 <Title.Huge>Open Source Coin</Title.Huge>
 <Title.Big>Open Source Coin</Title.Big>
