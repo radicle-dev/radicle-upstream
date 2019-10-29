8a217eab71d5c14b2babb528e02766b515045a8e
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-10-29 09:49:06 +0100

    Fix cargo cache directory permissions

--- .buildkite/run.sh
+++ .buildkite/run.sh
@@ -7,9 +7,12 @@ mkdir -p /cache/yarn
 mkdir -p /cache/cargo
 mkdir -p /cache/rustup
 
+
 export YARN_CACHE_FOLDER=/cache/yarn
 export CARGO_HOME=/cache/cargo
 export RUSTUP_HOME=/cache/rustup
+
+chmod -R a+w $CARGO_HOME $RUSTUP_HOME
 
 export PATH="$PATH:CARGO_HOME/bin"
 
