716f69996829aa8a20bb72669e518703c91d0d2f
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-11-05 10:22:23 +0100

    Style input focus state

--- app/src/DesignSystem/Input.svelte
+++ app/src/DesignSystem/Input.svelte
@@ -12,6 +12,12 @@
     width: 100%;
     height: 48px;
   }
+
+  input:focus {
+    outline: none;
+    box-shadow: 0 0 0 1px var(--color-purple);
+    border: 1px solid var(--color-purple);
+  }
 </style>
 
 <input {placeholder} {style} />
