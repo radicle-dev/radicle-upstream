3da531393efda30b70b26cc202b79ab42782cb46
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-11-07 16:45:48 +0100

    Clean up Layout component

--- app/src/components/Layout.svelte
+++ app/src/components/Layout.svelte
@@ -1,18 +1,10 @@
 <script>
-  import { location } from "svelte-spa-router";
   import Sidebar from "./Sidebar.svelte";
-  import ProjectSidebar from "./ProjectSidebar.svelte";
 
   let wrapperLeftPadding = $$props.$$slots.nestedSidebar ? "318px" : "68px";
-
-  export let params = {};
 </script>
 
 <style>
-  .wrapper {
-    width: 100vw;
-  }
-
   .content {
     margin-top: 80px;
     margin-left: 8vw;
@@ -24,7 +16,7 @@
   <Sidebar />
   <slot name="nestedSidebar" />
 
-  <div class="wrapper" style="padding-left: {wrapperLeftPadding}">
+  <div style="width: 100vw; padding-left: {wrapperLeftPadding}">
     <div class="content">
       <slot />
     </div>
