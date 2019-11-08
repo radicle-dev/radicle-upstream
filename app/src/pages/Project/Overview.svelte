a435a25469afef7c8f96db58f4c9350652ee24e0
Julien Donck <donckjulien@gmail.com>
2019-11-08 17:55:02 +0100

    styled project header

--- app/src/pages/Project/Overview.svelte
+++ app/src/pages/Project/Overview.svelte
@@ -1,7 +1,7 @@
 <script>
   import Layout from "../../components/Layout.svelte";
   import ProjectSidebar from "../../components/ProjectSidebar.svelte";
-
+  import { Button, Title, Text } from "../../DesignSystem";
   export let params = {};
 
   import { gql } from "apollo-boost";
@@ -28,6 +28,30 @@
   });
 </script>
 
+<style>
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .info {
+    display: grid;
+    grid-template-columns: 64px auto auto;
+    grid-column-gap: 24px;
+    padding-bottom: 16px;
+  }
+  .avatar {
+    width: 64px;
+    height: 64px;
+    border-radius: 2px;
+  }
+
+  hr {
+    margin-top: 32px;
+    border-top: var(--color-gray);
+  }
+</style>
+
 <Layout>
   <div slot="nestedSidebar">
     <ProjectSidebar projectId={params.id} />
@@ -36,8 +60,24 @@
   {#await $project}
     <h1>Loading project...</h1>
   {:then result}
-    <h1>{result.data.project.name}</h1>
-    <p>{result.data.project.description}</p>
+    <div class="header">
+      <div>
+        <div class="info">
+          <img class="avatar" src={result.data.project.imgUrl} alt="" />
+          <div>
+            <Title.Big>{result.data.project.name}</Title.Big>
+            <Text.Regular style="color: var(--color-gray)">
+              {result.data.id}
+            </Text.Regular>
+          </div>
+        </div>
+        <Text.Regular style="color: var(--color-dark-gray)">
+          {result.data.project.description}
+        </Text.Regular>
+      </div>
+      <Button size="small" variant="vanilla">Clone</Button>
+    </div>
+    <hr />
   {:catch error}
     <p>ERROR: {error}</p>
   {/await}
