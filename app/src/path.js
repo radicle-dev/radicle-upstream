867d52f9fd783d66e6d5487757060b768c7ed423
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-12-02 09:56:50 +0100

    Routing improvements (#60)
    * Add missing delimiter between objectType and path
    
    * Keep path related logic in one place

--- app/src/path.js
+++ app/src/path.js
@@ -1,9 +1,10 @@
 import regexparam from "regexparam";
+import { DEFAULT_PROJECT_REVISION } from "./config.js";
+import { BLOB, TREE } from "./types.js";
 
 export const search = _params => "/search";
 export const feed = _params => "/feed";
 export const projects = _params => "/projects";
-
 export const projectOverview = (domain, name) =>
   `/projects/${domain}/${name}/overview`;
 export const projectFeed = (domain, name) => `/projects/${domain}/${name}/feed`;
@@ -13,10 +14,29 @@ export const projectFunds = (domain, name) =>
   `/projects/${domain}/${name}/funds`;
 export const projectSource = (domain, name, revision, objectType, path) => {
   if (revision && path) {
-    return `/projects/${domain}/${name}/source/${revision}/${objectType}${path}`;
+    return `/projects/${domain}/${name}/source/${revision}/${objectType}/${path}`;
   } else {
     return `/projects/${domain}/${name}/source`;
   }
+};
+
+const PROJECT_SOURCE_PATH_MATCH = new RegExp(
+  `/source/(.*)/(${BLOB}|${TREE})/(.*)`
+);
+
+export const extractProjectSourceRevision = location => {
+  const rev = location.match(PROJECT_SOURCE_PATH_MATCH);
+  return rev === null ? DEFAULT_PROJECT_REVISION : rev[1];
+};
+
+export const extractProjectSourceObjectPath = location => {
+  const path = location.match(PROJECT_SOURCE_PATH_MATCH);
+  return path === null ? "/" : path[3];
+};
+
+export const extractProjectSourceObjectType = location => {
+  const type = location.match(PROJECT_SOURCE_PATH_MATCH);
+  return type === null ? TREE : type[2];
 };
 
 export const projectCommits = (domain, name) =>
