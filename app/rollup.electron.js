8571ceabde12be1cffe2fba80c29c5865ba8d672
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-12-17 12:26:08 +0100

    Fixup for directory input
    Rollup requires additional configuration for electron to work. I'm not
    sure this is the best way of doing it, but it at least works now. For
    reference, here are the relevant resources:
    
    - https://github.com/rollup/rollup-plugin-babel#helpers
    - https://babeljs.io/docs/en/babel-plugin-transform-runtime#regenerator
    - https://github.com/parcel-bundler/parcel/issues/871#issuecomment-517091956
    - https://github.com/babel/babel/issues/8829#issuecomment-456524916
    - https://github.com/rollup/rollup/issues/3160
    - https://github.com/rollup/rollup-plugin-babel/issues/209#issuecomment-408419124

--- app/rollup.electron.js
+++ app/rollup.electron.js
@@ -9,8 +9,22 @@ export default {
   },
   plugins: [
     babel({
-      exclude: "node_modules/**"
+      exclude: "node_modules/**",
+      // We need the runtimeHelpers to avoid
+      // "ReferenceError: regeneratorRuntime is not defined"
+      // when using ipcMain.handle
+      //
+      // The following babel plugins (and .babelrc config) are also required
+      // for this to work:
+      // @babel/plugin-transform-runtime
+      // @babel/runtime
+      runtimeHelpers: true
     }),
-    externals()
+
+    // This avoids the following warning:
+    //
+    // (!) Unresolved dependencies
+    // https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
+    externals({ builtins: true, deps: true })
   ]
 };
