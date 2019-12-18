f706e22e561d105152055ae8ca3b28a3939fc412
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-12-18 14:18:58 +0100

    Run GraphiQL to show latest commit visible via API

---
+++ app/cypress/integration/debug.spec.js
@@ -0,0 +1,26 @@
+context("debugging", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8080/");
+  });
+
+  it("shows latest commit", () => {
+    cy.get(".CodeMirror-code")
+      .first()
+      .type("{meta}{a}{del}");
+    cy.get(".CodeMirror-code")
+      .first()
+      .type(
+        `{
+  tree(id:{name: "bla", domain:"bla"}, revision:"master", prefix:""){
+    info{
+      lastCommit{
+        summary
+`,
+        { parseSpecialCharSequences: false }
+      );
+
+    cy.wait(1);
+    cy.get(".execute-button").click();
+    cy.screenshot();
+  });
+});
