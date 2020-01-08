f4ec93961eed17c96c23a6fa09fb20b9f15a2a49
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-01-08 15:12:09 +0100

    Add documentation about why we use electron open dialog

--- app/src/DesignSystem/DirectoryInput.svelte
+++ app/src/DesignSystem/DirectoryInput.svelte
@@ -10,6 +10,13 @@
 
   let files;
 
+  // We have to be able to select empty directories when we create new
+  // projects. Unfortunately we can't use the HTML5 open dialog via
+  // <input type="file"> for this. Although it lets us select directories,
+  // it doesn't fire an event when an empty directory is selected.
+  //
+  // The workaround is to use the electron native open dialog. As a bonus we
+  // can configure it to allow users to create new directories.
   const openFileDialog = async () => {
     path = await window.electron.ipcRenderer.invoke(MAIN_IPC_CHANNEL);
   };
