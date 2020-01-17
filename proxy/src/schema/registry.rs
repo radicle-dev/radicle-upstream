b1aba3928391ee33fcfb5f2e801f7aab931dea79
Alexander Simmerl <a.simmerl@gmail.com>
2020-01-17 09:38:03 +0100

    Bump registry client and silence errors

--- proxy/src/schema/registry.rs
+++ proxy/src/schema/registry.rs
@@ -21,19 +21,21 @@ impl<R> Registry<R>
 where
     R: ClientT,
 {
+    // TODO(xla): Remvoe once integrated in the schema.
+    #[allow(dead_code)]
     /// Wrap a registry client.
     pub fn new(client: R) -> Self {
         Self { client }
     }
 
+    // TODO(xla): Remvoe once integrated in the schema.
+    #[allow(dead_code)]
     /// Register a new project on the chain.
     pub fn register_project(
         &self,
         author: &Pair,
         name: String,
         domain: String,
-        description: String,
-        img_url: String,
     ) -> Result<(), Error> {
         let project_name = String32::from_string(name).map_err(ProjectValidation::NameTooLong)?;
         let project_domain =
@@ -57,8 +59,6 @@ where
                 author,
                 RegisterProjectParams {
                     id: (project_name, project_domain),
-                    description,
-                    img_url,
                     checkpoint_id,
                 },
             )
@@ -77,12 +77,6 @@ fn test_register_project() {
     let client = Client::new_emulator();
     let registry = Registry::new(client);
     let alice = Pair::from_legacy_string("//Alice", None);
-    let result = registry.register_project(
-        &alice,
-        "hello".into(),
-        "world".into(),
-        "a jolly old time".into(),
-        "example.com/icon".into(),
-    );
+    let result = registry.register_project(&alice, "hello".into(), "world".into());
     assert_eq!(result.is_err(), false);
 }
