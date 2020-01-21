3be71c7fec37f9a73da1564bee3362d3b32e882e
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-01-21 13:58:48 +0100

    Use unified run script

--- .buildkite/run.sh
+++ .buildkite/run.sh
@@ -3,23 +3,34 @@ set -Eeou pipefail
 
 TIMEFORMAT='elapsed time: %R (user: %U, system: %S)'
 
-export HOME=/cache
+if [[ "${BUILDKITE_AGENT_META_DATA_PLATFORM}" == "macos" ]]; then
+  echo "--- Setting up macOS environment"
 
-mkdir -p /cache/yarn
-mkdir -p /cache/cargo
-mkdir -p /cache/rustup
+  export HOME=/Users/buildkite
+  export CACHE_FOLDER="$HOME/buildkite-cache"
+else
+  echo "--- Setting up Linux environment"
 
-export YARN_CACHE_FOLDER=/cache/yarn
-export CARGO_HOME=/cache/cargo
-export RUSTUP_HOME=/cache/rustup
+  export CACHE_FOLDER="/cache"
+  export HOME="$CACHE_FOLDER"
+fi
+
+mkdir -p "$CACHE_FOLDER/yarn"
+mkdir -p "$CACHE_FOLDER/cargo"
+mkdir -p "$CACHE_FOLDER/rustup"
+
+export YARN_CACHE_FOLDER="$CACHE_FOLDER/yarn"
+export CARGO_HOME="$CACHE_FOLDER/cargo"
+export RUSTUP_HOME="$CACHE_FOLDER/rustup"
 
 chmod -R a+w $CARGO_HOME $RUSTUP_HOME
 
-export PATH="$PATH:$CARGO_HOME/bin"
+export PATH="$HOME/.cargo/bin:$PATH"
 
 # Incremental builds use timestamps of local code. Since we always
 # check it out fresh we can never use incremental builds.
 export CARGO_BUILD_INCREMENTAL=false
+
 # Most of the caching is done through caching ./target
 export SCCACHE_CACHE_SIZE="1G"
 
