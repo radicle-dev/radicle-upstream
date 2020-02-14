0f571532bf154a664c21a31d062b0c93865591d1
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-02-14 19:24:11 +0100

    Fix notifications

--- app/src/stores/notification.js
+++ app/src/stores/notification.js
@@ -3,5 +3,5 @@ import { writable } from "svelte/store";
 export const notificationStore = writable(null);
 
 export const showNotification = message => {
-  notification.set(message);
+  notificationStore.set(message);
 };
