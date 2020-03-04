1e0ae2d95794f3fc237ce63eb5366cac46db6f87
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-03-04 11:52:04 +0100

    Remove debug logs

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,42 +5,36 @@ const client = new ApolloClient({ uri: "http://localhost:8080/control" });
 
 Cypress.Commands.add("nukeCocoState", () => {
   console.log("Nuking CoCo state");
-  client
-    .mutate({
-      mutation: gql`
-        mutation {
-          nukeCocoState
-        }
-      `
-    })
-    .then(result => console.log(result));
+  client.mutate({
+    mutation: gql`
+      mutation {
+        nukeCocoState
+      }
+    `
+  });
 });
 
 Cypress.Commands.add("nukeRegistryState", () => {
   console.log("Nuking Registry state");
-  client
-    .mutate({
-      mutation: gql`
-        mutation {
-          nukeRegistryState
-        }
-      `
-    })
-    .then(result => console.log(result));
+  client.mutate({
+    mutation: gql`
+      mutation {
+        nukeRegistryState
+      }
+    `
+  });
 });
 
 Cypress.Commands.add("nukeAllState", () => {
   console.log("Nuking CoCo and Registry state");
-  client
-    .mutate({
-      mutation: gql`
-        mutation {
-          nukeCocoState
-          nukeRegistryState
-        }
-      `
-    })
-    .then(result => console.log(result));
+  client.mutate({
+    mutation: gql`
+      mutation {
+        nukeCocoState
+        nukeRegistryState
+      }
+    `
+  });
 });
 
 Cypress.Commands.add(
@@ -51,34 +45,32 @@ Cypress.Commands.add(
     defaultBranch = "master",
     imgUrl = "https://res.cloudinary.com/juliendonck/image/upload/v1549554598/monadic-icon_myhdjk.svg"
   ) => {
-    client
-      .mutate({
-        variables: {
-          name: name,
-          description: description,
-          defaultBranch: defaultBranch,
-          imgUrl: imgUrl
-        },
-        mutation: gql`
-          mutation CreateProjectWithFixture(
-            $name: String!
-            $description: String!
-            $defaultBranch: String!
-            $imgUrl: String!
-          ) {
-            createProjectWithFixture(
-              metadata: {
-                name: $name
-                description: $description
-                defaultBranch: $defaultBranch
-                imgUrl: $imgUrl
-              }
-            ) {
-              id
+    client.mutate({
+      variables: {
+        name: name,
+        description: description,
+        defaultBranch: defaultBranch,
+        imgUrl: imgUrl
+      },
+      mutation: gql`
+        mutation CreateProjectWithFixture(
+          $name: String!
+          $description: String!
+          $defaultBranch: String!
+          $imgUrl: String!
+        ) {
+          createProjectWithFixture(
+            metadata: {
+              name: $name
+              description: $description
+              defaultBranch: $defaultBranch
+              imgUrl: $imgUrl
             }
+          ) {
+            id
           }
-        `
-      })
-      .then(result => console.log(result));
+        }
+      `
+    });
   }
 );
