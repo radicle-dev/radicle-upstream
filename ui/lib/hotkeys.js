e400e19321e7acd5b48281c2d9bc5255e6fed5e8
Merle Breitkreuz <merle@monadic.xyz>
2020-03-06 14:17:40 +0100

    Add temporary hotkey access to register user modal

--- ui/lib/hotkeys.js
+++ ui/lib/hotkeys.js
@@ -11,6 +11,14 @@ export const initializeHotkeys = () => {
     push(path.designSystemGuide());
   });
 
+  // temporary hotkey to open user registration
+  hotkeys("shift+t", () => {
+    if (path.active(path.registerUser(), get(location))) {
+      pop();
+    }
+    push(path.registerUser());
+  });
+
   hotkeys("shift+/", () => {
     if (path.active(path.help(), get(location))) {
       pop();
