640187ac8516df0f85d015f599caad5f4cd0520d
Sarah Scott <sarahoscott@gmail.com>
2020-03-06 10:41:14 +0100

    change user to identity

--- ui/lib/path.js
+++ ui/lib/path.js
@@ -43,7 +43,7 @@ export const registerProject = id => `/projects/${id}/register`;
 export const wallet = _params => "/wallet";
 export const profile = _params => "/profile";
 export const help = _params => "/help";
-export const createUser = _params => "/user/new";
+export const createIdentity = _params => "/identity/new";
 
 export const active = (path, location, loose = false) => {
   return regexparam(path, loose).pattern.test(location);
