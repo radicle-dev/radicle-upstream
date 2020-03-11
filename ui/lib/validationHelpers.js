f9b293530e1864d7d06a207b572a4ec1ee22dac2
Sarah Scott <sarahoscott@gmail.com>
2020-03-11 14:51:39 +0100

    fix avatarUrl regex

--- ui/lib/validationHelpers.js
+++ ui/lib/validationHelpers.js
@@ -6,8 +6,7 @@ export const SINGLE_WORD_MATCH = new RegExp("^[a-z0-9][a-z0-9_-]+$", "i");
 // General name match
 export const NAME_MATCH = new RegExp("^[a-z0-9 ]+$", "i");
 
-// TODO: fix this regex
 export const IMAGE_FILENAME = new RegExp(
-  ".*.(gif|jpe?g|tiff|png|webp|bmp)$",
+  "^(https|http)://\\S+.(gif|jpe?g|tiff|png|webp|bmp)$",
   "i"
 );
