ee362fdf3e644b4a741d8b62436865cd4e6fc7b4
Merle Breitkreuz <merle@monadic.xyz>
2020-03-25 19:40:38 +0100

    chore: use stored identity for handle registration (#252)

--- ui/store/identity.js
+++ ui/store/identity.js
@@ -4,4 +4,5 @@ export const avatarUrlStore = writable(null);
 export const avatarFallbackStore = writable(null);
 export const displayNameStore = writable(null);
 export const handleStore = writable(null);
+export const idStore = writable(null);
 export const shareableEntityIdentifierStore = writable(null);
