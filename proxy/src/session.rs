5dc029bf98a0b75feaece808cf0dbc60e6526767
Alexander Simmerl <a.simmerl@gmail.com>
2020-04-07 08:57:52 +1000

    Add minimal docs

--- proxy/src/session.rs
+++ proxy/src/session.rs
@@ -1,5 +1,10 @@
+//! Management of local session state like the currently used identity, wallet related data and
+//! configuration of all sorts.
+
 use crate::identity;
 
+/// Container for all local state.
 pub struct Session {
+    /// The currently used [`identity::Identity`].
     pub identity: Option<identity::Identity>,
 }
