radicle-dev/radicle-upstream

commit 08b1c15d46018732863a9214476f3bf9f3282349

feat: fetch identity on app boot (#266)
* Remove GraphQL configuration which wasn't working
There was a warning in the console: "ApolloBoost was initialized with
unsupported options: defaultOptions".

https://github.com/apollographql/apollo-client/issues/3900

* Add session endpoint returning none
* Add minimal docs
* Introduce store
* Pass context in tests
* Store identity id in session
* Implement session nuke control
* Add nukeSessionState to cypress commands
* Wire up session
* Upgrade cypress
* Fix tests
* Prevent flickering related to font loading
* Update docs
* Make app boot more seamless
* Remove unneeded await
* Remove project loading state
* Center splash screen contents
* Disallow skipping identity creation
* Fix create identity success screen close button
* Fix modal navigation between identity creation and registration

Co-authored-by: Alexander Simmerl <a.simmerl@gmail.com>

ui/App.svelte
@@ -3,16 +3,19 @@
   import { query } from "svelte-apollo";
   import { InMemoryCache } from "apollo-cache-inmemory";
   import { setClient } from "svelte-apollo";
-  import Router from "svelte-spa-router";
+  import Router, { push } from "svelte-spa-router";
   import { initializeHotkeys } from "./lib/hotkeys.js";
+  import * as path from "./lib/path.js";
   import {
     identityAvatarUrlStore,
     identityAvatarFallbackStore,
     identityHandleStore,
     identityShareableEntityIdentifierStore
   } from "./store/identity.js";
+  import { showNotification } from "./store/notification.js";
 
   import CreateProject from "./Screen/CreateProject.svelte";
+  import Blank from "./Screen/Blank.svelte";
   import DesignSystemGuide from "./Screen/DesignSystemGuide.svelte";
   import Help from "./Screen/Help.svelte";
   import Network from "./Screen/Network.svelte";
@@ -30,69 +33,69 @@
 
   const client = new ApolloClient({
     uri: "http://127.0.0.1:8080/graphql",
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      watchQuery: {
-        fetchPolicy: "no-cache",
-        errorPolicy: "ignore"
-      },
-      query: {
-        fetchPolicy: "no-cache",
-        errorPolicy: "all"
-      },
-      mutate: {
-        errorPolicy: "all"
-      }
-    }
+    cache: new InMemoryCache()
   });
 
   setClient(client);
 
   const GET_IDENTITY = gql`
-    query Query($id: ID!) {
-      identity(id: $id) {
-        id
-        avatarFallback {
-          background {
-            b
-            g
-            r
+    query Query {
+      session {
+        identity {
+          id
+          avatarFallback {
+            background {
+              b
+              g
+              r
+            }
+            emoji
           }
-          emoji
+          metadata {
+            avatarUrl
+            displayName
+            handle
+          }
+          shareableEntityIdentifier
         }
-        metadata {
-          avatarUrl
-          displayName
-          handle
-        }
-        shareableEntityIdentifier
       }
     }
   `;
 
   const getIdentity = async () => {
     try {
-      const response = await query(client, {
-        query: GET_IDENTITY,
-        variables: { id: "123" }
+      const response = query(client, {
+        query: GET_IDENTITY
       });
       const result = await response.result();
-      if (result.data.identity) {
-        identityAvatarUrlStore.set(result.data.identity.metadata.avatarUrl);
-        identityAvatarFallbackStore.set(result.data.identity.avatarFallback);
-        identityHandleStore.set(result.data.identity.metadata.handle);
-        identityShareableEntityIdentifierStore.set(
-          result.data.identity.shareableEntityIdentifier
+      if (result.data.session.identity) {
+        identityAvatarUrlStore.set(
+          result.data.session.identity.metadata.avatarUrl
         );
+        identityAvatarFallbackStore.set(
+          result.data.session.identity.avatarFallback
+        );
+        identityHandleStore.set(result.data.session.identity.metadata.handle);
+        identityShareableEntityIdentifierStore.set(
+          result.data.session.identity.shareableEntityIdentifier
+        );
+        push(path.profile());
+      } else {
+        push(path.createIdentity());
       }
     } catch (error) {
       console.log(error);
+      showNotification({
+        text: "Identity could not be created",
+        level: "error"
+      });
     }
   };
 
   getIdentity();
 
   const routes = {
+    "/": Blank,
     "/identity/new": CreateIdentity,
     "/search": Search,
     "/network": Network,

cypress/integration/identity_creation.spec.js
@@ -1,25 +1,19 @@
 context("identity creation", () => {
   beforeEach(() => {
-    openModal();
+    cy.nukeSessionState();
+    cy.visit("./public/index.html");
   });
 
-  const openModal = () => {
-    cy.visit("./public/index.html#/search");
-    cy.get("body").type("{shift}i");
-  };
-
   context("modal", () => {
-    it("can be closed by pressing escape key", () => {
+    it("can't be closed by pressing escape key", () => {
+      cy.get('[data-cy="get-started-button"]').should("exist");
       cy.get("body").type("{esc}");
-      cy.get("h2")
-        .contains("Search")
-        .should("exist");
+      cy.get('[data-cy="get-started-button"]').should("exist");
     });
   });
 
   context("validations", () => {
     beforeEach(() => {
-      openModal();
       cy.get('[data-cy="get-started-button"]').click();
       cy.get('[data-cy="form"] [data-cy="handle"]').type("_rafalca");
       cy.get('[data-cy="form"] [data-cy="display-name"]').type(

cypress/integration/project_creation.spec.js
@@ -1,9 +1,12 @@
+before(() => {
+  cy.nukeAllState();
+  cy.createIdentity();
+  cy.createProjectWithFixture();
+  cy.visit("./public/index.html");
+});
+
 context("project creation", () => {
   context("project creation modal", () => {
-    beforeEach(() => {
-      cy.visit("./public/index.html#/profile");
-    });
-
     // TODO(rudolfs): test empty project listing has wording and button
 
     it("can be opened via the profile context menu and closed by pressing cancel", () => {
@@ -31,12 +34,20 @@ context("project creation", () => {
 
   context("validations", () => {
     beforeEach(() => {
-      cy.visit("./public/index.html#/projects/new");
+      cy.get('[data-cy="profile-context-menu"]').click();
+      cy.get('[data-cy="dropdown-menu"] [data-cy="new-project"]').click({
+        // TODO(rudolfs): remove this once #246 is fixed
+        force: true
+      });
 
       // Set up minimal form input to show validations
       cy.get('[data-cy="page"] [data-cy="name"]').type("this-name-is-valid");
       cy.get('[data-cy="page"] [data-cy="new-project"]').click();
       cy.get('[data-cy="page"] [data-cy="create-project-button"]').click();
+    });
+
+    afterEach(() => {
+      cy.get("body").type("{esc}", { force: true });
     });
 
     context("name", () => {

cypress/integration/source_browsing.spec.js
@@ -1,6 +1,7 @@
 before(() => {
   cy.nukeAllState();
   cy.createProjectWithFixture();
+  cy.createIdentity();
 });
 
 beforeEach(() => {

cypress/integration/user_registration.spec.js
@@ -1,8 +1,8 @@
 context("user registration", () => {
   context("modal navigation", () => {
     beforeEach(() => {
-      cy.visit("./public/index.html#/profile");
-      cy.get("body").type("{shift}t");
+      cy.createIdentity();
+      cy.visit("./public/index.html");
       cy.get('[data-cy="profile-context-menu"]').click();
       cy.get('[data-cy="dropdown-menu"] [data-cy="register-handle"]').click({
         // TODO(rudolfs): remove this once #246 is fixed
@@ -45,10 +45,13 @@ context("user registration", () => {
 
   context("validations", () => {
     beforeEach(() => {
-      cy.visit("./public/index.html#/user-registration");
-
-      cy.nukeAllState();
-      cy.registerUser();
+      cy.createIdentity();
+      cy.visit("./public/index.html");
+      cy.get('[data-cy="profile-context-menu"]').click();
+      cy.get('[data-cy="dropdown-menu"] [data-cy="register-handle"]').click({
+        // TODO(rudolfs): remove this once #246 is fixed
+        force: true
+      });
     });
 
     context("handle", () => {
@@ -94,7 +97,7 @@ context("user registration", () => {
       });
 
       // TODO(merle): Add test setup, when mocks are replaced
-      it("prevents the user from registering an unavailable handle", () => {
+      it.skip("prevents the user from registering an unavailable handle", () => {
         // shows a validation message when handle is not available
         cy.get('[data-cy="page"] [data-cy="handle"]').clear();
         cy.get('[data-cy="page"] [data-cy="handle"]').type("nope");

cypress/support/commands.js
@@ -1,11 +1,14 @@
 import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
 
-const client = new ApolloClient({ uri: "http://localhost:8080/control" });
+const controlClient = new ApolloClient({
+  uri: "http://localhost:8080/control"
+});
+const apiClient = new ApolloClient({ uri: "http://localhost:8080/graphql" });
 
 Cypress.Commands.add("nukeCocoState", () => {
   console.log("Nuking CoCo state");
-  client.mutate({
+  controlClient.mutate({
     mutation: gql`
       mutation {
         nukeCocoState
@@ -16,7 +19,7 @@ Cypress.Commands.add("nukeCocoState", () => {
 
 Cypress.Commands.add("nukeRegistryState", () => {
   console.log("Nuking Registry state");
-  client.mutate({
+  controlClient.mutate({
     mutation: gql`
       mutation {
         nukeRegistryState
@@ -25,13 +28,25 @@ Cypress.Commands.add("nukeRegistryState", () => {
   });
 });
 
+Cypress.Commands.add("nukeSessionState", () => {
+  console.log("Nuking Session state");
+  controlClient.mutate({
+    mutation: gql`
+      mutation {
+        nukeSessionState
+      }
+    `
+  });
+});
+
 Cypress.Commands.add("nukeAllState", () => {
   console.log("Nuking CoCo and Registry state");
-  client.mutate({
+  controlClient.mutate({
     mutation: gql`
       mutation {
         nukeCocoState
         nukeRegistryState
+        nukeSessionState
       }
     `
   });
@@ -44,7 +59,7 @@ Cypress.Commands.add(
     description = "Monadic is currently supporting radicle.",
     defaultBranch = "master"
   ) => {
-    client.mutate({
+    controlClient.mutate({
       variables: {
         name: name,
         description: description,
@@ -72,7 +87,7 @@ Cypress.Commands.add(
 );
 
 Cypress.Commands.add("registerUser", (handle = "nope", id = "123abcd.git") => {
-  client.mutate({
+  controlClient.mutate({
     variables: {
       handle: handle,
       id: id
@@ -91,3 +106,45 @@ Cypress.Commands.add("registerUser", (handle = "nope", id = "123abcd.git") => {
     `
   });
 });
+
+Cypress.Commands.add(
+  "createIdentity",
+  (
+    handle = "secretariat",
+    displayName = "Christopher Chenery",
+    avatarUrl = null
+  ) => {
+    apiClient.mutate({
+      variables: {
+        handle: handle,
+        displayName: displayName,
+        avatarUrl: avatarUrl
+      },
+      mutation: gql`
+        mutation($handle: String!, $displayName: String, $avatarUrl: String) {
+          createIdentity(
+            handle: $handle
+            displayName: $displayName
+            avatarUrl: $avatarUrl
+          ) {
+            id
+            shareableEntityIdentifier
+            avatarFallback {
+              emoji
+              background {
+                r
+                g
+                b
+              }
+            }
+            metadata {
+              handle
+              displayName
+              avatarUrl
+            }
+          }
+        }
+      `
+    });
+  }
+);

cypress/support/index.js
@@ -1,21 +1,2 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import "./commands";
 import "./assertions";
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')

native/main.js
@@ -46,8 +46,10 @@ const createWindow = () => {
     }
   });
 
-  mainWindow.maximize();
-  mainWindow.show();
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.maximize();
+    mainWindow.show();
+  });
 
   let watcher;
   if (isDev) {
@@ -60,9 +62,7 @@ const createWindow = () => {
     });
   }
 
-  mainWindow.loadURL(
-    `file://${path.join(__dirname, "../public/index.html#/profile")}`
-  );
+  mainWindow.loadURL(`file://${path.join(__dirname, "../public/index.html")}`);
   mainWindow.on("closed", () => {
     if (watcher) {
       watcher.close();

commit 22d9a26b0c8302d99e6e8ad264a825828828bd61

fix(ui): fix routing on browser reload (#297)
This fixes a regression introduced in #266. A browser reload won't land you on
a different screen now.

ui/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import ApolloClient from "apollo-boost";
   import { InMemoryCache } from "apollo-cache-inmemory";
-  import Router, { push } from "svelte-spa-router";
+  import Router, { push, location } from "svelte-spa-router";
   import { setClient } from "svelte-apollo";
 
   import { initializeHotkeys } from "./lib/hotkeys.js";
@@ -43,7 +43,9 @@
       if ($session.data.identity === null) {
         push(path.createIdentity());
       } else {
-        push(path.profile());
+        if ($location === "/") {
+          push(path.profile());
+        }
       }
       break;
 

cypress/integration/routing.spec.js
@@ -1,0 +1,51 @@
+context("routing", () => {
+  context("session persistancy", () => {
+    context("first time app start with no stored session data", () => {
+      it("opens on the identity creation wizard", () => {
+        cy.nukeSessionState();
+        cy.visit("./public/index.html");
+        cy.get('[data-cy="get-started-button"]').should("exist");
+      });
+    });
+
+    context("when there is an identity stored in the session", () => {
+      context(
+        "when there is no additional routing information stored in the browser location",
+        () => {
+          it("opens the app on the profile screen", () => {
+            cy.nukeSessionState();
+            cy.createIdentity();
+            cy.visit("./public/index.html");
+
+            cy.location().should(loc => {
+              expect(loc.hash).to.eq("#/profile");
+            });
+          });
+        }
+      );
+
+      context(
+        "when there is additional routing information stored in the browser location",
+        () => {
+          it("resumes the app from the browser location", () => {
+            cy.nukeSessionState();
+            cy.createIdentity();
+            cy.visit("./public/index.html");
+
+            cy.get('[data-cy="sidebar"] [data-cy="search"]').click();
+
+            cy.location().should(loc => {
+              expect(loc.hash).to.eq("#/search");
+            });
+
+            cy.reload();
+
+            cy.location().should(loc => {
+              expect(loc.hash).to.eq("#/search");
+            });
+          });
+        }
+      );
+    });
+  });
+});