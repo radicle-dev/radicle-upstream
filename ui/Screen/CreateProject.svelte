radicle-dev/radicle-upstream

commit bdc9946a617226779bd6575f9efcaee32e1e4640

refactor: Migrate outstanding endpoints to REST (#312)
Follow-up to #250

* Implement project registration REST call
* Implement register REST call
* Remove outdated network query
* Replace gql user endpoints with REST
* Migrate to user REST endpoints
* Migrate to control REST endpoints
* Please le clippy
* Remove outdated graphql docs
* Fix typo
* Remove last traces
* Update proxy/src/lib.rs
Co-Authored-By: Rūdolfs Ošiņš <rudolfs@osins.org>

ui/Screen/CreateProject.svelte
@@ -1,14 +1,14 @@
 <script>
-  import validatejs from "validate.js";
-  import { gql } from "apollo-boost";
-  import { getClient, mutate } from "svelte-apollo";
   import { pop, push } from "svelte-spa-router";
+  import validatejs from "validate.js";
 
   import { DEFAULT_BRANCH_FOR_NEW_PROJECTS } from "../config.js";
-  import { showNotification } from "../store/notification.js";
   import * as path from "../lib/path.js";
+  import { showNotification } from "../store/notification.js";
+  import { create } from "../src/project.ts";
   import { getLocalBranches } from "../src/source.ts";
 
+  import { ModalLayout, RadioOption } from "../DesignSystem/Component";
   import {
     Button,
     Flex,
@@ -17,7 +17,6 @@
     Text,
     Title
   } from "../DesignSystem/Primitive";
-  import { ModalLayout, RadioOption } from "../DesignSystem/Component";
 
   let currentSelection;
 
@@ -143,23 +142,6 @@
     existingRepositoryPath
   );
 
-  const client = getClient();
-
-  const CREATE_PROJECT = gql`
-    mutation(
-      $metadata: ProjectMetadataInput!
-      $path: String!
-      $publish: Boolean!
-    ) {
-      createProject(metadata: $metadata, path: $path, publish: $publish) {
-        id
-        metadata {
-          name
-        }
-      }
-    }
-  `;
-
   const createProject = async () => {
     beginValidation = true;
     validate();
@@ -171,22 +153,18 @@
     let response;
 
     try {
-      response = await mutate(client, {
-        mutation: CREATE_PROJECT,
-        variables: {
-          metadata: {
-            name: name,
-            description: description,
-            defaultBranch: defaultBranch
-          },
-          path: isNew ? newRepositoryPath : existingRepositoryPath,
-          publish: true
-        }
+      response = await create({
+        metadata: {
+          name,
+          description,
+          defaultBranch
+        },
+        path: isNew ? newRepositoryPath : existingRepositoryPath
       });
 
-      push(path.projectSource(response.data.createProject.id));
+      push(path.projectSource(response.id));
       showNotification({
-        text: `Project ${response.data.createProject.metadata.name} successfully created`,
+        text: `Project ${response.metadata.name} successfully created`,
         level: "info"
       });
     } catch (error) {

ui/src/project.ts
@@ -1,22 +1,28 @@
-import { writable } from "svelte/store";
+import { Readable, writable } from "svelte/store";
 
 import * as api from "./api";
 import * as event from "./event";
 import * as remote from "./remote";
+import * as transaction from "./transaction";
 
-// Types.
+// TYPES.
+export interface Metadata {
+  name: string;
+  default_branch: string;
+  description?: string;
+}
+
 export interface Project {
   id: string;
-  metadata: {
-    name: string;
-    default_branch: string;
-    description?: string;
-  };
+  metadata: Metadata;
 }
 
 type Projects = Project[]
 
-// State.
+// STATE
+const creationStore = remote.createStore<Project>();
+export const creation = creationStore.readable;
+
 const projectStore = remote.createStore<Project>();
 export const project = projectStore.readable;
 
@@ -25,10 +31,17 @@ export const projects = projectsStore.readable;
 
 export const projectNameStore = writable(null);
 
-// State transitions.
+// EVENTS
 enum Kind {
+  Create = "CREATE",
   Fetch = "FETCH",
   FetchList = "FETCH_LIST",
+}
+
+interface Create extends event.Event<Kind> {
+  kind: Kind.Create;
+  metadata: Metadata;
+  path: string;
 }
 
 interface Fetch extends event.Event<Kind> {
@@ -40,10 +53,30 @@ interface FetchList extends event.Event<Kind> {
   kind: Kind.FetchList;
 }
 
-type Msg = Fetch | FetchList;
+type Msg = Create | Fetch | FetchList;
+
+interface CreateInput {
+  metadata: Metadata;
+  path: string;
+}
+
+interface RegisterInput {
+  orgId: string;
+  projectName: string;
+}
 
 const update = (msg: Msg): void => {
   switch (msg.kind) {
+    case Kind.Create:
+      creationStore.loading();
+      api.post<CreateInput, Project>(`projects`, {
+        metadata: msg.metadata,
+        path: msg.path,
+      })
+        .then(creationStore.success)
+        .catch(creationStore.error);
+
+      break;
     case Kind.Fetch:
       projectStore.loading();
       api.get<Project>(`projects/${msg.id}`)
@@ -62,7 +95,32 @@ const update = (msg: Msg): void => {
   }
 }
 
-// Events.
+export const create = (
+  metadata: Metadata,
+  path: string,
+): Readable<remote.Data<Project>> => {
+  const store = remote.createStore<Project>();
+
+  api.post<CreateInput, Project>(`projects`, {
+    metadata,
+    path,
+  })
+    .then(store.success)
+    .catch(store.error);
+
+  return store.readable;
+}
+
+export const register = (
+  orgId: string,
+  projectName: string,
+): Promise<transaction.Transaction> => {
+  return api.post<RegisterInput, transaction.Transaction>(`projects/register`, {
+    orgId,
+    projectName,
+  });
+}
+
 export const fetch = event.create<Kind, Msg>(Kind.Fetch, update);
 const fetchList = event.create<Kind, Msg>(Kind.FetchList, update);
 

cypress/support/commands.js
@@ -1,55 +1,23 @@
-import ApolloClient from "apollo-boost";
-import { gql } from "apollo-boost";
-
-const controlClient = new ApolloClient({
-  uri: "http://localhost:8080/control"
-});
-const apiClient = new ApolloClient({ uri: "http://localhost:8080/graphql" });
-
 Cypress.Commands.add("nukeCocoState", () => {
   console.log("Nuking CoCo state");
-  controlClient.mutate({
-    mutation: gql`
-      mutation {
-        nukeCocoState
-      }
-    `
-  });
+  fetch("http://localhost:8080/v1/control/nuke/coco");
 });
 
 Cypress.Commands.add("nukeRegistryState", () => {
   console.log("Nuking Registry state");
-  controlClient.mutate({
-    mutation: gql`
-      mutation {
-        nukeRegistryState
-      }
-    `
-  });
+  fetch("http://localhost:8080/v1/control/nuke/registry");
 });
 
 Cypress.Commands.add("nukeSessionState", () => {
   console.log("Nuking Session state");
-  controlClient.mutate({
-    mutation: gql`
-      mutation {
-        nukeSessionState
-      }
-    `
-  });
+  fetch("http://localhost:8080/v1/control/nuke/session");
 });
 
 Cypress.Commands.add("nukeAllState", () => {
   console.log("Nuking CoCo and Registry state");
-  controlClient.mutate({
-    mutation: gql`
-      mutation {
-        nukeCocoState
-        nukeRegistryState
-        nukeSessionState
-      }
-    `
-  });
+  fetch("http://localhost:8080/v1/control/nuke/coco");
+  fetch("http://localhost:8080/v1/control/nuke/registry");
+  fetch("http://localhost:8080/v1/control/nuke/session");
 });
 
 Cypress.Commands.add(
@@ -58,54 +26,32 @@ Cypress.Commands.add(
     name = "Monadic",
     description = "Monadic is currently supporting radicle.",
     defaultBranch = "master"
-  ) => {
-    controlClient.mutate({
-      variables: {
-        name: name,
-        description: description,
-        defaultBranch: defaultBranch
+  ) =>
+    fetch("http://localhost:8080/v1/control/create-project", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
       },
-      mutation: gql`
-        mutation CreateProjectWithFixture(
-          $name: String!
-          $description: String!
-          $defaultBranch: String!
-        ) {
-          createProjectWithFixture(
-            metadata: {
-              name: $name
-              description: $description
-              defaultBranch: $defaultBranch
-            }
-          ) {
-            id
-          }
-        }
-      `
-    });
-  }
+      body: JSON.stringify({
+        name,
+        description,
+        defaultBranch
+      })
+    })
 );
 
-Cypress.Commands.add("registerUser", (handle = "nope", id = "123abcd.git") => {
-  controlClient.mutate({
-    variables: {
-      handle: handle,
-      id: id
+Cypress.Commands.add("registerUser", (handle = "nope", id = "123abcd.git") =>
+  fetch("http://localhost:8080/v1/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
     },
-    mutation: gql`
-      mutation RegisterUser($handle: ID!, $id: ID!) {
-        registerUser(handle: $handle, id: $id) {
-          messages {
-            ... on UserRegistrationMessage {
-              handle
-              id
-            }
-          }
-        }
-      }
-    `
-  });
-});
+    body: JSON.stringify({
+      handle,
+      id
+    })
+  })
+);
 
 Cypress.Commands.add(
   "createIdentity",
@@ -113,38 +59,16 @@ Cypress.Commands.add(
     handle = "secretariat",
     displayName = "Christopher Chenery",
     avatarUrl = null
-  ) => {
-    apiClient.mutate({
-      variables: {
-        handle: handle,
-        displayName: displayName,
-        avatarUrl: avatarUrl
+  ) =>
+    fetch("http://localhost:8080/v1/identities", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
       },
-      mutation: gql`
-        mutation($handle: String!, $displayName: String, $avatarUrl: String) {
-          createIdentity(
-            handle: $handle
-            displayName: $displayName
-            avatarUrl: $avatarUrl
-          ) {
-            id
-            shareableEntityIdentifier
-            avatarFallback {
-              emoji
-              background {
-                r
-                g
-                b
-              }
-            }
-            metadata {
-              handle
-              displayName
-              avatarUrl
-            }
-          }
-        }
-      `
-    });
-  }
+      body: JSON.stringify({
+        handle,
+        displayName,
+        avatarUrl
+      })
+    })
 );

graphiql/index.html
@@ -1,78 +1,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      html,
-      body {
-        height: 100%;
-        margin: 0;
-        overflow: hidden;
-        width: 100%;
-      }
-      #graphiql {
-        height: 100vh;
-      }
-    </style>
-
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/graphiql-with-extensions@0.14.3/graphiqlWithExtensions.css"
-      integrity="sha384-GBqwox+q8UtVEyBLBKloN5QDlBDsQnuoSUfMeJH1ZtDiCrrk103D7Bg/WjIvl4ya"
-      crossorigin="anonymous"
-    />
-    <script
-      src="https://cdn.jsdelivr.net/npm/whatwg-fetch@2.0.3/fetch.min.js"
-      integrity="sha384-dcF7KoWRaRpjcNbVPUFgatYgAijf8DqW6NWuqLdfB5Sb4Cdbb8iHX7bHsl9YhpKa"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/react@16.8.6/umd/react.production.min.js"
-      integrity="sha384-qn+ML/QkkJxqn4LLs1zjaKxlTg2Bl/6yU/xBTJAgxkmNGc6kMZyeskAG0a7eJBR1"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/react-dom@16.8.6/umd/react-dom.production.min.js"
-      integrity="sha384-85IMG5rvmoDsmMeWK/qUU4kwnYXVpC+o9hoHMLi4bpNR+gMEiPLrvkZCgsr7WWgV"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/graphiql-with-extensions@0.14.3/graphiqlWithExtensions.min.js"
-      integrity="sha384-TqI6gT2PjmSrnEOTvGHLad1U4Vm5VoyzMmcKK0C/PLCWTnwPyXhCJY6NYhC/tp19"
-      crossorigin="anonymous"
-    ></script>
-  </head>
-  <body>
-    <div id="graphiql"></div>
-    <script>
-      var fetchURL = "http://localhost:8080/graphql";
-      function graphQLFetcher(graphQLParams) {
-        var headers = {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        };
-
-        return fetch(fetchURL, {
-          method: "post",
-          headers: headers,
-          body: JSON.stringify(graphQLParams)
-        })
-          .then(function(response) {
-            return response.text();
-          })
-          .then(function(responseBody) {
-            try {
-              return JSON.parse(responseBody);
-            } catch (error) {
-              return responseBody;
-            }
-          });
-      }
-      ReactDOM.render(
-        React.createElement(GraphiQLWithExtensions.GraphiQLWithExtensions, {
-          fetcher: graphQLFetcher
-        }),
-        document.getElementById("graphiql")
-      );
-    </script>
-  </body>
-</html>

proxy/Cargo.toml
@@ -4,7 +4,7 @@ version = "0.1.0"
 license = "GPL-3.0-only"
 authors = ["radicle team <dev@radicle.xyz>"]
 edition = "2018"
-description = "Proxy to serve a specialised GraphQL API to radicle-upstream."
+description = "Proxy to serve a specialised API to the Upstream UI."
 readme = "https://github.com/radicle-dev/radicle-upstream/blob/master/proxy/README.md"
 categories = [ "crypto", "p2p" ]
 keywords = [ "radicle-upstream", "proxy" ]
@@ -14,8 +14,6 @@ repository = "git@github.com:radicle-dev/radicle-upstream.git"
 directories = "2.0"
 futures = { version = "0.3", features = [ "compat" ] }
 hex = "0.4"
-juniper = "0.14"
-juniper_codegen = "0.14"
 kv = { version = "0.20", features = [ "bincode-value" ] }
 log = "0.4"
 nonempty = "0.2"
@@ -41,5 +39,4 @@ git = "https://github.com/radicle-dev/radicle-registry.git"
 rev = "9a751cbc2941ce0ead6195b118c150b017d63c8a"
 
 [dev-dependencies]
-indexmap = "1.3"
 pretty_assertions = "0.6"

proxy/src/graphql.rs
@@ -1,8 +1,0 @@
-//! Defines the schema served to the application via `GraphQL`.
-
-/// Server infrastructure used to power the API.
-pub mod api;
-/// Mappings to juniper errors.
-pub mod error;
-/// Query and mutation surface of the `GraphQL` API.
-pub mod schema;

proxy/src/graphql/api.rs
@@ -1,79 +1,0 @@
-use std::sync::Arc;
-use tokio::sync::RwLock;
-use warp::filters;
-use warp::http;
-use warp::{Filter, Rejection, Reply};
-
-use crate::registry;
-
-use super::schema;
-
-/// `GraphQL` API routes.
-#[must_use]
-pub fn routes(
-    librad_paths: Arc<RwLock<librad::paths::Paths>>,
-    registry: Arc<RwLock<registry::Registry>>,
-    store: Arc<RwLock<kv::Store>>,
-    enable_control: bool,
-) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    let context = schema::Context::new(librad_paths, registry, store);
-    let state = warp::any().map(move || context.clone());
-    let graphql_filter = make_graphql_filter(schema::create(), state.clone().boxed());
-    let control_filter = make_graphql_filter(schema::create_control(), state.boxed());
-
-    warp::path("control")
-        .map(move || enable_control)
-        .and_then(|enable_control| async move {
-            if enable_control {
-                Ok(())
-            } else {
-                Err(warp::reject::not_found())
-            }
-        })
-        .untuple_one()
-        .and(control_filter)
-        .or(warp::path("graphql").and(graphql_filter))
-        .with(warp::log("proxy::graphql"))
-}
-
-/// Filter for the graphql endpoint.
-fn make_graphql_filter<Context, Mutation, Query>(
-    schema: juniper::RootNode<'static, Query, Mutation>,
-    context_extractor: filters::BoxedFilter<(Context,)>,
-) -> impl Filter<Extract = (http::Response<Vec<u8>>,), Error = Rejection> + Clone
-where
-    Context: Clone + Send + Sync + 'static,
-    Mutation: juniper::GraphQLType<Context = Context, TypeInfo = ()> + Send + Sync + 'static,
-    Query: juniper::GraphQLType<Context = Context, TypeInfo = ()> + Send + Sync + 'static,
-{
-    let schema = Arc::new(schema);
-
-    warp::post()
-        .map(move || Arc::<juniper::RootNode<'static, Query, Mutation>>::clone(&schema))
-        .and(context_extractor)
-        .and(warp::body::json())
-        .and_then(handle_request)
-}
-
-/// Executes the request and crafts the serialised response.
-async fn handle_request<Context, Mutation, Query>(
-    schema: Arc<juniper::RootNode<'static, Query, Mutation>>,
-    context: Context,
-    request: juniper::http::GraphQLRequest,
-) -> Result<http::Response<Vec<u8>>, std::convert::Infallible>
-where
-    Context: Clone + Send + Sync + 'static,
-    Mutation: juniper::GraphQLType<Context = Context, TypeInfo = ()> + Send + Sync + 'static,
-    Query: juniper::GraphQLType<Context = Context, TypeInfo = ()> + Send + Sync + 'static,
-{
-    match serde_json::to_vec(&request.execute(&schema, &context)) {
-        Ok(body) => Ok(http::Response::builder()
-            .header("content-type", "application/json; charset=utf-8")
-            .body(body)
-            .expect("unable to build response")),
-        Err(_) => Ok(http::Response::builder()
-            .status(warp::http::StatusCode::INTERNAL_SERVER_ERROR)
-            .body(Vec::new())
-            .expect("unable to build response")),
-    }
-}

proxy/src/graphql/error.rs
@@ -1,261 +1,0 @@
-use librad::meta::common::url;
-use librad::surf;
-use librad::surf::git::git2;
-
-use crate::error;
-
-impl juniper::IntoFieldError for error::Error {
-    fn into_field_error(self) -> juniper::FieldError {
-        match self {
-            Self::IdentityExists(id) => juniper::FieldError::new(
-                format!("an identity exists already: '{}'", id),
-                graphql_value!({
-                    "type": "IDENTITY_EXISTS",
-                }),
-            ),
-            Self::FS(fs_error) => convert_fs(&fs_error),
-            Self::Git(git_error) => convert_git(&git_error),
-            Self::Git2(git2_error) => convert_git2(&git2_error),
-            Self::Io(io_error) => convert_io(&io_error),
-            Self::IntConversion(int_error) => juniper::FieldError::new(
-                int_error.to_string(),
-                graphql_value!({
-                    "type": "INT_CONVERSION",
-                }),
-            ),
-            Self::InordinateString32() => juniper::FieldError::new(
-                "String too long",
-                graphql_value!({
-                    "type": "STRING_TOO_LONG",
-                }),
-            ),
-            Self::InvalidOrgId(reason) => juniper::FieldError::new(
-                reason,
-                graphql_value!({
-                    "type": "INVALID_ORG_ID",
-                }),
-            ),
-            Self::InvalidProjectName(reason) => juniper::FieldError::new(
-                reason,
-                graphql_value!({
-                    "type": "INVALID_PROJECT_NAME",
-                }),
-            ),
-            Self::InvalidUserId(reason) => juniper::FieldError::new(
-                reason,
-                graphql_value!({
-                    "type": "INVALID_USER_ID",
-                }),
-            ),
-            Self::Librad(librad_error) => convert_librad_git(&librad_error),
-            Self::LibradParse(parse_error) => {
-                convert_librad_parse_error_to_field_error(&parse_error)
-            },
-            Self::LibradProject(project_error) => match project_error {
-                librad::project::Error::Git(librad_error) => convert_librad_git(&librad_error),
-            },
-            Self::Url(url_error) => convert_url_parse_error_to_field_error(&url_error),
-            Self::ProjectValidation(project_error) => match project_error {
-                error::ProjectValidation::NameTooLong => juniper::FieldError::new(
-                    "A Project name can only be at most 32 bytes long",
-                    graphql_value!({ "type": "PROJECT_NAME_TOO_LONG" }),
-                ),
-                error::ProjectValidation::OrgTooLong => juniper::FieldError::new(
-                    "An Org Id can only be at most 32 bytes long",
-                    graphql_value!({ "type": "ORG_TOO_LONG" }),
-                ),
-            },
-            Self::UserValidation(user_error) => match user_error {
-                error::UserValidation::HandleTooLong => juniper::FieldError::new(
-                    "A user handle can only be at most 32 bytes long",
-                    graphql_value!({ "type": "HANDLE_TOO_LONG" }),
-                ),
-                error::UserValidation::IdTooLong => juniper::FieldError::new(
-                    "A user id can only be at most 32 bytes long",
-                    graphql_value!({ "type": "ID_TOO_LONG" }),
-                ),
-            },
-            // TODO(garbados): expand via sub-match
-            Self::Protocol(error) => juniper::FieldError::new(
-                error.to_string(),
-                graphql_value!({ "type": "RADICLE_PROTOCOL" }),
-            ),
-            // TODO(garbados): expand via sub-match
-            Self::Runtime(error) => juniper::FieldError::new(
-                format!("{:?}", error),
-                graphql_value!({ "type": "RADICLE_RUNTIME" }),
-            ),
-            Self::Time(error) => {
-                juniper::FieldError::new(error.to_string(), graphql_value!({ "type": "TIME" }))
-            },
-        }
-    }
-}
-
-/// Helper to convert `radicle_surf` `FileSystem` error to `juniper::FieldError`.
-fn convert_fs(error: &surf::file_system::error::Error) -> juniper::FieldError {
-    let error_str = match &error {
-        surf::file_system::error::Error::Label(label_error) => match label_error {
-            surf::file_system::error::Label::ContainsSlash => "Label contains slashes",
-            surf::file_system::error::Label::Empty => "Label is empty",
-            surf::file_system::error::Label::InvalidUTF8 => "Label is not valid utf8",
-        },
-        surf::file_system::error::Error::Path(path_error) => match path_error {
-            surf::file_system::error::Path::Empty => "Path is empty",
-        },
-    };
-
-    juniper::FieldError::new(
-        error_str,
-        graphql_value!({
-            "type": "FS"
-        }),
-    )
-}
-
-/// Helper to convert `std::io::Error` to `juniper::FieldError`.
-fn convert_io(error: &std::io::Error) -> juniper::FieldError {
-    juniper::FieldError::new(
-        error.to_string(),
-        graphql_value!({
-            "type": "IO_ERROR",
-        }),
-    )
-}
-
-/// Helper to convert a `radicle_surf` Git error to `juniper::FieldError`.
-fn convert_git(error: &surf::git::error::Error) -> juniper::FieldError {
-    match error {
-        surf::git::error::Error::NotBranch(branch) => juniper::FieldError::new(
-            format!("branch '{}' not known", branch.name()),
-            graphql_value!({
-                "type": "GIT_NOT_BRANCH"
-            }),
-        ),
-        surf::git::error::Error::NotTag(tag) => juniper::FieldError::new(
-            format!("tag '{}' not known", tag.name()),
-            graphql_value!({
-                "type": "GIT_NOT_TAG"
-            }),
-        ),
-        surf::git::error::Error::RevParseFailure(rev) => juniper::FieldError::new(
-            format!("revspec '{}' malformed", rev),
-            graphql_value!({
-                "type": "GIT_REV_PARSE"
-            }),
-        ),
-        surf::git::error::Error::Utf8Error(_utf8_error) => juniper::FieldError::new(
-            "String conversion error",
-            graphql_value!({
-                "type": "STRING_CONVERSION",
-            }),
-        ),
-        surf::git::error::Error::FileSystem(fs_error) => convert_fs(fs_error),
-        surf::git::error::Error::LastCommitException => juniper::FieldError::new(
-            "last commit failed",
-            graphql_value!({
-                "type": "GIT_LAST_COMMIT"
-            }),
-        ),
-        surf::git::error::Error::Git(error) => juniper::FieldError::new(
-            format!("Internal Git error: {:?}", error),
-            graphql_value!({
-                "type": "GIT_INTERNAL"
-            }),
-        ),
-    }
-}
-
-/// Helper to convert a `git2::error::Error` to `juniper::FieldError`.
-fn convert_git2(error: &git2::Error) -> juniper::FieldError {
-    juniper::FieldError::new(
-        error.to_string(),
-        graphql_value!({
-            "type": "GIT2_ERROR"
-        }),
-    )
-}
-
-/// Helper to convert `librad::git::Error` to `juniper::FieldError`.
-fn convert_librad_git(error: &librad::git::Error) -> juniper::FieldError {
-    match error {
-        librad::git::Error::MissingPgpAddr => juniper::FieldError::new(
-            "Missing PGP address.",
-            graphql_value!({
-                "type": "LIBRAD_MISSING_PGP_ADDRESS"
-            }),
-        ),
-        librad::git::Error::MissingPgpUserId => juniper::FieldError::new(
-            "Missing PGP user ID.",
-            graphql_value!({
-                "type": "LIBRAD_MISSING_PGP_USER_ID"
-            }),
-        ),
-        librad::git::Error::ProjectExists => juniper::FieldError::new(
-            "Project already exists.",
-            graphql_value!({
-                "type": "LIBRAD_PROJECT_EXISTS"
-            }),
-        ),
-        librad::git::Error::NoSuchProject => juniper::FieldError::new(
-            "No such project exists.",
-            graphql_value!({
-                "type": "LIBRAD_NO_SUCH_PROJECT"
-            }),
-        ),
-        librad::git::Error::Libgit(git2_error) => convert_git2(git2_error),
-        librad::git::Error::Io(io_error) => convert_io(io_error),
-        librad::git::Error::Serde(json_error) => juniper::FieldError::new(
-            json_error.to_string(),
-            graphql_value!({
-                "type": "LIBRAD_JSON_ERROR"
-            }),
-        ),
-        librad::git::Error::Pgp(pgp_error) => juniper::FieldError::new(
-            pgp_error.to_string(),
-            graphql_value!({
-                "type": "LIBRAD_PGP_ERROR"
-            }),
-        ),
-        librad::git::Error::Surf(surf_error) => convert_git(surf_error),
-        librad::git::Error::MissingDefaultBranch(branch, _) => juniper::FieldError::new(
-            format!(
-                "Branch {} specified as default_branch does not exist in the source repo",
-                branch
-            ),
-            graphql_value!({
-                "type": "LIBRAD_MISING_DEFAULT_BRANCH"
-            }),
-        ),
-    }
-}
-
-/// Helper to convert `librad::project::projectid::ParseError` to `juniper::FieldError`.
-fn convert_librad_parse_error_to_field_error(
-    error: &librad::project::projectid::ParseError,
-) -> juniper::FieldError {
-    match error {
-        librad::project::projectid::ParseError::Git(parse_error) => match parse_error {
-            librad::git::projectid::ParseError::InvalidBackend(error) => juniper::FieldError::new(
-                error.to_string(),
-                graphql_value!({
-                    "type": "LIBRAD_PARSE_INVALID_BACKEND"
-                }),
-            ),
-            librad::git::projectid::ParseError::InvalidFormat(error) => juniper::FieldError::new(
-                error.to_string(),
-                graphql_value!({
-                    "type": "LIBRAD_PARSE_INVALID_FORMAT"
-                }),
-            ),
-            librad::git::projectid::ParseError::InvalidOid(_, git2_error) => {
-                convert_git2(git2_error)
-            },
-        },
-    }
-}
-
-/// Helper to convert `url::ParseError` to `juniper::FieldError`.
-fn convert_url_parse_error_to_field_error(error: &url::ParseError) -> juniper::FieldError {
-    juniper::FieldError::new(error.to_string(), graphql_value!({ "type": "URL_PARSE" }))
-}

commit f57632be4e37f9dd97606acf91e17f5158c4f547

Fix regression in project creation introduced in #312

ui/Screen/CreateProject.svelte
@@ -153,14 +153,10 @@
     let response;
 
     try {
-      response = await create({
-        metadata: {
-          name,
-          description,
-          defaultBranch
-        },
-        path: isNew ? newRepositoryPath : existingRepositoryPath
-      });
+      response = await create(
+        { name, description, defaultBranch },
+        isNew ? newRepositoryPath : existingRepositoryPath
+      );
 
       push(path.projectSource(response.id));
       showNotification({

ui/src/project.ts
@@ -8,7 +8,7 @@ import * as transaction from "./transaction";
 // TYPES.
 export interface Metadata {
   name: string;
-  default_branch: string;
+  defaultBranch: string;
   description?: string;
 }
 