radicle-dev/radicle-upstream

commit bdc9946a617226779bd6575f9efcaee32e1e4640

refactor: Migrate outstanding endpoints to REST (#312)
Follow-up to #250

* Implement project registration REST call
* Implement register REST call
* Remove outdated network query
* Replace gql user endpoints with REST
* Migrate to user REST endpoints
* Migrate to control REST endpoints
* Please le clippy
* Remove outdated graphql docs
* Fix typo
* Remove last traces
* Update proxy/src/lib.rs
Co-Authored-By: Rūdolfs Ošiņš <rudolfs@osins.org>

ui/Screen/CreateProject.svelte
@@ -1,14 +1,14 @@
 <script>
-  import validatejs from "validate.js";
-  import { gql } from "apollo-boost";
-  import { getClient, mutate } from "svelte-apollo";
   import { pop, push } from "svelte-spa-router";
+  import validatejs from "validate.js";
 
   import { DEFAULT_BRANCH_FOR_NEW_PROJECTS } from "../config.js";
-  import { showNotification } from "../store/notification.js";
   import * as path from "../lib/path.js";
+  import { showNotification } from "../store/notification.js";
+  import { create } from "../src/project.ts";
   import { getLocalBranches } from "../src/source.ts";
 
+  import { ModalLayout, RadioOption } from "../DesignSystem/Component";
   import {
     Button,
     Flex,
@@ -17,7 +17,6 @@
     Text,
     Title
   } from "../DesignSystem/Primitive";
-  import { ModalLayout, RadioOption } from "../DesignSystem/Component";
 
   let currentSelection;
 
@@ -143,23 +142,6 @@
     existingRepositoryPath
   );
 
-  const client = getClient();
-
-  const CREATE_PROJECT = gql`
-    mutation(
-      $metadata: ProjectMetadataInput!
-      $path: String!
-      $publish: Boolean!
-    ) {
-      createProject(metadata: $metadata, path: $path, publish: $publish) {
-        id
-        metadata {
-          name
-        }
-      }
-    }
-  `;
-
   const createProject = async () => {
     beginValidation = true;
     validate();
@@ -171,22 +153,18 @@
     let response;
 
     try {
-      response = await mutate(client, {
-        mutation: CREATE_PROJECT,
-        variables: {
-          metadata: {
-            name: name,
-            description: description,
-            defaultBranch: defaultBranch
-          },
-          path: isNew ? newRepositoryPath : existingRepositoryPath,
-          publish: true
-        }
+      response = await create({
+        metadata: {
+          name,
+          description,
+          defaultBranch
+        },
+        path: isNew ? newRepositoryPath : existingRepositoryPath
       });
 
-      push(path.projectSource(response.data.createProject.id));
+      push(path.projectSource(response.id));
       showNotification({
-        text: `Project ${response.data.createProject.metadata.name} successfully created`,
+        text: `Project ${response.metadata.name} successfully created`,
         level: "info"
       });
     } catch (error) {

cypress/support/commands.js
@@ -1,55 +1,23 @@
-import ApolloClient from "apollo-boost";
-import { gql } from "apollo-boost";
-
-const controlClient = new ApolloClient({
-  uri: "http://localhost:8080/control"
-});
-const apiClient = new ApolloClient({ uri: "http://localhost:8080/graphql" });
-
 Cypress.Commands.add("nukeCocoState", () => {
   console.log("Nuking CoCo state");
-  controlClient.mutate({
-    mutation: gql`
-      mutation {
-        nukeCocoState
-      }
-    `
-  });
+  fetch("http://localhost:8080/v1/control/nuke/coco");
 });
 
 Cypress.Commands.add("nukeRegistryState", () => {
   console.log("Nuking Registry state");
-  controlClient.mutate({
-    mutation: gql`
-      mutation {
-        nukeRegistryState
-      }
-    `
-  });
+  fetch("http://localhost:8080/v1/control/nuke/registry");
 });
 
 Cypress.Commands.add("nukeSessionState", () => {
   console.log("Nuking Session state");
-  controlClient.mutate({
-    mutation: gql`
-      mutation {
-        nukeSessionState
-      }
-    `
-  });
+  fetch("http://localhost:8080/v1/control/nuke/session");
 });
 
 Cypress.Commands.add("nukeAllState", () => {
   console.log("Nuking CoCo and Registry state");
-  controlClient.mutate({
-    mutation: gql`
-      mutation {
-        nukeCocoState
-        nukeRegistryState
-        nukeSessionState
-      }
-    `
-  });
+  fetch("http://localhost:8080/v1/control/nuke/coco");
+  fetch("http://localhost:8080/v1/control/nuke/registry");
+  fetch("http://localhost:8080/v1/control/nuke/session");
 });
 
 Cypress.Commands.add(
@@ -58,54 +26,32 @@ Cypress.Commands.add(
     name = "Monadic",
     description = "Monadic is currently supporting radicle.",
     defaultBranch = "master"
-  ) => {
-    controlClient.mutate({
-      variables: {
-        name: name,
-        description: description,
-        defaultBranch: defaultBranch
+  ) =>
+    fetch("http://localhost:8080/v1/control/create-project", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
       },
-      mutation: gql`
-        mutation CreateProjectWithFixture(
-          $name: String!
-          $description: String!
-          $defaultBranch: String!
-        ) {
-          createProjectWithFixture(
-            metadata: {
-              name: $name
-              description: $description
-              defaultBranch: $defaultBranch
-            }
-          ) {
-            id
-          }
-        }
-      `
-    });
-  }
+      body: JSON.stringify({
+        name,
+        description,
+        defaultBranch
+      })
+    })
 );
 
-Cypress.Commands.add("registerUser", (handle = "nope", id = "123abcd.git") => {
-  controlClient.mutate({
-    variables: {
-      handle: handle,
-      id: id
+Cypress.Commands.add("registerUser", (handle = "nope", id = "123abcd.git") =>
+  fetch("http://localhost:8080/v1/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
     },
-    mutation: gql`
-      mutation RegisterUser($handle: ID!, $id: ID!) {
-        registerUser(handle: $handle, id: $id) {
-          messages {
-            ... on UserRegistrationMessage {
-              handle
-              id
-            }
-          }
-        }
-      }
-    `
-  });
-});
+    body: JSON.stringify({
+      handle,
+      id
+    })
+  })
+);
 
 Cypress.Commands.add(
   "createIdentity",
@@ -113,38 +59,16 @@ Cypress.Commands.add(
     handle = "secretariat",
     displayName = "Christopher Chenery",
     avatarUrl = null
-  ) => {
-    apiClient.mutate({
-      variables: {
-        handle: handle,
-        displayName: displayName,
-        avatarUrl: avatarUrl
+  ) =>
+    fetch("http://localhost:8080/v1/identities", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
       },
-      mutation: gql`
-        mutation($handle: String!, $displayName: String, $avatarUrl: String) {
-          createIdentity(
-            handle: $handle
-            displayName: $displayName
-            avatarUrl: $avatarUrl
-          ) {
-            id
-            shareableEntityIdentifier
-            avatarFallback {
-              emoji
-              background {
-                r
-                g
-                b
-              }
-            }
-            metadata {
-              handle
-              displayName
-              avatarUrl
-            }
-          }
-        }
-      `
-    });
-  }
+      body: JSON.stringify({
+        handle,
+        displayName,
+        avatarUrl
+      })
+    })
 );

graphiql/index.html
@@ -1,78 +1,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      html,
-      body {
-        height: 100%;
-        margin: 0;
-        overflow: hidden;
-        width: 100%;
-      }
-      #graphiql {
-        height: 100vh;
-      }
-    </style>
-
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/graphiql-with-extensions@0.14.3/graphiqlWithExtensions.css"
-      integrity="sha384-GBqwox+q8UtVEyBLBKloN5QDlBDsQnuoSUfMeJH1ZtDiCrrk103D7Bg/WjIvl4ya"
-      crossorigin="anonymous"
-    />
-    <script
-      src="https://cdn.jsdelivr.net/npm/whatwg-fetch@2.0.3/fetch.min.js"
-      integrity="sha384-dcF7KoWRaRpjcNbVPUFgatYgAijf8DqW6NWuqLdfB5Sb4Cdbb8iHX7bHsl9YhpKa"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/react@16.8.6/umd/react.production.min.js"
-      integrity="sha384-qn+ML/QkkJxqn4LLs1zjaKxlTg2Bl/6yU/xBTJAgxkmNGc6kMZyeskAG0a7eJBR1"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/react-dom@16.8.6/umd/react-dom.production.min.js"
-      integrity="sha384-85IMG5rvmoDsmMeWK/qUU4kwnYXVpC+o9hoHMLi4bpNR+gMEiPLrvkZCgsr7WWgV"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/graphiql-with-extensions@0.14.3/graphiqlWithExtensions.min.js"
-      integrity="sha384-TqI6gT2PjmSrnEOTvGHLad1U4Vm5VoyzMmcKK0C/PLCWTnwPyXhCJY6NYhC/tp19"
-      crossorigin="anonymous"
-    ></script>
-  </head>
-  <body>
-    <div id="graphiql"></div>
-    <script>
-      var fetchURL = "http://localhost:8080/graphql";
-      function graphQLFetcher(graphQLParams) {
-        var headers = {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        };
-
-        return fetch(fetchURL, {
-          method: "post",
-          headers: headers,
-          body: JSON.stringify(graphQLParams)
-        })
-          .then(function(response) {
-            return response.text();
-          })
-          .then(function(responseBody) {
-            try {
-              return JSON.parse(responseBody);
-            } catch (error) {
-              return responseBody;
-            }
-          });
-      }
-      ReactDOM.render(
-        React.createElement(GraphiQLWithExtensions.GraphiQLWithExtensions, {
-          fetcher: graphQLFetcher
-        }),
-        document.getElementById("graphiql")
-      );
-    </script>
-  </body>
-</html>

proxy/Cargo.toml
@@ -4,7 +4,7 @@ version = "0.1.0"
 license = "GPL-3.0-only"
 authors = ["radicle team <dev@radicle.xyz>"]
 edition = "2018"
-description = "Proxy to serve a specialised GraphQL API to radicle-upstream."
+description = "Proxy to serve a specialised API to the Upstream UI."
 readme = "https://github.com/radicle-dev/radicle-upstream/blob/master/proxy/README.md"
 categories = [ "crypto", "p2p" ]
 keywords = [ "radicle-upstream", "proxy" ]
@@ -14,8 +14,6 @@ repository = "git@github.com:radicle-dev/radicle-upstream.git"
 directories = "2.0"
 futures = { version = "0.3", features = [ "compat" ] }
 hex = "0.4"
-juniper = "0.14"
-juniper_codegen = "0.14"
 kv = { version = "0.20", features = [ "bincode-value" ] }
 log = "0.4"
 nonempty = "0.2"
@@ -41,5 +39,4 @@ git = "https://github.com/radicle-dev/radicle-registry.git"
 rev = "9a751cbc2941ce0ead6195b118c150b017d63c8a"
 
 [dev-dependencies]
-indexmap = "1.3"
 pretty_assertions = "0.6"

proxy/src/graphql.rs
@@ -1,8 +1,0 @@
-//! Defines the schema served to the application via `GraphQL`.
-
-/// Server infrastructure used to power the API.
-pub mod api;
-/// Mappings to juniper errors.
-pub mod error;
-/// Query and mutation surface of the `GraphQL` API.
-pub mod schema;

proxy/src/graphql/api.rs
@@ -1,79 +1,0 @@
-use std::sync::Arc;
-use tokio::sync::RwLock;
-use warp::filters;
-use warp::http;
-use warp::{Filter, Rejection, Reply};
-
-use crate::registry;
-
-use super::schema;
-
-/// `GraphQL` API routes.
-#[must_use]
-pub fn routes(
-    librad_paths: Arc<RwLock<librad::paths::Paths>>,
-    registry: Arc<RwLock<registry::Registry>>,
-    store: Arc<RwLock<kv::Store>>,
-    enable_control: bool,
-) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    let context = schema::Context::new(librad_paths, registry, store);
-    let state = warp::any().map(move || context.clone());
-    let graphql_filter = make_graphql_filter(schema::create(), state.clone().boxed());
-    let control_filter = make_graphql_filter(schema::create_control(), state.boxed());
-
-    warp::path("control")
-        .map(move || enable_control)
-        .and_then(|enable_control| async move {
-            if enable_control {
-                Ok(())
-            } else {
-                Err(warp::reject::not_found())
-            }
-        })
-        .untuple_one()
-        .and(control_filter)
-        .or(warp::path("graphql").and(graphql_filter))
-        .with(warp::log("proxy::graphql"))
-}
-
-/// Filter for the graphql endpoint.
-fn make_graphql_filter<Context, Mutation, Query>(
-    schema: juniper::RootNode<'static, Query, Mutation>,
-    context_extractor: filters::BoxedFilter<(Context,)>,
-) -> impl Filter<Extract = (http::Response<Vec<u8>>,), Error = Rejection> + Clone
-where
-    Context: Clone + Send + Sync + 'static,
-    Mutation: juniper::GraphQLType<Context = Context, TypeInfo = ()> + Send + Sync + 'static,
-    Query: juniper::GraphQLType<Context = Context, TypeInfo = ()> + Send + Sync + 'static,
-{
-    let schema = Arc::new(schema);
-
-    warp::post()
-        .map(move || Arc::<juniper::RootNode<'static, Query, Mutation>>::clone(&schema))
-        .and(context_extractor)
-        .and(warp::body::json())
-        .and_then(handle_request)
-}
-
-/// Executes the request and crafts the serialised response.
-async fn handle_request<Context, Mutation, Query>(
-    schema: Arc<juniper::RootNode<'static, Query, Mutation>>,
-    context: Context,
-    request: juniper::http::GraphQLRequest,
-) -> Result<http::Response<Vec<u8>>, std::convert::Infallible>
-where
-    Context: Clone + Send + Sync + 'static,
-    Mutation: juniper::GraphQLType<Context = Context, TypeInfo = ()> + Send + Sync + 'static,
-    Query: juniper::GraphQLType<Context = Context, TypeInfo = ()> + Send + Sync + 'static,
-{
-    match serde_json::to_vec(&request.execute(&schema, &context)) {
-        Ok(body) => Ok(http::Response::builder()
-            .header("content-type", "application/json; charset=utf-8")
-            .body(body)
-            .expect("unable to build response")),
-        Err(_) => Ok(http::Response::builder()
-            .status(warp::http::StatusCode::INTERNAL_SERVER_ERROR)
-            .body(Vec::new())
-            .expect("unable to build response")),
-    }
-}

commit 06d02e63c990a8f308e1b7e573e4b4aa1e9f2e34

chore(ui): design cleanup (#326)
* fix: cleanup button styles
* Fix regression introduced in #312
* Resize icon button to match new button size
* remove small button size
* fix: give button specific class
* fix: update button spacing from 24 to 16
* fix: fix arrow positioning
* fix: variant specific disabled styles
* fix: update transaction statusbar height
* fix: clean up color swatches
* remove user select from buttons
* add .button class to button to avoid bleed into other components
* add .vanilla class to most default styles

Co-authored-by: Rūdolfs Ošiņš <rudolfs@osins.org>

ui/DesignSystem/Component/Transaction/Accordion.svelte
@@ -130,7 +130,7 @@
         on:click={() => {
           push(path.transactions(transaction.id));
         }}>
-        <Flex>
+        <Flex style="height: 100%">
           <div slot="left" style="display: flex;">
             <Icon.TransactionState
               state={stateToIconState[transaction.state]}

ui/DesignSystem/Component/Transaction/Statusbar.svelte
@@ -20,7 +20,7 @@
 
 <style>
   .statusbar {
-    height: 48px;
+    height: 40px;
     width: 100%;
     border-radius: 4px;
     display: flex;
@@ -41,14 +41,14 @@
 
 <div {style} class="statusbar {variant}">
   {#if variant === 'negative'}
-    <Icon.Important style="margin: 12px; fill: var(--color-background)" />
+    <Icon.Important style="margin: 8px; fill: var(--color-background)" />
   {:else if variant === 'positive'}
-    <Icon.CheckCircle style="margin: 12px; fill: var(--color-background)" />
+    <Icon.CheckCircle style="margin: 8px; fill: var(--color-background)" />
   {:else}
     <Icon.TransactionState
       variant="inverted"
       {progress}
-      style="margin: 12px; fill: var(--color-background)"
+      style="margin: 8px; fill: var(--color-background)"
       state={variant} />
   {/if}
   <Text variant="tiny" style="align-self: center;">{text[variant]}</Text>

ui/DesignSystem/Primitive/Button.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <style>
-  button {
+  .button {
     white-space: nowrap;
     background-color: var(--color-foreground-level-3);
     border-radius: 4px;
@@ -29,13 +29,14 @@
     height: 40px;
     outline-style: none;
     padding: 0 12px;
+    user-select: none;
   }
 
   .icon {
     padding: 0 16px 0 15px;
   }
 
-  button :global(svg) {
+  .vanilla :global(svg) {
     fill: var(--color-foreground-level-6);
     margin-right: 8px;
   }
@@ -45,20 +46,20 @@
     fill: var(--color-background);
   }
 
-  .disabled :global(svg) {
+  .vanilla.disabled :global(svg) {
     fill: var(--color-foreground-level-5);
   }
 
-  button:hover {
+  .vanilla:hover {
     background-color: var(--color-foreground-level-2);
     border-color: var(--color-foreground-level-2);
   }
 
-  button:active {
+  .vanilla:active {
     color: var(--color-foreground-level-5);
   }
 
-  button:active :global(svg) {
+  .vanilla:active :global(svg) {
     fill: var(--color-foreground-level-5);
   }
 
@@ -154,27 +155,133 @@
     fill: var(--color-foreground-level-4);
   }
 
-  .disabled {
-    background-color: var(--color-foreground-level-3);
-    border-color: var(--color-foreground-level-3);
-    color: var(--color-foreground-level-5);
+  .vanilla.disabled {
+    background-color: var(--color-foreground-level-1);
+    border-color: var(--color-foreground-level-1);
+    color: var(--color-foreground-level-3);
+    cursor: default;
   }
 
-  .disabled:hover {
-    background-color: var(--color-foreground-level-3);
+  .vanilla.disabled:hover {
+    background-color: var(--color-foreground-level-1);
     box-shadow: 0 0 0 0;
-    border-color: var(--color-foreground-level-3);
-    color: var(--color-foreground-level-5);
+    border-color: var(--color-foreground-level-1);
+    color: var(--color-foreground-level-3);
   }
 
-  .disabled:active {
-    background-color: var(--color-foreground-level-3);
-    border-color: var(--color-foreground-level-3);
-    color: var(--color-foreground-level-5);
+  .vanilla.disabled:active {
+    background-color: var(--color-foreground-level-1);
+    border-color: var(--color-foreground-level-1);
+    color: var(--color-foreground-level-3);
+  }
+
+  .vanilla.disabled :global(svg) {
+    fill: var(--color-foreground-level-3);
+  }
+
+  .primary.disabled {
+    background-color: var(--color-primary-level-1);
+    border-color: var(--color-primary-level-1);
+    color: var(--color-background);
+    cursor: default;
+  }
+
+  .primary.disabled:hover {
+    background-color: var(--color-primary-level-1);
+    box-shadow: 0 0 0 0;
+    border-color: var(--color-primary-level-1);
+    color: var(--color-background);
+  }
+
+  .primary.disabled :global(svg) {
+    fill: var(--color-background);
+  }
+
+  .primary.disabled:active {
+    background-color: var(--color-primary-level-1);
+    border-color: var(--color-primary-level-1);
+    color: var(--color-background);
+  }
+
+  .secondary.disabled {
+    background-color: var(--color-secondary-level-1);
+    border-color: var(--color-secondary-level-1);
+    color: var(--color-background);
+    cursor: default;
+  }
+
+  .secondary.disabled:hover {
+    background-color: var(--color-secondary-level-1);
+    box-shadow: 0 0 0 0;
+    border-color: var(--color-secondary-level-1);
+    color: var(--color-background);
+  }
+
+  .secondary.disabled:active {
+    background-color: var(--color-secondary-level-1);
+    border-color: var(--color-secondary-level-1);
+    color: var(--color-background);
+  }
+
+  .secondary.disabled :global(svg) {
+    fill: var(--color-background);
+  }
+
+  .outline.disabled {
+    background-color: var(--color-background);
+    border-color: var(--color-foreground-level-2);
+    color: var(--color-foreground-level-3);
+    cursor: default;
+  }
+
+  .outline.disabled:hover {
+    background-color: var(--color-background);
+    box-shadow: 0 0 0 0;
+    border-color: var(--color-foreground-level-2);
+    color: var(--color-foreground-level-3);
+  }
+
+  .outline.disabled:active {
+    background-color: var(--color-background);
+    border-color: var(--color-foreground-level-2);
+    color: var(--color-foreground-level-3);
+  }
+
+  .outline.disabled :global(svg) {
+    fill: var(--color-foreground-level-3);
+  }
+
+  .transparent.disabled {
+    background-color: var(--color-background);
+    border-color: var(--color-background);
+    color: var(--color-foreground-level-3);
+    cursor: default;
+  }
+
+  .transparent.disabled:hover {
+    background-color: var(--color-background);
+    box-shadow: 0 0 0 0;
+    border-color: var(--color-background);
+    color: var(--color-foreground-level-3);
+  }
+
+  .transparent.disabled:active {
+    background-color: var(--color-background);
+    border-color: var(--color-background);
+    color: var(--color-foreground-level-3);
+  }
+
+  .transparent.disabled :global(svg) {
+    fill: var(--color-foreground-level-3);
   }
 </style>
 
-<button data-cy={dataCy} class={buttonClass} {disabled} on:click {style}>
+<button
+  data-cy={dataCy}
+  class="button {buttonClass}"
+  {disabled}
+  on:click
+  {style}>
   <svelte:component this={icon} />
   <Title>
     <slot />

ui/Screen/CreateProject.svelte
@@ -224,8 +224,9 @@
   }
 
   .double-button {
-    display: flex;
-    flex-direction: row;
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-column-gap: 16px;
   }
 
   .default-branch-row {
@@ -341,11 +342,7 @@
         </div>
         <div slot="right">
           <div class="double-button">
-            <Button
-              dataCy="cancel-button"
-              variant="transparent"
-              on:click={pop}
-              style="margin-right: 24px;">
+            <Button dataCy="cancel-button" variant="transparent" on:click={pop}>
               Cancel
             </Button>
             <Button

ui/Screen/DesignSystemGuide.svelte
@@ -153,7 +153,11 @@
   <Section title="Colors" subTitle="Primary, secondary and grays">
 
     {#each colors as color}
-      <Text style="background-color: var({color})">{color}</Text>
+      <Text
+        style="background-color: var({color}); margin-bottom: 8px;
+        border-radius: 2px; padding: 4px 8px;">
+        {color}
+      </Text>
     {/each}
   </Section>
 