5858b4689a7ff62b99c9a2499cb835bb48036437
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-05-06 10:23:06 +0200

    Shuffle file around for better readability

--- ui/src/org.ts
+++ ui/src/org.ts
@@ -1,15 +1,12 @@
-import * as api from "./api"
-import { createValidationStore } from "./validation"
-import { Transaction } from './transaction';
-import { EmojiAvatar } from "./avatar"
-import * as remote from "./remote";
+import { createValidationStore } from "./validation";
+import { EmojiAvatar } from "./avatar";
+import { Transaction } from "./transaction";
+
+import * as api from "./api";
 import * as event from "./event";
+import * as remote from "./remote";
 
-export enum RegistrationFlowState {
-  NameSelection,
-  TransactionConfirmation
-}
-
+// TYPES.
 export interface Org {
   name: string;
   avatarFallback: EmojiAvatar;
@@ -20,7 +17,16 @@ interface Fetch extends event.Event<Kind> {
   id: string;
 }
 
+interface RegisterOrgInput {
+  id: string;
+}
+
 type Msg = Fetch;
+
+export enum RegistrationFlowState {
+  NameSelection,
+  TransactionConfirmation
+}
 
 // STATE
 const orgStore = remote.createStore<Org>();
@@ -28,25 +34,21 @@ export const org = orgStore.readable;
 
 // EVENTS
 enum Kind {
-  Fetch = "FETCH",
+  Fetch = "FETCH"
 }
 
 const update = (msg: Msg): void => {
   switch (msg.kind) {
     case Kind.Fetch:
       orgStore.loading();
-      api.get<Org>(`orgs/${msg.id}`)
+      api
+        .get<Org>(`orgs/${msg.id}`)
         .then(orgStore.success)
-        .catch(orgStore.error)
+        .catch(orgStore.error);
 
       break;
   }
-}
-
-// Api
-export const getOrg = (id: string): Promise<Org> => api.get<Org>(`orgs/${id}`)
-export const getNameAvailability = (id: string): Promise<boolean> =>
-  getOrg(id).then(org => !org)
+};
 
 // Name validation
 const VALID_NAME_MATCH = new RegExp("^[a-z0-9][a-z0-9]+$");
@@ -61,15 +63,22 @@ export const nameConstraints = {
   }
 };
 
+export const getOrg = (id: string): Promise<Org> => api.get<Org>(`orgs/${id}`);
+export const getNameAvailability = (id: string): Promise<boolean> =>
+  getOrg(id).then(org => !org);
+
 // Make sure we make a new one every time
-export const validationStore = () => createValidationStore(nameConstraints, {
-  promise: getNameAvailability,
-  validationMessage: "Sorry, this name is already taken"
-})
+export const validationStore = () =>
+  createValidationStore(nameConstraints, {
+    promise: getNameAvailability,
+    validationMessage: "Sorry, this name is already taken"
+  });
 
 // MOCK DATA
-// TODO(sos): Replace with actual avatar fallback request once the endpoint is ready
-export const generateAvatar = (id: string): EmojiAvatar => {
+
+// TODO(sos): Replace with actual avatar fallback request once the endpoint
+// is ready
+export const generateAvatar = (_id: string): EmojiAvatar => {
   return {
     background: {
       r: 0,
@@ -77,17 +86,13 @@ export const generateAvatar = (id: string): EmojiAvatar => {
       b: 222
     },
     emoji: "😘"
-  }
-}
+  };
+};
 
-interface RegisterOrgInput {
-  id: string;
-}
 
+export const fetch = event.create<Kind, Msg>(Kind.Fetch, update);
 export const register = (id: string): Promise<Transaction> => {
   return api.post<RegisterOrgInput, Transaction>(`orgs`, {
     id
   });
-}
-
-export const fetch = event.create<Kind, Msg>(Kind.Fetch, update);
+};
