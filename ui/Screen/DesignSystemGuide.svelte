6230333b2fe5a4dbbadfb824f0822911f2287181
Julien Donck <donckjulien@gmail.com>
2020-05-12 14:31:45 +0200

    add destructive button variant

--- ui/Screen/DesignSystemGuide.svelte
+++ ui/Screen/DesignSystemGuide.svelte
@@ -475,6 +475,22 @@
             </Button>
           </td>
         </tr>
+        <tr>
+          <td>
+            <Button variant="destructive">Destructive</Button>
+          </td>
+          <td>
+            <Button variant="destructive" disabled>Destructive</Button>
+          </td>
+          <td>
+            <Button icon={Icon.Check} variant="destructive">Destructive</Button>
+          </td>
+          <td>
+            <Button icon={Icon.Check} variant="destructive" disabled>
+              Destructive
+            </Button>
+          </td>
+        </tr>
 
       </table>
     </Section>
