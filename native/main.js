1cee493fd77271ad713b4e648199fb26f037d7ec
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-05-26 13:39:18 +0200

    Don't try to start proxy every time macOS window is recreated

--- native/main.js
+++ native/main.js
@@ -114,6 +114,6 @@ app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    startApp();
+    createWindow();
   }
 });
