029d92abbc83f270592b9957c04b95a332d298cc
FintanH <fintan.halpenny@gmail.com>
2020-06-15 10:45:48 +0100

    Separate clippy and rustfmt into a separate step so it can run in parallel

---
+++ .buildkite/clippy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -Eeou pipefail
+
+TIMEFORMAT='elapsed time: %R (user: %U, system: %S)'
+
+echo "--- Run cargo fmt"
+(cd proxy && time cargo fmt --all -- --check)
+
+echo "--- Run proxy lints"
+(cd proxy && time cargo clippy --all --all-features --all-targets)
