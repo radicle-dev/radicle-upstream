71233c8b2d1bde9c826fc29bd91546e43adb1785
Alexander Simmerl <a.simmerl@gmail.com>
2020-06-23 00:38:43 +0200

    Always box

--- proxy/src/http.rs
+++ proxy/src/http.rs
@@ -29,27 +29,13 @@ mod user;
 macro_rules! combine {
     ($x:expr, $($y:expr),+) => {
         {
-            let filter = boxed_on_debug!($x);
+            let filter = $x.boxed();
             $(
-                let filter = boxed_on_debug!(filter.or($y));
+                let filter = filter.or($y).boxed();
             )+
             filter
         }
     }
-}
-
-#[cfg(debug_assertions)]
-macro_rules! boxed_on_debug {
-    ($x:expr) => {
-        $x.boxed()
-    };
-}
-
-#[cfg(not(debug_assertions))]
-macro_rules! boxed_on_debug {
-    ($x:expr) => {
-        $x
-    };
 }
 
 /// Main entry point for HTTP API.
