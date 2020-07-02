8df50a7891835937ccc081627ec738c59def3d94
Alexander Simmerl <a.simmerl@gmail.com>
2020-07-02 16:35:46 +0200

    Remove unused button

--- ui/Screen/Settings.svelte
+++ ui/Screen/Settings.svelte
@@ -143,26 +143,6 @@
 
     <section>
       <header>
-        <Title variant="large">Developer tools</Title>
-      </header>
-      <div class="section-item">
-        <div class="info">
-          <Text variant="medium">
-            Unlink all unregistered projects from my profile
-          </Text>
-          <Text style="color: var(--color-foreground-level-6);">
-            This unlinks your local repositories from Upstream. The local data
-            will remain on your computer.
-          </Text>
-        </div>
-        <div class="action">
-          <Button variant="destructive">Remove</Button>
-        </div>
-      </div>
-    </section>
-
-    <section>
-      <header>
         <Title variant="large">Legal</Title>
       </header>
       <div class="section-item">
