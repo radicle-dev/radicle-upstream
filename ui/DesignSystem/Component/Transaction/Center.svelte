e5e56214184252bc0e0ec3d00f42ee6a85a5662e
Alexander Simmerl <a.simmerl@gmail.com>
2020-07-02 12:38:46 +0200

    Implement tx center animation according to spec

--- ui/DesignSystem/Component/Transaction/Center.svelte
+++ ui/DesignSystem/Component/Transaction/Center.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { slide } from "svelte/transition";
   import { IconState, summaryIconState } from "../../../src/transaction.ts";
 
   import List from "./List.svelte";
@@ -9,74 +8,73 @@
   export let transactions = null;
 
   // Transaction center element. Set by the view.
-  let txList = null;
-  let hidden = true;
+  let element = null;
+  let expand = false;
+  let list = null;
 
-  const toggleList = () => {
-    hidden = !hidden;
-  };
-
-  const handleClick = ev => {
-    // Any click *outside* the elem should hide the elem.
-    if (txList !== ev.target && !txList.contains(ev.target) && !hidden) {
-      hidden = true;
+  const toggleList = ev => {
+    if ((element === ev.target || element.contains(ev.target)) && !expand) {
+      expand = true;
+    } else if (expand) {
+      expand = false;
     }
   };
 
   $: negative = summaryIconState(summary.counts) === IconState.Negative;
+  $: console.log(list && list.offsetHeight);
 </script>
 
 <style>
   .center {
     background: var(--color-background);
-    bottom: 0;
-    right: 0;
     border: 1px solid var(--color-foreground-level-2);
     border-radius: 8px;
     box-shadow: var(--elevation-medium);
     cursor: pointer;
+    height: 56px;
     min-width: 275px;
     overflow: hidden;
-    position: absolute;
+    transition: height 360ms ease;
     user-select: none;
-    z-index: 900;
   }
 
   .center:hover {
     background: var(--color-foreground-level-1);
   }
 
+  .center.expand {
+    height: calc(var(--list-height) - 56px);
+  }
+
   .negative {
     border: 1px solid var(--color-negative);
   }
 
-  .list-wrapper {
+  .list {
     max-height: 80vh;
     overflow-y: auto;
+    transform: translateY(calc(-1 * calc(100% - 56px)));
+    transition: transform 360ms ease;
   }
-  .list-wrapper::-webkit-scrollbar {
+  .list::-webkit-scrollbar {
     display: none; /* Chrome Safari */
   }
 
-  .hidden {
-    display: none;
+  .list.expand {
+    transform: translateY(0px);
   }
 </style>
 
-<svelte:window on:click={handleClick} />
+<svelte:window on:click={toggleList} />
 <div
-  bind:this={txList}
+  bind:this={element}
   class="center"
+  class:expand
   class:negative
-  data-cy="transaction-center">
-  {#if !hidden}
-    <div
-      class="list-wrapper"
-      class:hidden
-      in:slide={{ duration: 360 }}
-      out:slide={{ duration: 240 }}>
-      <List on:select {transactions} />
-    </div>
-  {/if}
-  <Summary on:click={toggleList} {summary} />
+  data-cy="transaction-center"
+  style="--list-height: {list && list.offsetHeight}px">
+  <div bind:this={list} class="list" class:expand>
+    <List on:select {transactions} />
+    <Summary {summary} />
+  </div>
 </div>
