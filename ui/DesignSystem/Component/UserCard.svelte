c119de239f6a4715609e171c5e3f6b7817fd1e0d
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-07-04 11:55:41 +0200

    Remove avatar from commit author (#633)
    Closes: #559

--- ui/DesignSystem/Component/UserCard.svelte
+++ ui/DesignSystem/Component/UserCard.svelte
@@ -10,16 +10,8 @@
     display: flex;
     align-items: center;
   }
-
-  img {
-    width: 24px;
-    height: 24px;
-    border-radius: 12px;
-    margin-right: 8px;
-  }
 </style>
 
 <span class="user" {style}>
-  <img src={user.avatar} alt="user-avatar" />
   <Title>{user.username}</Title>
 </span>
