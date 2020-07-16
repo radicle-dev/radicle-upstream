d78b642b8c0bd154c6d23bd3f84fb57573148452
Julien Donck <donckjulien@gmail.com>
2020-07-16 13:46:10 +0200

    filter transactions from profile to org

--- ui/Screen/Org/Fund.svelte
+++ ui/Screen/Org/Fund.svelte
@@ -1,8 +1,18 @@
 <script>
   import { transactions as store } from "../../src/transaction.ts";
   import { Wallet, Remote } from "../../DesignSystem/Component";
+
+  export let params = null;
+
+  const orgTransactions = transactions => {
+    return transactions.filter((tx) => {
+      return "domainId" in tx.messages[0]
+        ? tx.messages[0].domainId == params.id
+        : null;
+    });
+  };
 </script>
 
 <Remote {store} let:data={transactions}>
-  <Wallet {transactions} />
+  <Wallet transactions={orgTransactions(transactions)} />
 </Remote>
--- ui/Screen/Profile/Wallet.svelte
+++ ui/Screen/Profile/Wallet.svelte
@@ -1,8 +1,14 @@
 <script>
   import { transactions as store } from "../../src/transaction.ts";
   import { Wallet, Remote } from "../../DesignSystem/Component";
+
+  const userTransactions = transactions => {
+    return transactions.filter((tx) => {
+      return tx.messages[0].domainType !== "org";
+    });
+  };
 </script>
 
 <Remote {store} let:data={transactions}>
-  <Wallet {transactions} />
+  <Wallet transactions={userTransactions(transactions)} />
 </Remote>
