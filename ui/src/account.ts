6b195da159c6765f918115f801e67c29386f1893
Nuno Alexandre <hi@nunoalexandre.com>
2020-07-17 08:40:58 +0200

    make ui account.ts queries public

--- ui/src/account.ts
+++ ui/src/account.ts
@@ -3,9 +3,9 @@ import * as api from "./api";
 /* Offers commodities to query useful information regarding accounts on chain. */
 
 // Get the balance of the account with the given id
-const balance = (id: string): Promise<number> =>
+export const balance = (id: string): Promise<number> =>
   api.get<number>(`accounts/${id}/balance`);
 
 // Check whether an account with the given id exists on chain
-const exists = (id: string): Promise<boolean> =>
+export const exists = (id: string): Promise<boolean> =>
   api.get<boolean>(`accounts/${id}/exists`);
