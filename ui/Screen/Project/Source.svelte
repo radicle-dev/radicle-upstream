d08927c71396b10da4b95f3083692ad82c3bd8b7
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-07-22 11:00:08 +0200

    Fix race condition in commit navigation

--- ui/Screen/Project/Source.svelte
+++ ui/Screen/Project/Source.svelte
@@ -5,6 +5,7 @@
 
   import * as path from "../../src/path.ts";
   import { project as projectStore } from "../../src/project.ts";
+  import * as remote from "../../src/remote.ts";
   import {
     fetchCommits,
     fetchRevisions,
@@ -71,13 +72,21 @@
     );
   };
 
+  let unsubscribe;
+
   const navigateOnReady = (path, store) => {
-    const unsubscribe = store.subscribe(state => {
-      if (state.status === "SUCCESS") {
+    if (unsubscribe) {
+      unsubscribe();
+    }
+
+    fetchCommits({ projectId: id, revision: currentRevision });
+
+    unsubscribe = store.subscribe(state => {
+      if (state.status === remote.Status.Success) {
         push(path);
+        unsubscribe();
       }
     });
-    unsubscribe();
   };
 
   $: fetchObject({
@@ -87,8 +96,6 @@
     revision: currentRevision,
     type: currentObjectType,
   });
-
-  $: fetchCommits({ projectId: id, revision: currentRevision });
 
   fetchRevisions({ projectId: id });
 </script>
