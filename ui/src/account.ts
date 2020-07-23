fc06cc46a075573be8e4ee96edd765ed0cb54f3c
Merle Breitkreuz <merle@monadic.xyz>
2020-07-23 17:43:09 +0200

    feat(ui): show actual free balance on wallet screens (#715)
    * Show balance on profile wallet
    * Show balance on org wallet

--- ui/src/account.ts
+++ ui/src/account.ts
@@ -1,11 +1,21 @@
 import * as api from "./api";
+import * as remote from "./remote";
 
 /* Offers commodities to query useful information regarding accounts on chain. */
 
 // Get the balance of the account with the given id
-export const balance = (id: string): Promise<number> =>
+export const getBalance = (id: string): Promise<number> =>
   api.get<number>(`accounts/${id}/balance`);
 
 // Check whether an account with the given id exists on chain
 export const exists = (id: string): Promise<boolean> =>
   api.get<boolean>(`accounts/${id}/exists`);
+
+// State
+const balanceStore = remote.createStore<number>();
+export const balance = balanceStore.readable;
+
+export const updateBalance = (id: string) => {
+  balanceStore.loading();
+  getBalance(id).then(balanceStore.success).catch(balanceStore.error);
+};
