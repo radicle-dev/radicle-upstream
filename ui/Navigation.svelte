fc4b82cb6280caadb7288a89f58cad6c2be1b698
Alexander Simmerl <a.simmerl@gmail.com>
2020-07-24 18:43:32 +0200

    Wiggle these types

--- ui/Navigation.svelte
+++ ui/Navigation.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-  import { create } from "./src/navigation";
+  /* import { create } from "./src/navigation"; */
 
-  const nav = create({});
-  const view = nav.current;
+  /* const nav = create({}); */
+  /* const view = nav.current; */
 </script>
 
+<!--
 {#if $view.props}
   <svelte:component this={$view.component} {...$view.props} />
 {:else}
   <svelte:component this={$view.component} />
 {/if}
+-->
--- ui/src/navigation.ts
+++ ui/src/navigation.ts
@@ -1,4 +1,4 @@
-import { SvelteComponentDev } from "svelte";
+import { SvelteComponent } from "svelte";
 import { Readable } from "svelte/store";
 
 import * as history from "./history";
@@ -6,8 +6,7 @@ import * as history from "./history";
 import Blank from "../Screen/Blank.svelte";
 import Help from "../Screen/Help.svelte";
 
-console.log(typeof Blank)
-console.log(Blank)
+process.stdout.write(Blank.toString());
 
 export enum Screen {
   Blank = "Blank",
@@ -15,33 +14,38 @@ export enum Screen {
 }
 
 export interface View {
-  readonly component: SvelteComponent;
+  readonly component: unknown;
   readonly props?: Props;
 }
 
 type Props = Record<string, 0 | string>;
-type ComponentMap<Key extends string> = Record<Key, SvelteComponent>;
+type ComponentMap<Key extends string, C extends SvelteComponent> = Required<
+  Record<Key, C>
+>;
 
-const screenMap: ComponentMap<Screen> = {
+const screenMap: ComponentMap<Screen, SvelteComponent> = {
   [Screen.Blank]: Blank,
   [Screen.Help]: Help,
-}
+};
 
-interface Navigation {
+interface Navigation<Key extends string> {
   readonly current: Readable<View>;
   back(): void;
-  set(key: Screen, props?: Props): void;
+  set(key: Key, props?: Props): void;
 }
 
-export const create = <Key extends string>(componentMap: ComponentMap<Key>): Navigation => {
-  const hist = history.create<View>({ component: componentMap[0] });
+export const create = <K extends string, C extends SvelteComponent>(
+  componentMap: ComponentMap<K, C>,
+  initial: K
+): Navigation<K> => {
+  const hist = history.create<View>({ component: componentMap[initial] });
 
   return {
     current: hist.current,
     back: (): void => {
       hist.pop();
     },
-    set: (key: Screen, props?: Props): void => {
+    set: (key: K, props?: Props): void => {
       hist.push({
         component: componentMap[key],
         props,
@@ -49,3 +53,5 @@ export const create = <Key extends string>(componentMap: ComponentMap<Key>): Nav
     },
   };
 };
+
+console.log(screenMap);
