6d85990b297f2c20ab02841f220f479ebbd97000
sarah olimpia <sarahscott@users.noreply.github.com>
2020-07-30 15:33:15 -0700

    feat(proxy): Feed API (#666)
    * Return a stubbed list of projects

--- proxy/src/coco.rs
+++ proxy/src/coco.rs
@@ -1,7 +1,7 @@
 //! Abstractions and utilities for git interactions through the API.
-
-pub use librad::uri::rad_urn::ParseError;
-pub use librad::uri::RadUrn as Urn;
+pub use librad::hash::Hash;
+pub use librad::meta::project::Project;
+pub use librad::uri::{self, RadUrn as Urn};
 
 pub use radicle_surf::diff::{Diff, FileDiff};
 pub use radicle_surf::vcs::git::Stats;
