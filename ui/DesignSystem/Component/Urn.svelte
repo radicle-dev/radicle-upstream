4d8e28fd8f6b0eadf10a014b537eb294983922d8
Julien Donck <donckjulien@gmail.com>
2020-07-31 14:35:57 +0200

    add conditional show expanded + remove rad:git:

--- ui/DesignSystem/Component/Urn.svelte
+++ ui/DesignSystem/Component/Urn.svelte
@@ -3,21 +3,64 @@
   import { Icon, Text } from "../Primitive";
 
   export let urn = null;
-  const firstSix = urn.substring(0, 7);
-  const lastSix = urn.substring(urn.length - 7, urn.length);
+  export let showOnHover = false;
+
+  const cleanUrn = urn.includes("rad:git:") ? urn.substring(8, urn.length) : urn;
+  console.log(cleanUrn);
+
+  const firstSix = cleanUrn.substring(0, 7);
+  const lastSix = cleanUrn.substring(cleanUrn.length - 7, cleanUrn.length);
+
+  const fullUrn = { className: "hidden" };
+
+  const hideFullUrn = () => {
+    fullUrn.className = "hidden";
+  };
+  const showFullUrn = () => {
+    fullUrn.className = "visible";
+  };
 </script>
 
 <style>
   .wrapper {
     display: flex;
     justify-content: center;
+    position: relative;
+  }
+  .urn {
     background-color: var(--color-foreground-level-1);
     padding: 0 4px;
     border-radius: 4px;
   }
+  .fullUrn {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .fullUrn.visible {
+    visibility: visible;
+  }
+  .fullUrn.hidden {
+    visibility: hidden;
+  }
 </style>
 
-<div class="wrapper" data-cy="urn">
+<div
+  class="wrapper urn"
+  data-cy="urn"
+  on:mouseover={showFullUrn}
+  on:mouseout={hideFullUrn}>
+  {#if showOnHover}
+    <div class={`fullUrn urn ${fullUrn.className}`}>
+      <Copyable iconSize="small" style="align-items: center;" copyContent={urn}>
+        <Text
+          style="font-family: var(--typeface-mono-medium); font-size: 14px;
+          color: var(--color-foreground-level-5); padding-right: 0.25rem;">
+          {cleanUrn}
+        </Text>
+      </Copyable>
+    </div>
+  {/if}
   <Copyable iconSize="small" style="align-items: center;" copyContent={urn}>
     <Text
       style="font-family: var(--typeface-mono-medium); font-size: 14px; color:
