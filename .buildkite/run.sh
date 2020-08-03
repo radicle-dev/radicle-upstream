radicle-dev/radicle-upstream

commit 6e83b0f76198e8593ce9d620ae41bc4a9f5acdd7

refactor(ci): run checks and lints first (#721)

.buildkite/run.sh
@@ -36,17 +36,14 @@ echo "--- Set custom git config"
 cp .buildkite/.gitconfig "$HOME/"
 cat "$HOME/.gitconfig"
 
-echo "--- Run proxy tests"
-(cd proxy && time cargo test --all --all-features --all-targets)
+echo "--- Run proxy docs"
+(cd proxy && time cargo doc --no-deps)
 
-echo "--- Run cargo fmt"
+echo "--- Run proxy fmt"
 (cd proxy && time cargo fmt --all -- --check)
 
 echo "--- Run proxy lints"
 (cd proxy && time cargo clippy --all --all-features --all-targets)
-
-echo "--- Run proxy docs"
-(cd proxy && time cargo doc --no-deps)
 
 echo "--- Run app eslint checks"
 time yarn lint
@@ -56,6 +53,9 @@ time yarn prettier:check
 
 echo "--- Run app svelte checks"
 time yarn svelte:check
+
+echo "--- Run proxy tests"
+(cd proxy && time cargo test --all --all-features --all-targets)
 
 echo "--- Starting proxy daemon and runing app tests"
 time ELECTRON_ENABLE_LOGGING=1 yarn test

commit 3941ab1ddff257d645c761544756c5942a589bcd

fix(ci): make clippy work on CI again (#748)
* Make clippy work on CI again

This was first fixed in:
https://github.com/radicle-dev/radicle-upstream/pull/430.

But then later broken in:
https://github.com/radicle-dev/radicle-upstream/pull/721

* Appease clippy

.buildkite/run.sh
@@ -43,7 +43,7 @@ echo "--- Run proxy fmt"
 (cd proxy && time cargo fmt --all -- --check)
 
 echo "--- Run proxy lints"
-(cd proxy && time cargo clippy --all --all-features --all-targets)
+(cd proxy && time cargo clippy --all --all-features --all-targets -Z unstable-options)
 
 echo "--- Run app eslint checks"
 time yarn lint

proxy/src/registry.rs
@@ -337,6 +337,7 @@ pub struct Registry {
     client: protocol::Client,
 }
 
+/// A fake credit balance which we use in integration tests.
 const PREPAID_AMOUNT_MICRO_RAD: Balance = 321 * 1_000_000;
 
 /// Registry client wrapper methods