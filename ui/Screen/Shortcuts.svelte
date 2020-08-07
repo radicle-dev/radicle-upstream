1485bba8d343f00e4cb87e3a644db22948ce72d2
Julien Donck <julien@monadic.xyz>
2020-08-07 16:31:47 +0200

    feat(ui): add keyboard shortcuts (#759)
    * quick button fix that was annoying
    * add new shortcuts + clean up help page
    * address feedback
    * update variable
    * rename help -> shortcuts + add link in settings to shortcuts page
    * extract OS checking function to settings.ts
    * Switch shortcuts modifier to shift+/ for shortcuts page
    
    Co-authored-by: Alexis Sellier <self@cloudhead.io>
    Co-authored-by: Rūdolfs Ošiņš <rudolfs@osins.org>

---
+++ ui/Screen/Shortcuts.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { isMac } from "../src/settings.ts";
+
+  import ModalLayout from "../DesignSystem/Component/ModalLayout.svelte";
+
+  const modifierKeys = isMac ? "⌘" : "ctrl";
+</script>
+
+<style>
+  .shortcut {
+    margin-bottom: 16px;
+  }
+
+  .shortcut:first-child {
+    margin-top: 6rem;
+  }
+
+  kbd {
+    border: 1px solid var(--color-foreground-level-3);
+    padding: 4px 8px 4px 8px;
+    border-radius: 4px;
+  }
+</style>
+
+<ModalLayout>
+  <div class="shortcut">
+    <kbd>shift</kbd>
+    +
+    <kbd>/</kbd>
+    - shortcuts
+  </div>
+  <div class="shortcut">
+    <kbd>{modifierKeys}</kbd>
+    +
+    <kbd>,</kbd>
+    - settings
+  </div>
+  <div class="shortcut">
+    <kbd>{modifierKeys}</kbd>
+    +
+    <kbd>d</kbd>
+    - design system
+  </div>
+  <div class="shortcut">
+    <kbd>{modifierKeys}</kbd>
+    +
+    <kbd>p</kbd>
+    - search
+  </div>
+  <div class="shortcut">
+    <kbd>{modifierKeys}</kbd>
+    +
+    <kbd>n</kbd>
+    - create new project
+  </div>
+  <div class="shortcut">
+    <kbd>esc</kbd>
+    - close modal
+  </div>
+</ModalLayout>
