3d55f3a3ba6b78877916dafa9dff29f4f2616e76
Merle Breitkreuz <merle@monadic.xyz>
2020-08-28 10:49:57 +0200

    Move new project button

--- ui/DesignSystem/Component/Header/Large.svelte
+++ ui/DesignSystem/Component/Header/Large.svelte
@@ -34,10 +34,15 @@
 
   .banner-content {
     display: flex;
+    justify-content: space-between;
     max-width: var(--content-max-width);
     width: 100%;
     margin: 0 auto;
     padding: 40px var(--content-padding);
+  }
+
+  .left {
+    display: flex;
   }
 
   .metadata {
@@ -68,35 +73,47 @@
     align-items: center;
     background-color: var(--color-background);
   }
+
+  .banner-action {
+    display: flex;
+    align-items: center;
+  }
 </style>
 
 <div data-cy="header" class="header" {style}>
   <div class="banner">
     <div class="banner-content">
-      <Avatar
-        style="margin-right: 32px"
-        size="huge"
-        variant={variant === 'profile' ? 'circle' : 'square'}
-        avatarFallback={entity.avatarFallback} />
+      <div class="left">
+        <Avatar
+          style="margin-right: 32px"
+          size="huge"
+          variant={variant === 'profile' ? 'circle' : 'square'}
+          avatarFallback={entity.avatarFallback} />
 
-      <div class="metadata">
-        <div class="user">
-          <h1 data-cy="entity-name" style="display: flex; align-items: center;">
-            {name}
-          </h1>
-          {#if variant === 'org' || entity.registered}
-            <Icon.Registered
-              dataCy="verified-badge"
-              style="fill: var(--color-primary); margin-left: 6px;" />
-          {/if}
+        <div class="metadata">
+          <div class="user">
+            <h1
+              data-cy="entity-name"
+              style="display: flex; align-items: center;">
+              {name}
+            </h1>
+            {#if variant === 'org' || entity.registered}
+              <Icon.Registered
+                dataCy="verified-badge"
+                style="fill: var(--color-primary); margin-left: 6px;" />
+            {/if}
+          </div>
+          <div class="shareable-entity-identifier">
+            <Urn
+              urn={entity.shareableEntityIdentifier}
+              showOnHover
+              truncate
+              notificationText={`Radicle ID for ${name} copied to your clipboard.`} />
+          </div>
         </div>
-        <div class="shareable-entity-identifier">
-          <Urn
-            urn={entity.shareableEntityIdentifier}
-            showOnHover
-            truncate
-            notificationText={`Radicle ID for ${name} copied to your clipboard.`} />
-        </div>
+      </div>
+      <div class="banner-action">
+        <slot name="top" />
       </div>
     </div>
   </div>
