adc55d8f2e662ae33907ea8957720cb49c2a809f
Sarah Scott <sarahoscott@gmail.com>
2020-08-31 18:54:47 -0400

    rm console log

--- ui/DesignSystem/Component/ProjectList.svelte
+++ ui/DesignSystem/Component/ProjectList.svelte
@@ -9,8 +9,6 @@
   export let projects = null;
   export let urn = null;
 
-  console.log(projects);
-
   const projectCardProps = project => ({
     title: project.metadata.name,
     description: project.metadata.description,
