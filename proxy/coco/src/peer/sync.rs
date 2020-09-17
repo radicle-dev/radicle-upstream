50cde5bcb0b7e26559d76233746a4ce07302e4f9
Alexander Simmerl <a.simmerl@gmail.com>
2020-09-17 18:02:32 +0200

    Add logging to sync

--- proxy/coco/src/peer/sync.rs
+++ proxy/coco/src/peer/sync.rs
@@ -8,6 +8,8 @@ use super::Error;
 
 /// Initiaites a fetch for all locally tracked projects from the given [`PeerId`].
 pub async fn sync(state: Lock, peer_id: PeerId) -> Result<(), Error> {
+    log::debug!("Starting sync for {}", peer_id);
+
     let urls = state
         .lock()
         .await
@@ -21,11 +23,14 @@ pub async fn sync(state: Lock, peer_id: PeerId) -> Result<(), Error> {
         .collect::<Vec<RadUrl>>();
 
     for url in urls {
+        log::debug!("Starting fetch of {} from {}", url.clone(), peer_id);
         let state = state.clone();
         let state = state.lock_owned().await;
-        tokio::task::spawn_blocking(move || state.fetch(url, vec![]))
+        let task_url = url.clone();
+        tokio::task::spawn_blocking(move || state.fetch(task_url, vec![]))
             .await
             .expect("join thread failed")?;
+        log::debug!("Finished fetch of {} from {}", url, peer_id);
     }
 
     Ok(())
