cd7133791272407b242fd2bd8751b4b10c027997
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-09-21 15:42:15 +0200

    Update ui/Screen/Onboarding/EnterName.svelte
    Co-authored-by: Nuno Alexandre <hi@nunoalexandre.com>

--- ui/Screen/Onboarding/EnterName.svelte
+++ ui/Screen/Onboarding/EnterName.svelte
@@ -73,7 +73,7 @@
     </p>
     <Input.Text
       autofocus={true}
-      placeholder="Enter a display name (i.e. coolprogrammer3000)"
+      placeholder="Enter a display name (e.g. coolprogrammer3000)"
       bind:value={handle}
       on:enter={next}
       dataCy="handle-input"
