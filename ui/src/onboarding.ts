bb492c522ebe44bdc90222354e84add645c56635
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-09-21 14:24:23 +0200

    Rename handle -> display name

--- ui/src/onboarding.ts
+++ ui/src/onboarding.ts
@@ -11,12 +11,12 @@ const HANDLE_MATCH = "^[a-z0-9][a-z0-9_-]+$";
 
 const handleConstraints = {
   presence: {
-    message: "You must provide a handle",
+    message: "You must provide a display name",
     allowEmpty: false,
   },
   format: {
     pattern: new RegExp(HANDLE_MATCH, "i"),
-    message: `Handle should match ${HANDLE_MATCH}`,
+    message: `Display name should match ${HANDLE_MATCH}`,
   },
 };
 
