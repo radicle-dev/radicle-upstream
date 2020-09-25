56d71bc6724e5953719b2816e2c9428e959609a0
FintanH <fintan.halpenny@gmail.com>
2020-09-25 16:58:31 +0100

    Introduce Shared type
    This type makes it easy to use the `Into` pattern for something that
    needs to be held in a Arc/RwLock.

---
+++ proxy/coco/src/shared.rs
@@ -0,0 +1,23 @@
+use std::{ops::Deref, sync::Arc};
+
+use tokio::sync::RwLock;
+
+pub struct Shared<T> {
+    pub value: Arc<RwLock<T>>,
+}
+
+impl<T> From<T> for Shared<T> {
+    fn from(other: T) -> Self {
+        Self {
+            value: Arc::from(RwLock::from(other)),
+        }
+    }
+}
+
+impl<T> Deref for Shared<T> {
+    type Target = Arc<RwLock<T>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
