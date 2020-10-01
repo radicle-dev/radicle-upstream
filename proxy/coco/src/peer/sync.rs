radicle-dev/radicle-upstream

commit ebabc6fc9cd386be5ed43dd3d2fa48f7dba9ac46

refactor(proxy): move to coco pooled storage, cancellable async everywhere (#944)
Contains three somewhat unrelated changes:

1. `librad` now exposes access to `Storage` through a bounded pool, and ensures the blocking operations on it are properly `spawn_blocking`'ed. This means less opportunities to accidentally starve the async runtime, at the cost of some performance overhead. Acquiring locks is, however, no longer necessary. As a consequence, many formerly synchronous methods an functions are now `async`.
2. `librad` now goes through much pain to ensure all resources held by a running `PeerApi` are released when it is dropped. This required a bit of restructuring -- when restarting the proxy, we now simply drop all stateful handles, and restart from either the persistent state, or a fresh ephemeral one when `--test` is used. On the bright side, no interior mutability is required for the api `Context`. 
3. We upgrade to the released v0.1.0 of `radicle-keystore`, which removes the dependency on `libsodium`. Please note that this is a **breaking change**, as keystores created with the previous version will no longer be readable.

proxy/coco/src/peer/sync.rs
@@ -2,19 +2,17 @@
 
 use librad::{peer::PeerId, uri::RadUrl};
 
-use crate::state::Lock;
+use crate::state::State;
 
 use super::Error;
 
 /// Initiaites a fetch for all locally tracked projects from the given [`PeerId`].
-pub async fn sync(state: Lock, peer_id: PeerId) -> Result<(), Error> {
+pub async fn sync(state: &State, peer_id: PeerId) -> Result<(), Error> {
     log::debug!("Starting sync from {}", peer_id);
 
     let urls = state
-        .lock()
-        .await
         .list_projects()
-        .map_err(Error::from)?
+        .await?
         .iter()
         .map(|project| RadUrl {
             authority: peer_id.clone(),
@@ -24,12 +22,7 @@ pub async fn sync(state: Lock, peer_id: PeerId) -> Result<(), Error> {
 
     for url in urls {
         log::debug!("Starting fetch of {} from {}", url.clone(), peer_id);
-        let state = state.clone();
-        let state = state.lock_owned().await;
-        let task_url = url.clone();
-        tokio::task::spawn_blocking(move || state.fetch(task_url, vec![]))
-            .await
-            .expect("join thread failed")?;
+        state.fetch(url.clone(), vec![]).await?;
         log::debug!("Finished fetch of {} from {}", url, peer_id);
     }
 

cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add("resetCocoState", async () => {
   console.log("Reset CoCo state");
-  await fetch("http://localhost:8080/v1/control/reset/coco");
+  await fetch("http://localhost:8080/v1/control/reload");
 });
 
 Cypress.Commands.add("resetSessionState", async () => {
@@ -12,7 +12,7 @@ Cypress.Commands.add("resetAllState", async () => {
   console.log("Reset CoCo, Registry and session state");
   try {
     await fetch("http://localhost:8080/v1/session", { method: "DELETE" });
-    await fetch("http://localhost:8080/v1/control/reset/coco");
+    await fetch("http://localhost:8080/v1/control/reload");
   } catch (error) {
     console.error(error);
   }

proxy/api/src/context.rs
@@ -1,24 +1,12 @@
 //! Datastructure and machinery to safely share the common dependencies across components.
 
-use std::sync::Arc;
-
-use tokio::sync::RwLock;
-
-use coco::{keystore, signer};
-
-/// Wrapper around the thread-safe handle on [`Context`].
-pub type Ctx = Arc<RwLock<Context>>;
-
-impl From<Context> for Ctx {
-    fn from(ctx: Context) -> Self {
-        Arc::new(RwLock::new(ctx))
-    }
-}
+use coco::signer;
 
 /// Container to pass down dependencies into HTTP filter chains.
+#[derive(Clone)]
 pub struct Context {
     /// [`coco::State`] to operate on the local monorepo.
-    pub state: coco::Lock,
+    pub state: coco::State,
     /// [`coco::signer::BoxedSigner`] for write operations on the monorepo.
     pub signer: signer::BoxedSigner,
     /// [`kv::Store`] used for session state and cache.
@@ -26,75 +14,32 @@ pub struct Context {
 }
 
 impl Context {
-    /// Initialises a new [`Ctx`] the given temporary directory.
+    /// Initialises a new [`Context`] the given temporary directory.
     ///
     /// # Errors
     ///
     /// * coco key creation fails
     /// * creation of the [`kv::Store`] fails
     #[cfg(test)]
-    pub async fn tmp(tmp_dir: &tempfile::TempDir) -> Result<Ctx, crate::error::Error> {
-        let paths = coco::Paths::from_root(tmp_dir.path())?;
+    pub async fn tmp(tmp_dir: &tempfile::TempDir) -> Result<Self, crate::error::Error> {
+        use coco::{keystore, RunConfig};
 
         let store = kv::Store::new(kv::Config::new(tmp_dir.path().join("store")))?;
+
         let pw = keystore::SecUtf8::from("radicle-upstream");
-        let mut keystore = keystore::Keystorage::new(&paths, pw);
-        let key = keystore.init()?;
+        let key = keystore::Keystorage::memory(pw)?.get();
         let signer = signer::BoxedSigner::from(signer::SomeSigner { signer: key });
 
         let (_peer, state) = {
             let config = coco::config::default(key, tmp_dir.path())?;
-            coco::into_peer_state(config, signer.clone()).await?
+            coco::into_peer_state(config, signer.clone(), store.clone(), RunConfig::default())
+                .await?
         };
 
-        Ok(Arc::new(RwLock::new(Self {
+        Ok(Self {
             state,
             signer,
             store,
-        })))
+        })
     }
-}
-
-/// Resets the peer and keystore within the `Ctx`.
-///
-/// # Errors
-///
-///   * If we could not create a new temporary path.
-///   * If we could not initialise the key.
-///   * If we could not construct the peer API.
-///
-/// # Panics
-///
-///   * If we could not get the temporary directory.
-pub async fn reset_ctx_peer(ctx: Ctx) -> Result<(), crate::error::Error> {
-    // TmpDir deletes the temporary directory once it DROPS.
-    // This means our new directory goes missing, and future calls will fail.
-    // The Peer creates the directory again.
-    //
-    // N.B. this may gather lot's of tmp files on your system. We're sorry.
-    let tmp_path = {
-        let temp_dir = tempfile::tempdir()?;
-        log::debug!("New temporary path is: {:?}", temp_dir.path());
-        std::env::set_var("RAD_HOME", temp_dir.path());
-        temp_dir.path().to_path_buf()
-    };
-
-    let paths = coco::Paths::from_root(tmp_path.clone())?;
-
-    let pw = keystore::SecUtf8::from("radicle-upstream");
-    let mut new_keystore = keystore::Keystorage::new(&paths, pw);
-    let key = new_keystore.init()?;
-    let signer = signer::BoxedSigner::from(key);
-
-    let (_new_peer, new_state) = {
-        let config = coco::config::configure(paths, key, *coco::config::LOCALHOST_ANY, vec![]);
-        coco::into_peer_state(config, signer.clone()).await?
-    };
-
-    let mut ctx = ctx.write().await;
-    ctx.state = new_state;
-    ctx.signer = signer;
-    ctx.store = kv::Store::new(kv::Config::new(tmp_path.join("store")))?;
-
-    Ok(())
 }

proxy/api/src/http.rs
@@ -1,7 +1,8 @@
 //! HTTP API delivering JSON over `RESTish` endpoints.
 
 use serde::Deserialize;
-use warp::{filters::BoxedFilter, path, reject, Filter, Rejection, Reply};
+use tokio::sync::mpsc;
+use warp::{filters::BoxedFilter, path, Filter, Rejection, Reply};
 
 use crate::{context, notification::Subscriptions};
 
@@ -33,22 +34,17 @@ macro_rules! combine {
 
 /// Main entry point for HTTP API.
 pub fn api(
-    ctx: context::Ctx,
+    ctx: context::Context,
     subscriptions: Subscriptions,
-    enable_control: bool,
+    selfdestruct: mpsc::Sender<()>,
+    enable_fixture_creation: bool,
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     let avatar_filter = path("avatars").and(avatar::get_filter());
-    let control_filter = path("control")
-        .map(move || enable_control)
-        .and_then(|enable| async move {
-            if enable {
-                Ok(())
-            } else {
-                Err(reject::not_found())
-            }
-        })
-        .untuple_one()
-        .and(control::filters(ctx.clone()));
+    let control_filter = path("control").and(control::filters(
+        ctx.clone(),
+        selfdestruct,
+        enable_fixture_creation,
+    ));
     let identity_filter = path("identities").and(identity::filters(ctx.clone()));
     let notification_filter = path("notifications").and(notification::filters(subscriptions));
     let project_filter = path("projects").and(project::filters(ctx.clone()));
@@ -94,12 +90,10 @@ pub fn api(
 /// Asserts presence of the owner and reject the request early if missing. Otherwise unpacks and
 /// passes down.
 #[must_use]
-fn with_owner_guard(ctx: context::Ctx) -> BoxedFilter<(coco::user::User,)> {
+fn with_owner_guard(ctx: context::Context) -> BoxedFilter<(coco::user::User,)> {
     warp::any()
         .and(with_context(ctx))
-        .and_then(|ctx: context::Ctx| async move {
-            let ctx = ctx.read().await;
-
+        .and_then(|ctx: context::Context| async move {
             let session = crate::session::current(ctx.state.clone(), &ctx.store)
                 .await
                 .expect("unable to get current sesison");
@@ -107,9 +101,8 @@ fn with_owner_guard(ctx: context::Ctx) -> BoxedFilter<(coco::user::User,)> {
             if let Some(identity) = session.identity {
                 let user = ctx
                     .state
-                    .lock()
+                    .get_user(identity.urn)
                     .await
-                    .get_user(&identity.urn)
                     .expect("unable to get coco user");
                 let user = coco::user::verify(user).expect("unable to verify user");
 
@@ -123,7 +116,7 @@ fn with_owner_guard(ctx: context::Ctx) -> BoxedFilter<(coco::user::User,)> {
 
 /// Middleware filter to inject a context into a filter chain to be passed down to a handler.
 #[must_use]
-fn with_context(ctx: context::Ctx) -> BoxedFilter<(context::Ctx,)> {
+fn with_context(ctx: context::Context) -> BoxedFilter<(context::Context,)> {
     warp::any().map(move || ctx.clone()).boxed()
 }
 

proxy/api/src/http/control.rs
@@ -1,79 +1,89 @@
 //! Endpoints to manipulate app state in test mode.
 
 use serde::{Deserialize, Serialize};
-use warp::{filters::BoxedFilter, path, Filter, Rejection, Reply};
+use tokio::sync::mpsc;
+use warp::{filters::BoxedFilter, path, reject, Filter, Rejection, Reply};
 
 use crate::context;
 
 /// Combination of all control filters.
-pub fn filters(ctx: context::Ctx) -> BoxedFilter<(impl Reply,)> {
-    create_project_filter(ctx.clone())
-        .or(reset_coco_filter(ctx))
+pub fn filters(
+    ctx: context::Context,
+    selfdestruct: mpsc::Sender<()>,
+    enable_fixture_creation: bool,
+) -> BoxedFilter<(impl Reply,)> {
+    create_project_filter(ctx, enable_fixture_creation)
+        .or(reload_filter(selfdestruct))
         .boxed()
 }
 
 /// POST /create-project
 fn create_project_filter(
-    ctx: context::Ctx,
+    ctx: context::Context,
+    enabled: bool,
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     path!("create-project")
+        .map(move || enabled)
+        .and_then(|enable| async move {
+            if enable {
+                Ok(())
+            } else {
+                Err(reject::not_found())
+            }
+        })
+        .untuple_one()
         .and(super::with_context(ctx.clone()))
         .and(super::with_owner_guard(ctx))
         .and(warp::body::json())
         .and_then(handler::create_project)
 }
 
-/// GET /reset/coco
-fn reset_coco_filter(
-    ctx: context::Ctx,
+/// GET /reload
+fn reload_filter(
+    selfdestruct: mpsc::Sender<()>,
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    path!("reset" / "coco")
-        .and(super::with_context(ctx))
-        .and_then(handler::reset_coco)
+    path!("reload")
+        .map(move || selfdestruct.clone())
+        .and_then(handler::reload)
 }
 
 /// Control handlers for conversion between core domain and http request fulfilment.
 mod handler {
+    use tokio::sync::mpsc;
     use warp::{http::StatusCode, reply, Rejection, Reply};
 
-    use coco::{keystore, signer, user};
+    use coco::user;
 
     use crate::{context, error, project};
 
     /// Create a project from the fixture repo.
     #[allow(clippy::let_underscore_must_use)]
     pub async fn create_project(
-        ctx: context::Ctx,
+        ctx: context::Context,
         owner: user::User,
         input: super::CreateInput,
     ) -> Result<impl Reply, Rejection> {
-        let ctx = ctx.read().await;
-
         let meta = coco::control::replicate_platinum(
-            &(*ctx.state.lock().await),
+            &ctx.state,
             &ctx.signer,
             &owner,
             &input.name,
             &input.description,
             &input.default_branch,
         )
+        .await
         .map_err(error::Error::from)?;
 
         if let Some(user_handle_list) = input.fake_peers {
             for user_handle in user_handle_list {
-                let _ = coco::control::track_fake_peer(
-                    &(*ctx.state.lock().await),
-                    &ctx.signer,
-                    &meta,
-                    &user_handle,
-                );
+                let _ =
+                    coco::control::track_fake_peer(&ctx.state, &ctx.signer, &meta, &user_handle);
             }
         }
         let stats = ctx
             .state
-            .lock()
+            .with_browser(meta.urn(), |browser| Ok(browser.get_stats()?))
             .await
-            .with_browser(&meta.urn(), |browser| Ok(browser.get_stats()?))
             .map_err(error::Error::from)?;
         let project: project::Project = (meta, stats).into();
 
@@ -83,82 +93,10 @@ mod handler {
         ))
     }
 
-    /// Reset the local `CoCo` state.
-    pub async fn reset_coco(ctx: context::Ctx) -> Result<impl Reply, Rejection> {
-        // TmpDir deletes the temporary directory once it DROPS.
-        // This means our new directory goes missing, and future calls will fail.
-        // The Peer creates the directory again.
-        //
-        // N.B. this may gather lot's of tmp files on your system. We're sorry.
-        let tmp_path = {
-            let temp_dir = tempfile::tempdir().expect("test dir creation failed");
-            log::debug!("New temporary path is: {:?}", temp_dir.path());
-            std::env::set_var("RAD_HOME", temp_dir.path());
-            temp_dir.path().to_path_buf()
-        };
-
-        let paths = coco::Paths::from_root(tmp_path.clone()).map_err(error::Error::from)?;
-
-        let store =
-            kv::Store::new(kv::Config::new(tmp_path.join("store"))).map_err(error::Error::from)?;
-        let pw = keystore::SecUtf8::from("radicle-upstream");
-        let mut new_keystore = keystore::Keystorage::new(&paths, pw);
-        let key = new_keystore.init().map_err(error::Error::from)?;
-        let signer = signer::BoxedSigner::from(key);
-
-        let (_new_peer, new_state) = {
-            let config = coco::config::configure(paths, key, *coco::config::LOCALHOST_ANY, vec![]);
-            coco::into_peer_state(config, signer.clone())
-                .await
-                .map_err(error::Error::from)?
-        };
-
-        let mut ctx = ctx.write().await;
-        ctx.state = new_state;
-        ctx.signer = signer;
-        ctx.store = store;
-
-        Ok(reply::json(&true))
-    }
-
-    #[allow(clippy::unwrap_used, clippy::panic)]
-    #[cfg(test)]
-    mod test {
-        use pretty_assertions::assert_ne;
-
-        use crate::{context, error};
-
-        #[tokio::test]
-        async fn reset_coco() -> Result<(), error::Error> {
-            let tmp_dir = tempfile::tempdir()?;
-            let ctx = context::Context::tmp(&tmp_dir).await?;
-
-            let (old_paths, old_peer_id) = {
-                let ctx = ctx.read().await;
-                let state = ctx.state.lock().await;
-                (state.paths(), state.peer_id())
-            };
-
-            super::reset_coco(ctx.clone()).await.unwrap();
-
-            let (new_paths, new_peer_id) = {
-                let ctx = ctx.read().await;
-                let state = ctx.state.lock().await;
-                (state.paths(), state.peer_id())
-            };
-
-            assert_ne!(old_paths.all_dirs(), new_paths.all_dirs());
-            assert_ne!(old_peer_id, new_peer_id);
-
-            let can_open = {
-                let ctx = ctx.read().await;
-                ctx.state.lock().await.reopen()?;
-                true
-            };
-            assert!(can_open);
-
-            Ok(())
-        }
+    /// Abort the server task, which causes `main` to restart it.
+    pub async fn reload(mut notify: mpsc::Sender<()>) -> Result<impl Reply, Rejection> {
+        log::warn!("reload requested");
+        Ok(reply::json(&notify.send(()).await.is_ok()))
     }
 }
 

proxy/api/src/http/identity.rs
@@ -1,23 +1,21 @@
 //! Manage the state and stateful interactions with the underlying peer API of librad.
 
-use std::sync::Arc;
-
 use serde::{Deserialize, Serialize};
 use warp::{filters::BoxedFilter, path, Filter, Rejection, Reply};
 
 use crate::{context, http};
 
 /// Combination of all identity routes.
-pub fn filters(ctx: context::Ctx) -> BoxedFilter<(impl Reply,)> {
-    get_filter(Arc::clone(&ctx))
-        .or(create_filter(Arc::clone(&ctx)))
+pub fn filters(ctx: context::Context) -> BoxedFilter<(impl Reply,)> {
+    get_filter(ctx.clone())
+        .or(create_filter(ctx.clone()))
         .or(list_filter(ctx))
         .boxed()
 }
 
 /// `POST /`
 fn create_filter(
-    ctx: context::Ctx,
+    ctx: context::Context,
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     http::with_context(ctx)
         .and(warp::post())
@@ -26,7 +24,9 @@ fn create_filter(
 }
 
 /// `GET /<id>`
-fn get_filter(ctx: context::Ctx) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+fn get_filter(
+    ctx: context::Context,
+) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     http::with_context(ctx)
         .and(warp::get())
         .and(path::param::<coco::Urn>())
@@ -34,7 +34,9 @@ fn get_filter(ctx: context::Ctx) -> impl Filter<Extract = impl Reply, Error = Re
 }
 
 /// `GET /`
-fn list_filter(ctx: context::Ctx) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+fn list_filter(
+    ctx: context::Context,
+) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     http::with_context(ctx)
         .and(warp::get())
         .and(path::end())
@@ -49,11 +51,9 @@ mod handler {
 
     /// Create a new [`identity::Identity`].
     pub async fn create(
-        ctx: context::Ctx,
+        ctx: context::Context,
         input: super::CreateInput,
     ) -> Result<impl Reply, Rejection> {
-        let ctx = ctx.read().await;
-
         if let Some(identity) = session::current(ctx.state.clone(), &ctx.store)
             .await?
             .identity
@@ -63,7 +63,7 @@ mod handler {
             )));
         }
 
-        let id = identity::create(&(*ctx.state.lock().await), &ctx.signer, &input.handle)?;
+        let id = identity::create(&ctx.state, &ctx.signer, &input.handle).await?;
 
         session::set_identity(&ctx.store, id.clone())?;
 
@@ -71,18 +71,14 @@ mod handler {
     }
 
     /// Get the [`identity::Identity`] for the given `id`.
-    pub async fn get(ctx: context::Ctx, id: coco::Urn) -> Result<impl Reply, Rejection> {
-        let ctx = ctx.read().await;
-        let state = ctx.state.lock().await;
-        let id = identity::get(&state, &id)?;
+    pub async fn get(ctx: context::Context, id: coco::Urn) -> Result<impl Reply, Rejection> {
+        let id = identity::get(&ctx.state, id.clone()).await?;
         Ok(reply::json(&id))
     }
 
     /// Retrieve the list of identities known to the session user.
-    pub async fn list(ctx: context::Ctx) -> Result<impl Reply, Rejection> {
-        let ctx = ctx.read().await;
-        let state = ctx.state.lock().await;
-        let users = identity::list(&state)?;
+    pub async fn list(ctx: context::Context) -> Result<impl Reply, Rejection> {
+        let users = identity::list(&ctx.state).await?;
         Ok(reply::json(&users))
     }
 }
@@ -121,19 +117,19 @@ mod test {
             .reply(&api)
             .await;
 
-        let ctx = ctx.read().await;
-
         let urn = {
             let session = session::current(ctx.state.clone(), &ctx.store).await?;
             session.identity.expect("failed to set identity").urn
         };
 
-        let state = ctx.state.lock().await;
-        let peer_id = state.peer_id();
+        let peer_id = ctx.state.peer_id();
 
         // Assert that we set the default owner and it's the same one as the session
         {
-            assert_eq!(state.default_owner(), Some(state.get_user(&urn)?));
+            assert_eq!(
+                ctx.state.default_owner().await,
+                Some(ctx.state.get_user(urn.clone()).await?)
+            );
         }
 
         http::test::assert_response(&res, StatusCode::CREATED, |have| {
@@ -162,12 +158,7 @@ mod test {
         let ctx = context::Context::tmp(&tmp_dir).await?;
         let api = super::filters(ctx.clone());
 
-        let ctx = ctx.read().await;
-
-        let user = {
-            let state = ctx.state.lock().await;
-            state.init_user(&ctx.signer, "cloudhead")?
-        };
+        let user = ctx.state.init_user(&ctx.signer, "cloudhead").await?;
 
         let res = request()
             .method("GET")
@@ -175,9 +166,8 @@ mod test {
             .reply(&api)
             .await;
 
-        let state = ctx.state.lock().await;
         let handle = user.name().to_string();
-        let peer_id = state.peer_id();
+        let peer_id = ctx.state.peer_id();
         let urn = user.urn();
         let shareable_entity_identifier = (peer_id.clone(), user).into();
 
@@ -203,29 +193,28 @@ mod test {
         let ctx = context::Context::tmp(&tmp_dir).await?;
         let api = super::filters(ctx.clone());
 
-        let ctx = ctx.read().await;
-
         let fintohaps: identity::Identity = {
-            let state = ctx.state.lock().await;
-            let id = identity::create(&state, &ctx.signer, "cloudhead")?;
+            let id = identity::create(&ctx.state, &ctx.signer, "cloudhead").await?;
 
             let owner = {
-                let user = state.get_user(&id.urn)?;
+                let user = ctx.state.get_user(id.urn.clone()).await?;
                 coco::user::verify(user)?
             };
 
             session::set_identity(&ctx.store, id)?;
 
             let platinum_project = coco::control::replicate_platinum(
-                &state,
+                &ctx.state,
                 &ctx.signer,
                 &owner,
                 "git-platinum",
                 "fixture data",
                 "master",
-            )?;
+            )
+            .await?;
 
-            coco::control::track_fake_peer(&state, &ctx.signer, &platinum_project, "fintohaps")
+            coco::control::track_fake_peer(&ctx.state, &ctx.signer, &platinum_project, "fintohaps")
+                .await
                 .into()
         };
 

commit c2840d49eebb0b2ffb27e7f08748807e2fe569b5

fix(proxy): ignore fetch errors in when syncing
Fixes a regression introduced in #944: while syncing, the other end may
not have the URLs we're interested in, or may go away in the middle of
us fetching. We should thus just ignore any errors.

proxy/coco/src/peer/sync.rs
@@ -22,8 +22,10 @@ pub async fn sync(state: &State, peer_id: PeerId) -> Result<(), Error> {
 
     for url in urls {
         log::debug!("Starting fetch of {} from {}", url.clone(), peer_id);
-        state.fetch(url.clone(), vec![]).await?;
-        log::debug!("Finished fetch of {} from {}", url, peer_id);
+        match state.fetch(url.clone(), vec![]).await {
+            Ok(()) => log::debug!("Finished fetch of {} from {}", url, peer_id),
+            Err(e) => log::debug!("Fetch of {} from {} errored: {}", url, peer_id, e),
+        }
     }
 
     Ok(())