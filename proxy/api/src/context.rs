radicle-dev/radicle-upstream

commit 35bc0b41f1e9d7d0e102b185879230918f55f0dc

fix(proxy): prevent halting peer subroutines (#992)
The polling of subroutines can starve if the channels are not completely
drained. To remedy that this refactor restructures the internals of the
peer, most notably by have the RunState drive the WaitingRoom
transitions.

* move WaitingRoom into RunState
* use SelectAll for subroutines
* introduce handle for peer control
* remove Shared

Fixes #981
Fixes #1009

Co-authored-by: Nuno Alexandre <hi@nunoalexandre.com>

proxy/api/src/context.rs
@@ -1,20 +1,21 @@
 //! Datastructure and machinery to safely share the common dependencies across components.
 
-use std::time::{Duration, Instant};
+use coco::{signer, PeerControl};
 
-use coco::{request::waiting_room::WaitingRoom, shared::Shared, signer};
+#[cfg(test)]
+use coco::{keystore, RunConfig};
 
 /// Container to pass down dependencies into HTTP filter chains.
 #[derive(Clone)]
 pub struct Context {
+    /// Handle to inspect state and perform actions on the currently running local [`coco::Peer`].
+    pub peer_control: PeerControl,
     /// [`coco::State`] to operate on the local monorepo.
     pub state: coco::State,
     /// [`coco::signer::BoxedSigner`] for write operations on the monorepo.
     pub signer: Option<signer::BoxedSigner>,
     /// [`kv::Store`] used for session state and cache.
     pub store: kv::Store,
-    /// The [`WaitingRoom`] for making requests for identities.
-    pub waiting_room: Shared<WaitingRoom<Instant, Duration>>,
 }
 
 impl Context {
@@ -26,32 +27,26 @@ impl Context {
     /// * creation of the [`kv::Store`] fails
     #[cfg(test)]
     pub async fn tmp(tmp_dir: &tempfile::TempDir) -> Result<Self, crate::error::Error> {
-        use coco::{keystore, request::waiting_room, RunConfig};
-
         let store = kv::Store::new(kv::Config::new(tmp_dir.path().join("store")))?;
 
         let pw = keystore::SecUtf8::from("radicle-upstream");
         let key = keystore::Keystorage::memory(pw)?.get();
         let signer = signer::BoxedSigner::from(signer::SomeSigner { signer: key });
 
-        let waiting_room = Shared::from(WaitingRoom::new(waiting_room::Config::default()));
-        let (_peer, state) = {
+        let (peer_control, state) = {
             let config = coco::config::default(key, tmp_dir.path())?;
-            coco::into_peer_state(
-                config,
-                signer.clone(),
-                store.clone(),
-                waiting_room.clone(),
-                RunConfig::default(),
-            )
-            .await?
+            let (peer, state) =
+                coco::into_peer_state(config, signer.clone(), store.clone(), RunConfig::default())
+                    .await?;
+
+            (peer.control(), state)
         };
 
         Ok(Self {
+            peer_control,
             state,
             signer: Some(signer),
             store,
-            waiting_room,
         })
     }
 }

proxy/api/src/http/project.rs
@@ -186,9 +186,9 @@ mod handler {
 
     /// Kick off a network request for the [`project::Project`] of the given `id`.
     pub async fn request(ctx: context::Context, urn: coco::Urn) -> Result<impl Reply, Rejection> {
+        let mut peer_control = ctx.peer_control;
         // TODO(finto): Check the request exists in the monorepo
-        let mut waiting_room = ctx.waiting_room.write().await;
-        let _request = waiting_room.request(urn, Instant::now());
+        let _request = peer_control.request_urn(&urn, Instant::now());
 
         // TODO(finto): Serialise request and respond with that.
         Ok(reply::json(&true))

proxy/api/src/error.rs
@@ -9,7 +9,7 @@ pub enum Error {
     #[error(transparent)]
     Keystore(#[from] coco::keystore::Error),
 
-    /// Error interacting with [`state::State`].
+    /// Error interacting with [`coco::state::State`].
     #[error(transparent)]
     State(#[from] coco::state::Error),
 

proxy/api/src/main.rs
@@ -8,13 +8,7 @@ use tokio::{
 };
 
 use api::{config, context, env, http, notification, session};
-use coco::{
-    keystore,
-    request::waiting_room::{self, WaitingRoom},
-    seed,
-    shared::Shared,
-    signer, Peer, RunConfig, SyncConfig,
-};
+use coco::{keystore, seed, signer, Peer, RunConfig, SyncConfig};
 
 /// Flags accepted by the proxy binary.
 #[derive(Clone, Copy)]
@@ -155,13 +149,6 @@ async fn rig(args: Args) -> Result<Rigging, Box<dyn std::error::Error>> {
 
     let signer = signer::BoxedSigner::new(signer::SomeSigner { signer: key });
 
-    // TODO(finto): We should store and load the waiting room
-    let waiting_room = {
-        let mut config = waiting_room::Config::default();
-        config.delta = Duration::from_secs(10);
-        Shared::from(WaitingRoom::new(config))
-    };
-
     let (peer, state) = {
         let seeds = session::settings(&store).await?.coco.seeds;
         let seeds = seed::resolve(&seeds).await.unwrap_or_else(|err| {
@@ -174,7 +161,6 @@ async fn rig(args: Args) -> Result<Rigging, Box<dyn std::error::Error>> {
             config,
             signer.clone(),
             store.clone(),
-            waiting_room.clone(),
             RunConfig {
                 sync: SyncConfig {
                     max_peers: 1,
@@ -187,12 +173,13 @@ async fn rig(args: Args) -> Result<Rigging, Box<dyn std::error::Error>> {
         .await?
     };
 
+    let peer_control = peer.control();
     let subscriptions = notification::Subscriptions::default();
     let ctx = context::Context {
+        peer_control,
         state,
         signer: Some(signer),
         store,
-        waiting_room,
     };
 
     Ok(Rigging {

proxy/coco/src/convert.rs
@@ -1,0 +1,15 @@
+//! Traits for conversions between types.
+
+use std::convert::TryFrom;
+
+/// Used to do conversions where the input maybe maps.
+pub trait MaybeFrom<T>: Sized {
+    /// Performs the conversion.
+    fn maybe_from(_: T) -> Option<Self>;
+}
+
+impl<T, U: TryFrom<T>> MaybeFrom<T> for U {
+    fn maybe_from(t: T) -> Option<Self> {
+        Self::try_from(t).ok()
+    }
+}

proxy/coco/src/lib.rs
@@ -25,10 +25,7 @@
 #![feature(hash_set_entry)]
 #![feature(or_patterns)]
 
-use std::{
-    net::SocketAddr,
-    time::{Duration, Instant},
-};
+use std::net::SocketAddr;
 
 pub use librad::{
     git::{include, local::url::LocalUrl},
@@ -50,6 +47,7 @@ pub use radicle_surf::{
 
 pub mod config;
 pub mod control;
+mod convert;
 pub mod git_helper;
 mod identifier;
 pub use identifier::Identifier;
@@ -57,10 +55,8 @@ pub mod keystore;
 pub mod oid;
 pub mod peer;
 pub use peer::{
-    AnnounceConfig, AnnounceEvent, Event as PeerEvent, Peer, RequestEvent, RunConfig, SyncConfig,
-    SyncEvent,
+    AnnounceConfig, Control as PeerControl, Event as PeerEvent, Peer, RunConfig, SyncConfig,
 };
-pub mod shared;
 pub mod state;
 pub use state::State;
 pub mod project;
@@ -76,9 +72,12 @@ pub use source::{
     Revisions, Tag, Tree, TreeEntry,
 };
 
+mod spawn_abortable;
+pub use spawn_abortable::{Error as SpawnAbortableError, SpawnAbortable};
+
 pub mod user;
 
-/// Constructs a [`Peer`] and [`State`] pair from a [`PeerConfig`].
+/// Constructs a [`Peer`] and [`State`] pair from a [`net::peer::PeerConfig`].
 ///
 /// # Errors
 ///
@@ -88,7 +87,6 @@ pub async fn into_peer_state<I>(
     config: net::peer::PeerConfig<discovery::Static<I, SocketAddr>, keys::SecretKey>,
     signer: librad::signer::BoxedSigner,
     store: kv::Store,
-    waiting_room: shared::Shared<request::waiting_room::WaitingRoom<Instant, Duration>>,
     run_config: RunConfig,
 ) -> Result<(Peer, State), state::Error>
 where
@@ -98,7 +96,7 @@ where
     let (api, run_loop) = peer.accept()?;
 
     let state = State::new(api, signer);
-    let peer = Peer::new(run_loop, state.clone(), store, waiting_room, run_config);
+    let peer = Peer::new(run_loop, state.clone(), store, run_config);
 
     Ok((peer, state))
 }

proxy/coco/src/peer.rs
@@ -2,58 +2,40 @@
 //! prorper state updates.
 
 use std::{
-    convert::From,
     future::Future,
     pin::Pin,
     sync::Arc,
     task::{Context, Poll},
-    time::{Duration, Instant},
 };
 
-use futures::{
-    future::{self, FutureExt as _},
-    stream::{BoxStream, FuturesUnordered, StreamExt as _},
-};
-use tokio::{
-    sync::{broadcast, mpsc, Barrier},
-    task::{JoinError, JoinHandle},
-    time::{interval, Interval},
-};
+use futures::{future::FutureExt as _, stream::StreamExt as _};
+use tokio::sync::{broadcast, mpsc, Barrier};
 
-use librad::{
-    net::{
-        peer::{Gossip, RunLoop},
-        protocol::ProtocolEvent,
-    },
-    peer::PeerId,
-    uri::{RadUrl, RadUrn},
-};
+use librad::net::peer::RunLoop;
 
 use crate::{
-    request::waiting_room::{self, WaitingRoom},
-    shared::Shared,
+    spawn_abortable::{self, SpawnAbortable},
     state::{self, State},
 };
 
 mod announcement;
 pub use announcement::Announcement;
 
-mod request;
+mod control;
+pub use control::Control;
+
+pub mod gossip;
 
 mod run_state;
-pub use run_state::{
-    AnnounceConfig, AnnounceEvent, Config as RunConfig, Event, RequestCommand, RequestEvent,
-    SyncConfig, SyncEvent, TimeoutEvent,
-};
-use run_state::{Command, RunState};
+pub use run_state::{AnnounceConfig, Config as RunConfig, Event, SyncConfig};
+
+mod subroutines;
+use subroutines::Subroutines;
 
 mod sync;
 
 /// Upper bound of messages stored in receiver channels.
-const RECEIVER_CAPACITY: usize = 128;
-
-/// The period at which we ping the `select!` loop.
-const PING_PERIOD: Duration = Duration::from_millis(500);
+pub const RECEIVER_CAPACITY: usize = 128;
 
 /// Peer operation errors.
 #[derive(Debug, thiserror::Error)]
@@ -64,7 +46,7 @@ pub enum Error {
 
     /// There was an error in a spawned task.
     #[error("the running peer was either cancelled, or one of its tasks panicked")]
-    Spawn(#[source] SpawnAbortableError),
+    Spawn(#[source] spawn_abortable::Error),
 
     /// There was an error when interacting with [`State`].
     #[error(transparent)]
@@ -79,33 +61,37 @@ pub struct Peer {
     state: State,
     /// On-disk storage for caching.
     store: kv::Store,
-    /// The waiting room for making requests for identities.
-    waiting_room: Shared<WaitingRoom<Instant, Duration>>,
-    /// Handle used to broadcast [`Event`].
+    /// Handle used to broadcast [`Event`]s.
     subscriber: broadcast::Sender<Event>,
     /// Subroutine config.
     run_config: RunConfig,
+    /// Receiving end of requests fired from control handles.
+    control_receiver: mpsc::Receiver<control::Request>,
+    /// Sending end for control handles to send requests to the running peer.
+    control_sender: mpsc::Sender<control::Request>,
 }
 
 impl Peer {
     /// Constructs a new [`Peer`].
     #[must_use = "give a peer some love"]
-    pub fn new(
-        run_loop: RunLoop,
-        state: State,
-        store: kv::Store,
-        waiting_room: Shared<WaitingRoom<Instant, Duration>>,
-        run_config: RunConfig,
-    ) -> Self {
+    pub fn new(run_loop: RunLoop, state: State, store: kv::Store, run_config: RunConfig) -> Self {
         let (subscriber, _receiver) = broadcast::channel(RECEIVER_CAPACITY);
+        let (control_sender, control_receiver) = mpsc::channel(RECEIVER_CAPACITY);
         Self {
             run_loop,
             state,
             store,
-            waiting_room,
             subscriber,
             run_config,
+            control_receiver,
+            control_sender,
         }
+    }
+
+    /// Acquire a handle to inspect state and perform actions on a running peer.
+    #[must_use = "take control"]
+    pub fn control(&self) -> Control {
+        Control::new(self.control_sender.clone())
     }
 
     /// Subscribe to peer events.
@@ -121,18 +107,20 @@ impl Peer {
     /// Start up the internal machinery to advance the underlying protocol, react to significant
     /// events and keep auxiliary tasks running.
     ///
-    /// The returned [`Running`] future has similar semantics to [`JoinHandle`]: internally, all
-    /// tasks are spawned immediately, and polling the future is only necessary to get notified of
-    /// errors. Unlike [`JoinHandle`], however, [`Running`] does not detach the tasks. That is,
-    /// if and when [`Running`] is dropped, all tasks are cancelled.
+    /// The returned [`Running`] future has similar semantics to [`tokio::task::JoinHandle`]:
+    /// internally, all tasks are spawned immediately, and polling the future is only necessary
+    /// to get notified of errors. Unlike [`tokio::task::JoinHandle`], however, [`Running`] does
+    /// not detach the tasks. That is, if and when [`Running`] is dropped, all tasks are
+    /// cancelled.
     pub fn into_running(self) -> Running {
         let Self {
             run_loop,
             state,
             store,
-            waiting_room,
             subscriber,
             run_config,
+            control_receiver,
+            ..
         } = self;
 
         // Rendezvous on a barrier to let the subroutines subscribe for protocol
@@ -148,10 +136,10 @@ impl Peer {
                 Subroutines::new(
                     state,
                     store,
-                    waiting_room,
                     run_config,
                     protocol_events,
                     subscriber,
+                    control_receiver,
                 )
                 .await
             })
@@ -174,7 +162,7 @@ pub struct Running {
     /// Join and abort handles for the protocol run loop.
     protocol: SpawnAbortable<()>,
     /// The [`Subroutines`] associated with this [`Peer`] instance.
-    subroutines: SpawnAbortable<Result<(), SpawnAbortableError>>,
+    subroutines: SpawnAbortable<Result<(), spawn_abortable::Error>>,
 }
 
 impl Drop for Running {
@@ -212,362 +200,5 @@ impl Future for Running {
             },
             Poll::Pending => Poll::Pending,
         }
-    }
-}
-
-/// [`SpawnAbortable`] errors.
-#[derive(Debug, thiserror::Error)]
-pub enum SpawnAbortableError {
-    /// The spawned task either panicked, or was cancelled by the runtime.
-    #[error(transparent)]
-    Join(#[from] JoinError),
-
-    /// The spawned task was aborted by calling [`SpawnAbortable::abort`].
-    #[error(transparent)]
-    Abort(#[from] future::Aborted),
-}
-
-/// A spawned task which can also be aborted by the user.
-///
-/// Stop-gap until we can abort [`JoinHandle`]s directly:
-/// tokio-rs@cbb14a7bb9a13363e1abee8caff2bad1f996c263
-#[allow(clippy::missing_docs_in_private_items)]
-pub struct SpawnAbortable<T> {
-    join_handle: JoinHandle<Result<T, future::Aborted>>,
-    abort_handle: future::AbortHandle,
-}
-
-impl<T> SpawnAbortable<T> {
-    /// Create a new [`SpawnAbortable`] from a [`Future`].
-    ///
-    /// The supplied [`Future`] will be spawned onto the async executor **immediately**!
-    pub fn new<Fut>(fut: Fut) -> Self
-    where
-        Fut: Future<Output = T> + Send + 'static,
-        Fut::Output: Send + 'static,
-    {
-        let (abort_handle, abort_reg) = future::AbortHandle::new_pair();
-        let join_handle = tokio::spawn(future::Abortable::new(fut, abort_reg));
-
-        Self {
-            join_handle,
-            abort_handle,
-        }
-    }
-
-    /// Abort this future.
-    ///
-    /// Subsequent polls will return `SpawnAbortableError::Abort`.
-    pub fn abort(&mut self) {
-        self.abort_handle.abort()
-    }
-}
-
-impl<T> Drop for SpawnAbortable<T> {
-    fn drop(&mut self) {
-        self.abort()
-    }
-}
-
-impl<T> Future for SpawnAbortable<T> {
-    type Output = Result<T, SpawnAbortableError>;
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        match self.join_handle.poll_unpin(cx) {
-            Poll::Pending => Poll::Pending,
-            Poll::Ready(val) => {
-                let val = match val {
-                    Err(join) => Err(join.into()),
-                    Ok(Err(abort)) => Err(abort.into()),
-                    Ok(Ok(t)) => Ok(t),
-                };
-                Poll::Ready(val)
-            },
-        }
-    }
-}
-
-/// Management of "subroutine" tasks.
-#[allow(clippy::missing_docs_in_private_items)]
-struct Subroutines {
-    pending_tasks: FuturesUnordered<SpawnAbortable<()>>,
-
-    state: State,
-    store: kv::Store,
-    waiting_room: Shared<WaitingRoom<Instant, Duration>>,
-    run_state: RunState,
-
-    protocol_events: BoxStream<'static, ProtocolEvent<Gossip>>,
-
-    announce_timer: Interval,
-    ping_timer: Interval,
-
-    subscriber: broadcast::Sender<Event>,
-
-    request_queries: waiting_room::stream::Queries,
-    request_clones: waiting_room::stream::Clones,
-
-    announce_sender: mpsc::Sender<AnnounceEvent>,
-    peer_sync_sender: mpsc::Sender<SyncEvent>,
-    timeout_sender: mpsc::Sender<TimeoutEvent>,
-    request_sender: mpsc::Sender<RequestEvent>,
-
-    announcements: mpsc::Receiver<AnnounceEvent>,
-    peer_syncs: mpsc::Receiver<SyncEvent>,
-    timeouts: mpsc::Receiver<TimeoutEvent>,
-    requests: mpsc::Receiver<RequestEvent>,
-}
-
-impl Subroutines {
-    /// Constructor.
-    pub fn new(
-        state: State,
-        store: kv::Store,
-        waiting_room: Shared<WaitingRoom<Instant, Duration>>,
-        run_config: RunConfig,
-        protocol_events: BoxStream<'static, ProtocolEvent<Gossip>>,
-        subscriber: broadcast::Sender<Event>,
-    ) -> Self {
-        let announce_timer = interval(run_config.announce.interval);
-        let ping_timer = interval(PING_PERIOD);
-
-        let (announce_sender, announcements) = mpsc::channel::<AnnounceEvent>(RECEIVER_CAPACITY);
-        let (peer_sync_sender, peer_syncs) = mpsc::channel::<SyncEvent>(RECEIVER_CAPACITY);
-        let (timeout_sender, timeouts) = mpsc::channel::<TimeoutEvent>(RECEIVER_CAPACITY);
-        let (request_sender, requests) = mpsc::channel::<RequestEvent>(RECEIVER_CAPACITY);
-
-        let run_state = RunState::from(run_config);
-
-        let request_queries = waiting_room::stream::Queries::new(waiting_room.clone().value);
-        let request_clones = waiting_room::stream::Clones::new(waiting_room.clone().value);
-
-        Self {
-            pending_tasks: FuturesUnordered::new(),
-
-            state,
-            store,
-            waiting_room,
-            run_state,
-
-            protocol_events: protocol_events.boxed(),
-
-            announce_timer,
-            ping_timer,
-
-            subscriber,
-
-            request_queries,
-            request_clones,
-
-            announce_sender,
-            peer_sync_sender,
-            timeout_sender,
-            request_sender,
-
-            announcements,
-            peer_syncs,
-            timeouts,
-            requests,
-        }
-    }
-}
-
-impl Drop for Subroutines {
-    fn drop(&mut self) {
-        for task in self.pending_tasks.iter_mut() {
-            task.abort()
-        }
-    }
-}
-
-impl Future for Subroutines {
-    type Output = Result<(), SpawnAbortableError>;
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        // Drain the task queue
-        loop {
-            match self.pending_tasks.poll_next_unpin(cx) {
-                Poll::Ready(Some(Err(e))) => {
-                    log::warn!("error in spawned subroutine task: {:?}", e);
-                    return Poll::Ready(Err(e));
-                },
-                Poll::Ready(Some(Ok(()))) => continue,
-                // Either pending, or FuturesUnordered thinks it's done, but
-                // we'll enqueue new tasks below
-                Poll::Ready(None) | Poll::Pending => break,
-            }
-        }
-
-        // Collect any task results, and enqueue new tasks if applicable
-        let mut events = Vec::with_capacity(9);
-        {
-            if let Poll::Ready(Some(protocol_event)) = self.protocol_events.poll_next_unpin(cx) {
-                events.push(Event::Protocol(protocol_event));
-            }
-
-            if let Poll::Ready(Some(_)) = self.announce_timer.poll_next_unpin(cx) {
-                events.push(Event::Announce(AnnounceEvent::Tick));
-            }
-
-            if let Poll::Ready(Some(_)) = self.ping_timer.poll_next_unpin(cx) {
-                events.push(Event::Ping);
-            }
-
-            if let Poll::Ready(Some(announce_event)) = self.announcements.poll_recv(cx) {
-                events.push(Event::Announce(announce_event));
-            }
-
-            if let Poll::Ready(Some(sync_event)) = self.peer_syncs.poll_recv(cx) {
-                events.push(Event::PeerSync(sync_event));
-            }
-
-            if let Poll::Ready(Some(timeout_event)) = self.timeouts.poll_recv(cx) {
-                events.push(Event::Timeout(timeout_event));
-            }
-
-            if let Poll::Ready(Some(urn)) = self.request_queries.poll_next_unpin(cx) {
-                events.push(Event::Request(RequestEvent::Query(urn)));
-            }
-
-            if let Poll::Ready(Some(url)) = self.request_clones.poll_next_unpin(cx) {
-                events.push(Event::Request(RequestEvent::Clone(url)));
-            }
-
-            if let Poll::Ready(Some(request_event)) = self.requests.poll_next_unpin(cx) {
-                events.push(Event::Request(request_event));
-            }
-        }
-
-        for event in events {
-            log::debug!("handling subroutine event: {:?}", event);
-
-            // Ignore if there are no subscribers
-            self.subscriber.send(event.clone()).ok();
-
-            for cmd in self.run_state.transition(&event) {
-                let task = match cmd {
-                    Command::Announce => SpawnAbortable::new(announce(
-                        self.state.clone(),
-                        self.store.clone(),
-                        self.announce_sender.clone(),
-                    )),
-                    Command::SyncPeer(peer_id) => SpawnAbortable::new(sync(
-                        self.state.clone(),
-                        peer_id,
-                        self.peer_sync_sender.clone(),
-                    )),
-                    Command::StartSyncTimeout(sync_period) => SpawnAbortable::new(
-                        start_sync_timeout(sync_period, self.timeout_sender.clone()),
-                    ),
-                    Command::Request(RequestCommand::Query(urn)) => SpawnAbortable::new(query(
-                        urn,
-                        self.state.clone(),
-                        self.waiting_room.clone(),
-                    )),
-                    Command::Request(RequestCommand::Found(url)) => {
-                        SpawnAbortable::new(found(url, self.waiting_room.clone()))
-                    },
-                    Command::Request(RequestCommand::Clone(url)) => SpawnAbortable::new(clone(
-                        url,
-                        self.state.clone(),
-                        self.waiting_room.clone(),
-                        self.request_sender.clone(),
-                    )),
-                };
-
-                self.pending_tasks.push(task);
-            }
-        }
-
-        // We're never done
-        Poll::Pending
-    }
-}
-
-/// Announcement subroutine.
-async fn announce(state: State, store: kv::Store, mut sender: mpsc::Sender<AnnounceEvent>) {
-    match announcement::run(&state, &store).await {
-        Ok(updates) => {
-            sender.send(AnnounceEvent::Succeeded(updates)).await.ok();
-        },
-        Err(err) => {
-            log::error!("announce error: {:?}", err);
-            sender.send(AnnounceEvent::Failed).await.ok();
-        },
-    }
-}
-
-/// Peer syncing subroutine.
-async fn sync(state: State, peer_id: PeerId, mut sender: mpsc::Sender<SyncEvent>) {
-    sender.send(SyncEvent::Started(peer_id)).await.ok();
-
-    match sync::sync(&state, peer_id).await {
-        Ok(_) => {
-            sender.send(SyncEvent::Succeeded(peer_id)).await.ok();
-        },
-        Err(err) => {
-            log::error!("sync error for {}: {:?}", peer_id, err);
-            sender.send(SyncEvent::Failed(peer_id)).await.ok();
-        },
-    }
-}
-
-/// Sync timeout subroutine.
-async fn start_sync_timeout(sync_period: Duration, mut sender: mpsc::Sender<TimeoutEvent>) {
-    tokio::time::delay_for(sync_period).await;
-    sender.send(TimeoutEvent::SyncPeriod).await.ok();
-}
-
-/// Query subroutine.
-async fn query(urn: RadUrn, state: State, waiting_room: Shared<WaitingRoom<Instant, Duration>>) {
-    request::query(urn.clone(), state, waiting_room)
-        .await
-        .unwrap_or_else(|err| {
-            log::warn!(
-                "an error occurred for the command 'Query' for the URN '{}':\n{}",
-                urn,
-                err
-            );
-        });
-}
-
-/// Found subroutine.
-async fn found(url: RadUrl, waiting_room: Shared<WaitingRoom<Instant, Duration>>) {
-    request::found(url.clone(), waiting_room)
-        .await
-        .unwrap_or_else(|err| {
-            log::warn!(
-                "an error occurred for the command 'Found' for the URL '{}':\n{}",
-                url,
-                err
-            );
-        });
-}
-
-/// Clone subroutine.
-async fn clone(
-    url: RadUrl,
-    state: State,
-    waiting_room: Shared<WaitingRoom<Instant, Duration>>,
-    mut sender: mpsc::Sender<RequestEvent>,
-) {
-    match request::clone(url.clone(), state, waiting_room).await {
-        Ok(()) => {
-            sender.send(RequestEvent::Cloned(url)).await.ok();
-        },
-        Err(err) => {
-            log::warn!(
-                "an error occurred for the command 'Clone' for the URL '{}':\n{}",
-                url,
-                err
-            );
-            sender
-                .send(RequestEvent::Failed {
-                    url,
-                    reason: err.to_string(),
-                })
-                .await
-                .ok();
-        },
     }
 }

proxy/coco/src/peer/announcement.rs
@@ -4,13 +4,11 @@ use std::{collections::HashSet, ops::Deref as _};
 
 use kv::Codec as _;
 
-use librad::{
-    net::peer::{Gossip, Rev},
-    uri::{path::ParseError, Path, RadUrn},
-};
+use librad::uri::{path::ParseError, Path, RadUrn};
 
 use crate::{
     oid::Oid,
+    peer::gossip,
     state::{self, State},
 };
 
@@ -47,14 +45,7 @@ pub type Updates = HashSet<Announcement>;
 /// * if the announcemnet of one of the project heads failed
 pub async fn announce(state: &State, updates: impl Iterator<Item = &Announcement> + Send) {
     for (urn, hash) in updates {
-        let protocol = state.api.protocol();
-
-        let have = Gossip {
-            urn: urn.clone(),
-            rev: Some(Rev::Git((*hash).into())),
-            origin: None,
-        };
-        protocol.announce(have).await;
+        gossip::announce(state, urn, hash).await;
     }
 }
 

commit 29ac0b0d1635fd2dc550c39f72f47c22f807094f

fix(proxy): await request urn
Regression from #992 where the future of the call was not awaited,
unfortunately our tests didn't break.

proxy/api/src/context.rs
@@ -39,7 +39,10 @@ impl Context {
                 coco::into_peer_state(config, signer.clone(), store.clone(), RunConfig::default())
                     .await?;
 
-            (peer.control(), state)
+            let peer_control = peer.control();
+            tokio::spawn(peer.into_running());
+
+            (peer_control, state)
         };
 
         Ok(Self {

proxy/api/src/http/project.rs
@@ -188,7 +188,7 @@ mod handler {
     pub async fn request(ctx: context::Context, urn: coco::Urn) -> Result<impl Reply, Rejection> {
         let mut peer_control = ctx.peer_control;
         // TODO(finto): Check the request exists in the monorepo
-        let _request = peer_control.request_urn(&urn, Instant::now());
+        let _request = peer_control.request_urn(&urn, Instant::now()).await;
 
         // TODO(finto): Serialise request and respond with that.
         Ok(reply::json(&true))