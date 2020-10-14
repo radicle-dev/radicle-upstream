e4c2c6ab9f83c453852f9e6720b2de496834f9e8
Alexander Simmerl <a.simmerl@gmail.com>
2020-10-14 12:33:30 +0200

    Add test for serialisation

--- proxy/api/src/http/notification.rs
+++ proxy/api/src/http/notification.rs
@@ -8,8 +8,6 @@ use crate::{context, http, notification::Subscriptions};
 /// SSE based notifications endpoint.
 pub fn filters(ctx: context::Context, subscriptions: Subscriptions) -> BoxedFilter<(impl Reply,)> {
     local_peer_status_stream(ctx, subscriptions.clone())
-        .or(stream_filter(subscriptions))
-        .boxed()
 }
 
 /// `GET /local_peer_status`
@@ -21,14 +19,6 @@ pub fn local_peer_status_stream(
         .and(http::with_context(ctx))
         .and(warp::any().map(move || subscriptions.clone()))
         .and_then(handler::local_peer_status)
-        .boxed()
-}
-
-/// `GET /`
-pub fn stream_filter(subscriptions: Subscriptions) -> BoxedFilter<(impl Reply,)> {
-    warp::get()
-        .and(warp::any().map(move || subscriptions.clone()))
-        .and_then(handler::stream)
         .boxed()
 }
 
@@ -61,7 +51,7 @@ mod handler {
             match notification.clone() {
                 Notification::LocalPeerStatusChanged(_old, _new) => {
                     Some(map_to_event(notification))
-                },
+                }
                 _ => None,
             }
         };
@@ -71,67 +61,33 @@ mod handler {
         ))
     }
 
-    /// Provides an SSE events endpoint providing a stream of
-    /// [`crate::notification::Notification`]s.
-    pub async fn stream(subscriptions: Subscriptions) -> Result<impl Reply, Rejection> {
-        let subscriber = subscriptions.subscribe().await;
-
-        Ok(sse::reply(sse::keep_alive().stream(map(subscriber))))
-    }
-
-    /// Maps the [`crate::notification::Notification`] from a subscription to an
-    /// [`sse::ServerSideEvent`].
-    fn map(
-        subscriber: mpsc::UnboundedReceiver<Notification>,
-    ) -> impl Stream<Item = Result<impl sse::ServerSentEvent, Infallible>> {
-        subscriber.map(|notification| match notification {
-            Notification::LocalPeerStatusChanged(_old, new) => {
-                Ok((sse::event("LOCAL_PEER_STATUS_CHANGED"), sse::json(new)))
-            },
-        })
-    }
-
     fn map_to_event(notification: Notification) -> Result<impl sse::ServerSentEvent, Infallible> {
         match notification {
             Notification::LocalPeerStatusChanged(_old, new) => {
                 Ok((sse::event("LOCAL_PEER_STATUS_CHANGED"), sse::json(new)))
-            },
+            }
         }
     }
-}
 
-#[cfg(test)]
-mod test {
-    use pretty_assertions::assert_eq;
-    use warp::test::request;
+    #[cfg(test)]
+    mod test {
+        use std::time::Instant;
 
-    use crate::{context, notification};
+        use warp::filters::sse;
 
-    /// This test blocks as we don't have a termination condition for the stream. We need to find
-    /// a way to test this properly. Warp does have test utility for websockets but not for SSE
-    /// streams.
-    #[ignore]
-    #[tokio::test]
-    async fn stream() -> Result<(), Box<dyn std::error::Error>> {
-        let tmp_dir = tempfile::tempdir()?;
-        let ctx = context::Context::tmp(&tmp_dir).await?;
-        let subscriptions = notification::Subscriptions::default();
-        let api = super::filters(ctx, subscriptions);
+        use crate::notification;
 
-        let res = request()
-            .method("GET")
-            .path("/")
-            .header("Connection", "Keep-Alive")
-            .reply(&api)
-            .await
-            .into_body();
+        #[test]
+        fn json_serialize() -> Result<(), Box<dyn std::error::Error>> {
+            sse::json(coco::PeerStatus::Started(Instant::now()));
 
-        assert_eq!(
-            res,
-            r#"data:"foo"
-"#
-        );
+            super::map_to_event(notification::Notification::LocalPeerStatusChanged(
+                coco::PeerStatus::Stopped(Instant::now()),
+                coco::PeerStatus::Started(Instant::now()),
+            ))
+            .unwrap();
 
-        Ok(())
+            Ok(())
+        }
     }
 }
