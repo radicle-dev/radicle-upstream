77326ba95b087305a662e6323bf4187b86f6236d
Rūdolfs Ošiņš <rudolfs@osins.org>
2020-10-15 14:37:47 +0200

    fix: fix "run all specs" from cypress UI (#1051)

--- cypress/integration/user_profile.spec.js
+++ cypress/integration/user_profile.spec.js
@@ -1,28 +1,30 @@
-before(() => {
-  cy.resetAllState();
-  cy.onboardUser("cloudhead");
-  cy.createProjectWithFixture("platinum", "Best project ever.", "master", [
-    "ele",
-    "abbey",
-  ]);
-});
+context("user profile", () => {
+  before(() => {
+    cy.resetAllState();
+    cy.onboardUser("cloudhead");
+    cy.createProjectWithFixture("platinum", "Best project ever.", "master", [
+      "ele",
+      "abbey",
+    ]);
+  });
 
-context("visitor view profile page", () => {
-  // TODO(sos): unskip when we have a proxy testnet
-  it.skip("opens from the peer selector with the correct data", () => {
-    // Go to the project source page
-    cy.visit("./public/index.html#/profile/projects");
-    cy.contains("platinum").click();
-    cy.contains("Source").click();
+  context("visitor view profile page", () => {
+    // TODO(sos): unskip when we have a proxy testnet
+    it.skip("opens from the peer selector with the correct data", () => {
+      // Go to the project source page
+      cy.visit("./public/index.html#/profile/projects");
+      cy.contains("platinum").click();
+      cy.contains("Source").click();
 
-    // Pick a user from the peer selector
-    cy.pick("peer-selector").click();
-    cy.get(".peer-dropdown").pick("abbey").click();
+      // Pick a user from the peer selector
+      cy.pick("peer-selector").click();
+      cy.get(".peer-dropdown").pick("abbey").click();
 
-    cy.pick("header").should("exist");
+      cy.pick("header").should("exist");
 
-    // Check for the correct data
-    cy.pick("entity-name").contains("abbey");
-    cy.pick("project-list").contains("platinum").should("exist");
+      // Check for the correct data
+      cy.pick("entity-name").contains("abbey");
+      cy.pick("project-list").contains("platinum").should("exist");
+    });
   });
 });
