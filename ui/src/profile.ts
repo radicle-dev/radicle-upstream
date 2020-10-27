radicle-dev/radicle-upstream

commit 7377dbfd889b118f5a80feb4591c434637c10fe8

feat: cancel project search (#1105)
Exposes an endpoint to list existing requests for projects in
preparation for a deeper UI integration.

Part of #984

Co-authored-by: Merle Breitkreuz <merle@monadic.xyz>

ui/src/profile.ts
@@ -12,12 +12,9 @@ interface Following {
 
 // STATE
 const followingProjectsStore = remote.createStore<project.Project[]>();
-export const followingProjects = followingProjectsStore.readable;
-
 const requestedProjectsStore = remote.createStore<
   waitingRoom.ProjectRequest[]
 >();
-export const requestedProjects = requestedProjectsStore.readable;
 
 export const following: Readable<remote.Data<Following | null>> = derived(
   [followingProjectsStore, requestedProjectsStore],
@@ -44,8 +41,15 @@ export const following: Readable<remote.Data<Following | null>> = derived(
       requests.status === remote.Status.Success
     ) {
       let data = null;
-      if (follows.data.length > 0 || requests.data.length > 0) {
-        data = { follows: follows.data, requests: requests.data };
+      const reqs = requests.data.filter(
+        req => req.type !== waitingRoom.Status.Cancelled
+      );
+
+      if (follows.data.length > 1 || reqs.length > 0) {
+        data = {
+          follows: follows.data,
+          requests: reqs,
+        };
       }
       return { status: remote.Status.Success, data };
     }

proxy/api/src/error.rs
@@ -20,4 +20,8 @@ pub enum Error {
     /// Issues when access persistent storage.
     #[error(transparent)]
     Store(#[from] kv::Error),
+
+    /// Errors stemming from [`coco::request::waiting_room::WaitingRoom`] interactions.
+    #[error(transparent)]
+    WaitingRoom(#[from] coco::request::waiting_room::Error),
 }

proxy/api/src/http/project.rs
@@ -7,6 +7,8 @@ use warp::{filters::BoxedFilter, path, Filter, Rejection, Reply};
 
 use crate::{context, http};
 
+mod request;
+
 /// Combination of all routes.
 pub fn filters(ctx: context::Context) -> BoxedFilter<(impl Reply,)> {
     checkout_filter(ctx.clone())
@@ -14,10 +16,9 @@ pub fn filters(ctx: context::Context) -> BoxedFilter<(impl Reply,)> {
         .or(discover_filter(ctx.clone()))
         .or(get_filter(ctx.clone()))
         .or(owner_contributed_filter(ctx.clone()))
-        .or(owner_requested_filter(ctx.clone()))
         .or(owner_tracked_filter(ctx.clone()))
         .or(peers_filter(ctx.clone()))
-        .or(request_filter(ctx.clone()))
+        .or(path("requests").and(request::filters(ctx.clone())))
         .or(track_filter(ctx.clone()))
         .or(user_filter(ctx.clone()))
         .or(track_filter(ctx.clone()))
@@ -81,17 +82,6 @@ fn owner_contributed_filter(
         .and_then(handler::list_owner_contributed)
 }
 
-/// `GET /requested`
-fn owner_requested_filter(
-    ctx: context::Context,
-) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    http::with_context(ctx)
-        .and(warp::get())
-        .and(path("requests"))
-        .and(path::end())
-        .and_then(handler::list_owner_requested)
-}
-
 /// `GET /tracked`
 fn owner_tracked_filter(
     ctx: context::Context,
@@ -113,18 +103,6 @@ fn peers_filter(
         .and(path("peers"))
         .and(path::end())
         .and_then(handler::peers)
-}
-
-/// `PUT /request/<urn>`
-fn request_filter(
-    ctx: context::Context,
-) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    http::with_context(ctx)
-        .and(warp::put())
-        .and(path("requests"))
-        .and(path::param::<coco::Urn>())
-        .and(path::end())
-        .and_then(handler::request)
 }
 
 /// `PUT /<urn>/track/<peer_id>`
@@ -168,7 +146,7 @@ fn user_filter(
 /// Project handlers to implement conversion and translation between core domain and http request
 /// fullfilment.
 mod handler {
-    use std::{path::PathBuf, time::Instant};
+    use std::path::PathBuf;
 
     use warp::{http::StatusCode, reply, Rejection, Reply};
 
@@ -241,13 +219,6 @@ mod handler {
         Ok(reply::json(&projects.contributed))
     }
 
-    /// List all project requests the current user has issued.
-    pub async fn list_owner_requested(mut ctx: context::Context) -> Result<impl Reply, Rejection> {
-        let requests = ctx.peer_control.get_project_requests().await;
-
-        Ok(reply::json(&requests))
-    }
-
     /// List all projects tracked by the current user.
     pub async fn list_owner_tracked(ctx: context::Context) -> Result<impl Reply, Rejection> {
         let projects = project::Projects::list(&ctx.state).await?.tracked;
@@ -280,16 +251,6 @@ mod handler {
             .collect::<Vec<_>>();
 
         Ok(reply::json(&peers))
-    }
-
-    /// Kick off a network request for the [`project::Project`] of the given `id`.
-    pub async fn request(
-        mut ctx: context::Context,
-        urn: coco::Urn,
-    ) -> Result<impl Reply, Rejection> {
-        let request = ctx.peer_control.request_urn(&urn, Instant::now()).await;
-
-        Ok(reply::json(&request))
     }
 
     /// Track the peer for the provided project.
@@ -349,8 +310,6 @@ pub struct MetadataInput {
 #[allow(clippy::panic, clippy::unwrap_used)]
 #[cfg(test)]
 mod test {
-    use std::time::Instant;
-
     use pretty_assertions::assert_eq;
     use serde_json::{json, Value};
     use warp::{http::StatusCode, test::request};
@@ -591,54 +550,6 @@ mod test {
 
         http::test::assert_response(&res, StatusCode::OK, |have| {
             assert_eq!(have, json!(project));
-        });
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn request_project() -> Result<(), Box<dyn std::error::Error>> {
-        let tmp_dir = tempfile::tempdir()?;
-        let mut ctx = context::Context::tmp(&tmp_dir).await?;
-        let api = super::filters(ctx.clone());
-
-        let urn = coco::Urn::new(
-            coco::Hash::hash(b"kisses-of-the-sun"),
-            coco::uri::Protocol::Git,
-            coco::uri::Path::empty(),
-        );
-
-        let res = request()
-            .method("PUT")
-            .path(&format!("/requests/{}", urn))
-            .reply(&api)
-            .await;
-        let want = ctx.peer_control.get_project_request(&urn).await;
-
-        http::test::assert_response(&res, StatusCode::OK, |have| {
-            assert_eq!(have, json!(want));
-        });
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn list_requested() -> Result<(), Box<dyn std::error::Error>> {
-        let tmp_dir = tempfile::tempdir()?;
-        let mut ctx = context::Context::tmp(&tmp_dir).await?;
-        let api = super::filters(ctx.clone());
-
-        let urn = coco::Urn::new(
-            coco::Hash::hash(b"kisses-of-the-sun"),
-            coco::uri::Protocol::Git,
-            coco::uri::Path::empty(),
-        );
-
-        let want = ctx.peer_control.request_urn(&urn, Instant::now()).await;
-        let res = request().method("GET").path("/requests").reply(&api).await;
-
-        http::test::assert_response(&res, StatusCode::OK, |have| {
-            assert_eq!(have, json!([want]));
         });
 
         Ok(())

proxy/api/src/http/project/request.rs
@@ -1,0 +1,170 @@
+//! Endpoints for project search requests.
+
+use warp::{filters::BoxedFilter, path, Filter, Rejection, Reply};
+
+use crate::{context, http};
+
+/// Combination of all routes.
+pub fn filters(ctx: context::Context) -> BoxedFilter<(impl Reply,)> {
+    cancel_filter(ctx.clone())
+        .or(create_filter(ctx.clone()))
+        .or(list_filter(ctx))
+        .boxed()
+}
+
+/// `DELETE /<urn>`
+fn cancel_filter(
+    ctx: context::Context,
+) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    http::with_context(ctx)
+        .and(warp::delete())
+        .and(path::param::<coco::Urn>())
+        .and(path::end())
+        .and_then(handler::cancel)
+}
+
+/// `PUT /<urn>`
+fn create_filter(
+    ctx: context::Context,
+) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    http::with_context(ctx)
+        .and(warp::put())
+        .and(path::param::<coco::Urn>())
+        .and(path::end())
+        .and_then(handler::create)
+}
+
+/// `GET /`
+fn list_filter(
+    ctx: context::Context,
+) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    http::with_context(ctx)
+        .and(warp::get())
+        .and(path::end())
+        .and_then(handler::list)
+}
+
+/// Request handlers for initiating searches for projects on the network.
+mod handler {
+    use std::time::Instant;
+
+    use warp::{http::StatusCode, reply, Rejection, Reply};
+
+    use crate::{context, error};
+
+    /// Abort search for an ongoing request.
+    pub async fn cancel(
+        mut ctx: context::Context,
+        urn: coco::Urn,
+    ) -> Result<impl Reply, Rejection> {
+        ctx.peer_control
+            .cancel_project_request(&urn, Instant::now())
+            .await
+            .map_err(error::Error::from)?;
+
+        Ok(reply::with_status(reply(), StatusCode::NO_CONTENT))
+    }
+
+    /// Kick off a network request for the [`crate::project::Project`] of the given `id`.
+    ///
+    /// FIXME(xla): Endpoint ought to return `201` if the request was newly created, otherwise
+    /// `200` if there was a request present for the urn.
+    pub async fn create(
+        mut ctx: context::Context,
+        urn: coco::Urn,
+    ) -> Result<impl Reply, Rejection> {
+        let request = ctx.peer_control.request_project(&urn, Instant::now()).await;
+
+        Ok(reply::json(&request))
+    }
+
+    /// List all project requests the current user has issued.
+    pub async fn list(mut ctx: context::Context) -> Result<impl Reply, Rejection> {
+        let requests = ctx.peer_control.get_project_requests().await;
+
+        Ok(reply::json(&requests))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::time::Instant;
+
+    use pretty_assertions::assert_eq;
+    use serde_json::json;
+    use warp::{http::StatusCode, test::request};
+
+    use crate::{context, http};
+
+    #[tokio::test]
+    async fn cancel() -> Result<(), Box<dyn std::error::Error>> {
+        let tmp_dir = tempfile::tempdir()?;
+        let mut ctx = context::Context::tmp(&tmp_dir).await?;
+        let api = super::filters(ctx.clone());
+
+        let urn = coco::Urn::new(
+            coco::Hash::hash(b"kisses-of-the-sun"),
+            coco::uri::Protocol::Git,
+            coco::uri::Path::empty(),
+        );
+
+        let _request = ctx.peer_control.request_project(&urn, Instant::now()).await;
+        let res = request()
+            .method("DELETE")
+            .path(&format!("/{}", urn))
+            .reply(&api)
+            .await;
+
+        assert_eq!(res.status(), StatusCode::NO_CONTENT);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn create() -> Result<(), Box<dyn std::error::Error>> {
+        let tmp_dir = tempfile::tempdir()?;
+        let mut ctx = context::Context::tmp(&tmp_dir).await?;
+        let api = super::filters(ctx.clone());
+
+        let urn = coco::Urn::new(
+            coco::Hash::hash(b"kisses-of-the-sun"),
+            coco::uri::Protocol::Git,
+            coco::uri::Path::empty(),
+        );
+
+        let res = request()
+            .method("PUT")
+            .path(&format!("/{}", urn))
+            .reply(&api)
+            .await;
+        let want = ctx.peer_control.get_project_request(&urn).await;
+
+        http::test::assert_response(&res, StatusCode::OK, |have| {
+            assert_eq!(have, json!(want));
+        });
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn list() -> Result<(), Box<dyn std::error::Error>> {
+        let tmp_dir = tempfile::tempdir()?;
+        let mut ctx = context::Context::tmp(&tmp_dir).await?;
+        let api = super::filters(ctx.clone());
+
+        let urn = coco::Urn::new(
+            coco::Hash::hash(b"kisses-of-the-sun"),
+            coco::uri::Protocol::Git,
+            coco::uri::Path::empty(),
+        );
+
+        let want = ctx.peer_control.request_project(&urn, Instant::now()).await;
+        let res = request().method("GET").path("/").reply(&api).await;
+
+        http::test::assert_response(&res, StatusCode::OK, |have| {
+            assert_eq!(have, json!([want]));
+        });
+
+        Ok(())
+    }
+}

proxy/coco/src/peer/control.rs
@@ -15,17 +15,24 @@ use super::run_state::Status;
 #[allow(clippy::pub_enum_variant_names)]
 #[derive(Debug)]
 pub enum Request {
-    /// Request the current status.
+    /// Request the current peer status.
     CurrentStatus(oneshot::Sender<Status>),
-    /// Request a project request.
-    GetProjectRequest(
+
+    /// Cancel an ongoing project search.
+    CancelSearch(
+        RadUrn,
+        Instant,
+        oneshot::Sender<Result<(), waiting_room::Error>>,
+    ),
+    /// Get a project search.
+    GetSearch(
         RadUrn,
         oneshot::Sender<Option<request::SomeRequest<Instant>>>,
     ),
-    /// Request the current project requests.
-    GetProjectRequests(oneshot::Sender<Vec<request::SomeRequest<Instant>>>),
-    /// Request a urn to be fetched from the network.
-    Urn(
+    /// List all project searches.
+    ListSearches(oneshot::Sender<Vec<request::SomeRequest<Instant>>>),
+    /// Initiate a search for a project on the network.
+    StartSearch(
         RadUrn,
         Instant,
         oneshot::Sender<waiting_room::Created<Instant>>,
@@ -37,18 +44,24 @@ pub enum Request {
 pub enum Response {
     /// Response to a status request.
     CurrentStatus(oneshot::Sender<Status>, Status),
-    /// Response to get project request request.
-    GetProjectRequest(
+
+    /// Response to a cancel project search request.
+    CancelSearch(
+        oneshot::Sender<Result<(), waiting_room::Error>>,
+        Result<(), waiting_room::Error>,
+    ),
+    /// Response to get project search request.
+    GetSearch(
         oneshot::Sender<Option<request::SomeRequest<Instant>>>,
         Option<request::SomeRequest<Instant>>,
     ),
-    /// Response to list project requests request.
-    GetProjectRequests(
+    /// Response to list project searches request.
+    ListSearches(
         oneshot::Sender<Vec<request::SomeRequest<Instant>>>,
         Vec<request::SomeRequest<Instant>>,
     ),
-    /// Response to a urn request.
-    Urn(
+    /// Response to a start project search request.
+    StartSearch(
         oneshot::Sender<waiting_room::Created<Instant>>,
         waiting_room::Created<Instant>,
     ),
@@ -80,7 +93,27 @@ impl Control {
         receiver.await.expect("receiver is gone")
     }
 
-    /// Initiate a new request to fetch a project request.
+    /// Cancel an ongoing search for a project.
+    ///
+    /// # Errors
+    ///
+    /// * if the waiting room returns an error
+    pub async fn cancel_project_request(
+        &mut self,
+        urn: &RadUrn,
+        timestamp: Instant,
+    ) -> Result<(), waiting_room::Error> {
+        let (sender, receiver) = oneshot::channel::<Result<(), waiting_room::Error>>();
+
+        self.sender
+            .send(Request::CancelSearch(urn.clone(), timestamp, sender))
+            .await
+            .expect("peer is gone");
+
+        receiver.await.expect("receiver is gone")
+    }
+
+    /// Initiate a new request to fetch a project from the network.
     pub async fn get_project_request(
         &mut self,
         urn: &RadUrn,
@@ -88,7 +121,7 @@ impl Control {
         let (sender, receiver) = oneshot::channel::<Option<request::SomeRequest<Instant>>>();
 
         self.sender
-            .send(Request::GetProjectRequest(urn.clone(), sender))
+            .send(Request::GetSearch(urn.clone(), sender))
             .await
             .expect("peer is gone");
 
@@ -100,7 +133,7 @@ impl Control {
         let (sender, receiver) = oneshot::channel::<Vec<request::SomeRequest<Instant>>>();
 
         self.sender
-            .send(Request::GetProjectRequests(sender))
+            .send(Request::ListSearches(sender))
             .await
             .expect("peer is gone");
 
@@ -108,15 +141,15 @@ impl Control {
     }
 
     /// Initiate a new request for the `urn`.
-    pub async fn request_urn(
+    pub async fn request_project(
         &mut self,
         urn: &RadUrn,
-        time: Instant,
+        timestamp: Instant,
     ) -> request::SomeRequest<Instant> {
         let (sender, receiver) = oneshot::channel::<waiting_room::Created<Instant>>();
 
         self.sender
-            .send(Request::Urn(urn.clone(), time, sender))
+            .send(Request::StartSearch(urn.clone(), timestamp, sender))
             .await
             .expect("peer is gone");
 

proxy/coco/src/peer/run_state.rs
@@ -157,12 +157,25 @@ pub enum AnnounceInput {
 /// Requests from the peer control.
 #[derive(Debug)]
 pub enum ControlInput {
-    /// Request a project request.
-    ProjectRequest(RadUrn, oneshot::Sender<Option<SomeRequest<Instant>>>),
-    /// Request the list of project requests.
-    ProjectRequests(oneshot::Sender<Vec<SomeRequest<Instant>>>),
     /// New status.
     Status(oneshot::Sender<Status>),
+
+    /// Cancel an ongoing project search.
+    CancelRequest(
+        RadUrn,
+        Instant,
+        oneshot::Sender<Result<(), waiting_room::Error>>,
+    ),
+    /// Initiate a new project search on the network.
+    CreateRequest(
+        RadUrn,
+        Instant,
+        Option<oneshot::Sender<waiting_room::Created<Instant>>>,
+    ),
+    /// Request a project search.
+    GetRequest(RadUrn, oneshot::Sender<Option<SomeRequest<Instant>>>),
+    /// Request the list of project searches.
+    ListRequests(oneshot::Sender<Vec<SomeRequest<Instant>>>),
 }
 
 /// Request event for projects requested from the network.
@@ -181,12 +194,6 @@ pub enum RequestInput {
     },
     /// Query the network for the `RadUrn`.
     Queried(RadUrn),
-    /// A urn was requested.
-    Requested(
-        RadUrn,
-        Instant,
-        Option<oneshot::Sender<waiting_room::Created<Instant>>>,
-    ),
     /// [`WaitingRoom`] query interval.
     Tick,
     /// The request for [`RadUrn`] timed out.
@@ -391,23 +398,38 @@ impl RunState {
     }
 
     /// Handle [`ControlInput`]s.
-    fn handle_control(&self, input: ControlInput) -> Vec<Command> {
+    fn handle_control(&mut self, input: ControlInput) -> Vec<Command> {
         match input {
-            ControlInput::ProjectRequest(urn, sender) => vec![Command::Control(
-                ControlCommand::Respond(control::Response::GetProjectRequest(
+            ControlInput::CancelRequest(urn, timestamp, sender) => vec![Command::Control(
+                ControlCommand::Respond(control::Response::CancelSearch(
                     sender,
-                    self.waiting_room.get(&urn).cloned(),
+                    self.waiting_room.canceled(&urn, timestamp),
                 )),
             )],
-            ControlInput::ProjectRequests(sender) => vec![Command::Control(
-                ControlCommand::Respond(control::Response::GetProjectRequests(
+            ControlInput::CreateRequest(urn, time, maybe_sender) => {
+                let request = self.waiting_room.request(&urn, time);
+                if let Some(sender) = maybe_sender {
+                    vec![Command::Control(ControlCommand::Respond(
+                        control::Response::StartSearch(sender, request),
+                    ))]
+                } else {
+                    vec![]
+                }
+            },
+            ControlInput::GetRequest(urn, sender) => {
+                vec![Command::Control(ControlCommand::Respond(
+                    control::Response::GetSearch(sender, self.waiting_room.get(&urn).cloned()),
+                ))]
+            },
+            ControlInput::ListRequests(sender) => vec![Command::Control(ControlCommand::Respond(
+                control::Response::ListSearches(
                     sender,
                     self.waiting_room
                         .iter()
                         .map(|pair| pair.1.clone())
                         .collect::<Vec<_>>(),
-                )),
-            )],
+                ),
+            ))],
             ControlInput::Status(sender) => vec![Command::Control(ControlCommand::Respond(
                 control::Response::CurrentStatus(sender, self.status.clone()),
             ))],
@@ -625,16 +647,6 @@ impl RunState {
                     Ok(_) => vec![],
                 }
             },
-            (_, RequestInput::Requested(urn, time, maybe_sender)) => {
-                let request = self.waiting_room.request(&urn, time);
-                if let Some(sender) = maybe_sender {
-                    vec![Command::Control(ControlCommand::Respond(
-                        control::Response::Urn(sender, request),
-                    ))]
-                } else {
-                    vec![]
-                }
-            },
             _ => vec![],
         }
     }
@@ -677,8 +689,8 @@ mod test {
     };
 
     use super::{
-        AnnounceInput, Command, Config, Input, RequestCommand, RequestInput, RunState, Status,
-        SyncConfig, SyncInput, TimeoutInput, DEFAULT_SYNC_MAX_PEERS,
+        AnnounceInput, Command, Config, ControlInput, Input, RequestCommand, RequestInput,
+        RunState, Status, SyncConfig, SyncInput, TimeoutInput, DEFAULT_SYNC_MAX_PEERS,
     };
 
     #[test]
@@ -883,7 +895,7 @@ mod test {
         let status = Status::Stopped;
         let status_since = Instant::now();
         let mut state = RunState::new(Config::default(), HashMap::new(), status, status_since);
-        let cmds = state.transition(Input::Request(RequestInput::Requested(
+        let cmds = state.transition(Input::Control(ControlInput::CreateRequest(
             urn.clone(),
             Instant::now(),
             None,
@@ -893,7 +905,7 @@ mod test {
         let status = Status::Started;
         let status_since = Instant::now();
         let mut state = RunState::new(Config::default(), HashMap::new(), status, status_since);
-        let cmds = state.transition(Input::Request(RequestInput::Requested(
+        let cmds = state.transition(Input::Control(ControlInput::CreateRequest(
             urn.clone(),
             Instant::now(),
             None,
@@ -903,7 +915,7 @@ mod test {
         let status = Status::Offline;
         let status_since = Instant::now();
         let mut state = RunState::new(Config::default(), HashMap::new(), status, status_since);
-        let cmds = state.transition(Input::Request(RequestInput::Requested(
+        let cmds = state.transition(Input::Control(ControlInput::CreateRequest(
             urn.clone(),
             Instant::now(),
             None,
@@ -916,7 +928,7 @@ mod test {
         };
         let status_since = Instant::now();
         let mut state = RunState::new(Config::default(), HashMap::new(), status, status_since);
-        let cmds = state.transition(Input::Request(RequestInput::Requested(
+        let cmds = state.transition(Input::Control(ControlInput::CreateRequest(
             urn.clone(),
             Instant::now(),
             None,
@@ -932,7 +944,7 @@ mod test {
         let status = Status::Online { connected: 1 };
         let status_since = Instant::now();
         let mut state = RunState::new(Config::default(), HashMap::new(), status, status_since);
-        let cmds = state.transition(Input::Request(RequestInput::Requested(
+        let cmds = state.transition(Input::Control(ControlInput::CreateRequest(
             urn.clone(),
             Instant::now(),
             None,
@@ -965,7 +977,7 @@ mod test {
         let mut state = RunState::new(Config::default(), HashMap::new(), status, status_since);
 
         assert!(state
-            .transition(Input::Request(RequestInput::Requested(
+            .transition(Input::Control(ControlInput::CreateRequest(
                 urn.clone(),
                 Instant::now(),
                 None

proxy/coco/src/peer/subroutines.rs
@@ -91,14 +91,17 @@ impl Subroutines {
                         control::Request::CurrentStatus(sender) => {
                             Input::Control(ControlInput::Status(sender))
                         },
-                        control::Request::GetProjectRequest(urn, sender) => {
-                            Input::Control(ControlInput::ProjectRequest(urn, sender))
+                        control::Request::CancelSearch(urn, time, sender) => {
+                            Input::Control(ControlInput::CancelRequest(urn, time, sender))
                         },
-                        control::Request::GetProjectRequests(sender) => {
-                            Input::Control(ControlInput::ProjectRequests(sender))
+                        control::Request::GetSearch(urn, sender) => {
+                            Input::Control(ControlInput::GetRequest(urn, sender))
                         },
-                        control::Request::Urn(urn, time, sender) => {
-                            Input::Request(RequestInput::Requested(urn, time, Some(sender)))
+                        control::Request::ListSearches(sender) => {
+                            Input::Control(ControlInput::ListRequests(sender))
+                        },
+                        control::Request::StartSearch(urn, time, sender) => {
+                            Input::Control(ControlInput::CreateRequest(urn, time, Some(sender)))
                         },
                     })
                     .boxed(),
@@ -256,9 +259,10 @@ async fn announce(state: State, store: kv::Store, mut sender: mpsc::Sender<Input
 async fn control_respond(cmd: control::Response) {
     match cmd {
         control::Response::CurrentStatus(sender, status) => sender.send(status).ok(),
-        control::Response::GetProjectRequest(sender, request) => sender.send(request).ok(),
-        control::Response::GetProjectRequests(sender, requests) => sender.send(requests).ok(),
-        control::Response::Urn(sender, request) => sender.send(request).ok(),
+        control::Response::CancelSearch(sender, request) => sender.send(request).ok(),
+        control::Response::GetSearch(sender, request) => sender.send(request).ok(),
+        control::Response::ListSearches(sender, requests) => sender.send(requests).ok(),
+        control::Response::StartSearch(sender, request) => sender.send(request).ok(),
     };
 }
 

proxy/coco/tests/gossip.rs
@@ -155,7 +155,7 @@ async fn can_ask_and_clone_project() -> Result<(), Box<dyn std::error::Error>> {
         alice_state.init_project(&alice, project).await?.urn()
     };
 
-    bob_control.request_urn(&urn, Instant::now()).await;
+    bob_control.request_project(&urn, Instant::now()).await;
 
     requested(query_listener, &urn).await?;
     assert_cloned(clone_listener, &urn.clone().into_rad_url(alice_peer_id)).await?;

commit e410d5898893139a689052725e69d151e9009224

fix(ui): correct check for listinng
Regression from #1105 - thanks @rudolfs for the :eyes:

ui/src/profile.ts
@@ -45,7 +45,7 @@ export const following: Readable<remote.Data<Following | null>> = derived(
         req => req.type !== waitingRoom.Status.Cancelled
       );
 
-      if (follows.data.length > 1 || reqs.length > 0) {
+      if (follows.data.length > 0 || reqs.length > 0) {
         data = {
           follows: follows.data,
           requests: reqs,