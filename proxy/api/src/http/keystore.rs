982aeecdc770d9dc5a2cbac9a5c986f2b5f810a0
Thomas Scholtes <thomas@monadic.xyz>
2020-10-29 11:06:45 +0100

    Update proxy/api/src/http/keystore.rs
    Co-authored-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- proxy/api/src/http/keystore.rs
+++ proxy/api/src/http/keystore.rs
@@ -63,7 +63,7 @@ mod handler {
         .into_response())
     }
 
-    /// Initialize the key store with a new key
+    /// Initialize the keystore with a new key.
     pub async fn create(mut ctx: context::Context) -> Result<impl Reply, Rejection> {
         // TODO Load the real key from disk. The service manager ignores the key for now and uses a
         // hardcoded one.
