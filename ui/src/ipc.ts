8a387e00080d4bf07d6d7c3e54d496293a953eda
Thomas Scholtes <thomas@monadic.xyz>
2020-11-16 11:45:34 +0100

    fixup! fixup! fixup! feat(ui): Use error logging for all `catch` statements

--- ui/src/ipc.ts
+++ ui/src/ipc.ts
@@ -5,7 +5,8 @@ import * as ipcTypes from "../../native/ipc-types";
 export type { ProxyError } from "../../native/ipc-types";
 
 // `true` if we are running unit tests with jst.
-const isTestEnv = global.process && global.process.env["NODE_ENV"] === "test";
+const isTestEnv =
+  globalThis.process && globalThis.process.env["NODE_ENV"] === "test";
 
 // We have to be able to select empty directories when we create new
 // projects. Unfortunately we can't use the HTML5 open dialog via
