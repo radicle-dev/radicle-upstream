58e1c48bf403b5b93076783e36fb706b136257f3
Nuno Alexandre <hi@nunoalexandre.com>
2020-11-17 16:34:28 +0100

    Move Eth -> Radicle linking to separate component

---
+++ ui/DesignSystem/Component/Funding/Link/EthToRadicle.svelte
@@ -0,0 +1,104 @@
+<script lang="typescript">
+  import { Avatar, Button, Icon } from "../../../Primitive";
+  import { Copyable, Illustration } from "../../../Component";
+
+  import { Variant as IllustrationVariant } from "../../../../src/illustration";
+  import type { Identity } from "../../../../src/identity";
+  import * as pool from "../../../../src/funding/pool";
+
+  export let address: pool.Address;
+  export let identity: Identity;
+
+  export let onCancel: () => void;
+  export let onConfirm: () => Promise<void>;
+</script>
+
+<style>
+  .wrapper {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    min-height: 500px;
+  }
+
+  header {
+    padding: 0px var(--content-padding);
+  }
+
+  .data {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+
+    width: 100%;
+    height: 170px;
+
+    border: 1px solid var(--color-foreground-level-3);
+    background-color: var(--color-foreground-level-1);
+
+    margin-top: var(--content-padding);
+    padding: calc(var(--content-padding) / 2);
+    border-radius: 16px;
+  }
+
+  .radicle-user {
+    display: flex;
+    align-items: center;
+  }
+  .submit {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    margin-top: calc(var(--content-padding) / 2);
+  }
+</style>
+
+<div class="wrapper">
+  <Illustration variant={IllustrationVariant.Socks} />
+
+  <header>
+    <h1 style="margin-top: 1.5rem;">Add your Ethereum account to Radicle</h1>
+    <p
+      style="margin-top: 1.5rem; padding: 0 var(--content-pading)"
+      class="typo-text">
+      Are you sure you want to add your ethereum address to your Radicle
+      account?
+    </p>
+  </header>
+
+  <div class="data">
+    <p class="address typo-text">
+      <Copyable
+        showIcon={false}
+        styleContent={false}
+        copyContent={address}
+        notificationText="Address copied to the clipboard">
+        {address}
+      </Copyable>
+    </p>
+    <Icon.ArrowDown />
+    <p class="radicle-user typo-text-bold">
+      <Avatar
+        size="small"
+        avatarFallback={identity.avatarFallback}
+        variant="circle"
+        style="margin-right: 10px" />
+      {identity.metadata.handle}
+    </p>
+  </div>
+
+  <div class="submit">
+    <Button variant="transparent" dataCy="cancel-topup" on:click={onCancel}>
+      Cancel
+    </Button>
+
+    <Button
+      dataCy="confirm-button"
+      on:click={onConfirm}
+      style="margin-left: 14px;">
+      Confirm
+    </Button>
+  </div>
+</div>
--- ui/Modal/Funding/LinkAddress.svelte
+++ ui/Modal/Funding/LinkAddress.svelte
@@ -1,15 +1,16 @@
 <script lang="typescript">
-  import { Illustration, Copyable } from "../../DesignSystem/Component";
-  import { Avatar, Icon, Button } from "../../DesignSystem/Primitive";
+  import EthToRadicle from "../../DesignSystem/Component/Funding/Link/EthToRadicle.svelte";
 
   import { wallet } from "../../src/wallet";
   import { session } from "../../src/session";
   import * as identity from "../../src/identity";
 
-  import { Variant as IllustrationVariant } from "../../src/illustration";
   import * as modal from "../../src/modal";
 
-  async function onConfirmed(): Promise<void> {
+  function onCancel(): void {
+    modal.hide();
+  }
+  async function onConfirm(): Promise<void> {
     console.log("onConfirmed: ", $wallet.connected.account.address);
     const p = identity
       .linkEthereumAddress($wallet.connected.account.address)
@@ -20,92 +21,25 @@
 </script>
 
 <style>
-  .qrcode-modal {
+  .wrapper {
     display: flex;
     justify-content: space-around;
     align-items: center;
     flex-direction: column;
     padding: var(--content-padding);
     width: 600px;
+    min-height: 400px;
     background: var(--color-background);
     border-radius: 0.5rem;
 
     text-align: center;
   }
-
-  header {
-    padding: 0px var(--content-padding);
-  }
-
-  .data {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-
-    width: 100%;
-    height: 170px;
-
-    border: 1px solid var(--color-foreground-level-3);
-    background-color: var(--color-foreground-level-1);
-
-    margin-top: var(--content-padding);
-    padding: calc(var(--content-padding) / 2);
-    border-radius: 16px;
-  }
-
-  .radicle-user {
-    display: flex;
-    align-items: center;
-  }
-  .submit {
-    display: flex;
-    justify-content: flex-end;
-    width: 100%;
-    margin-top: calc(var(--content-padding) / 2);
-  }
 </style>
 
-<div class="qrcode-modal">
-  <Illustration variant={IllustrationVariant.Socks} />
-
-  <header>
-    <h1 style="margin-top: 1.5rem;">Link your Ethereum Account</h1>
-    <p style="margin-top: 1.5rem;" class="typo-text">
-      Are you sure you want to add your ethereum address to your Radicle
-      account?
-    </p>
-  </header>
-
-  <div class="data">
-    <p class="address typo-text">
-      <Copyable
-        showIcon={false}
-        styleContent={false}
-        copyContent={$wallet.connected.account.address}
-        notificationText="Address copied to the clipboard">
-        {$wallet.connected.account.address}
-      </Copyable>
-    </p>
-    <Icon.Link />
-    <p class="radicle-user typo-text-bold">
-      <Avatar
-        size="small"
-        avatarFallback={$session.data.identity.avatarFallback}
-        variant="circle"
-        style="margin-right: 10px" />
-      {$session.data.identity.metadata.handle}
-    </p>
-  </div>
-
-  <div class="submit">
-    <Button
-      variant="transparent"
-      dataCy="cancel-topup"
-      on:click={() => modal.hide()}>
-      Cancel
-    </Button>
-
-    <Button dataCy="confirm-button" on:click={onConfirmed}>Confirm</Button>
-  </div>
+<div class="wrapper">
+  <EthToRadicle
+    address={$wallet.connected.account.address}
+    identity={$session.data.identity}
+    {onCancel}
+    {onConfirm} />
 </div>
