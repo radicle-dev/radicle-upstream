9386ea7e8114046099809df8a70defdc8efbb7b3
Nuno Alexandre <hi@nunoalexandre.com>
2020-11-25 09:53:28 +0100

    Delete obsolete Box component

--- ui/DesignSystem/Primitive/index.ts
+++ ui/DesignSystem/Primitive/index.ts
@@ -1,5 +1,4 @@
 import Avatar from "./Avatar.svelte";
-import Box from "./Box.svelte";
 import Button from "./Button.svelte";
 import Emoji from "./Emoji.svelte";
 import Icon from "./Icon";
@@ -7,4 +6,4 @@ import Input from "./Input";
 import Line from "./Line.svelte";
 import Markdown from "./Markdown.svelte";
 
-export { Avatar, Box, Button, Emoji, Icon, Input, Line, Markdown };
+export { Avatar, Button, Emoji, Icon, Input, Line, Markdown };
