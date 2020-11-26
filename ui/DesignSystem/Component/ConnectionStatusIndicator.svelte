83c117c14eb46531f7ebe309214efca4f5f7036f
Julien Donck <donckjulien@gmail.com>
2020-11-26 16:20:39 +0100

    update data-cy

--- ui/DesignSystem/Component/ConnectionStatusIndicator.svelte
+++ ui/DesignSystem/Component/ConnectionStatusIndicator.svelte
@@ -50,26 +50,26 @@
   <div>
     {#if status.type === StatusType.Online}
       <Tooltip value={`You’re connected to ${peerCount(status.connected)}`}>
-        <div class="item indicator" data-cy="search">
+        <div class="item indicator" data-cy="connection-status">
           <Icon.Network />
         </div>
       </Tooltip>
     {:else if status.type === StatusType.Syncing}
       <Tooltip
         value={`Syncing with ${peerCount(status.syncs)} to get new content from your network`}>
-        <div class="item indicator" data-cy="search">
+        <div class="item indicator" data-cy="connection-status">
           <Syncing />
         </div>
       </Tooltip>
     {:else if status.type === StatusType.Offline || status.type === StatusType.Started}
       <Tooltip value="You’re not connected to any peers">
-        <div class="item indicator" data-cy="search">
+        <div class="item indicator" data-cy="connection-status">
           <Offline />
         </div>
       </Tooltip>
     {:else if status.type === StatusType.Stopped}
       <Tooltip value="The app couldn't start your peer">
-        <div class="item indicator" data-cy="search">
+        <div class="item indicator" data-cy="connection-status">
           <Offline style="fill: var(--color-negative);" />
         </div>
       </Tooltip>
