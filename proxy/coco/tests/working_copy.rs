a0a271847b6f130782025a953dde404e625e9b13
Alexander Simmerl <a.simmerl@gmail.com>
2021-01-07 18:21:59 +0100

    Add checkout test

--- proxy/coco/tests/working_copy.rs
+++ proxy/coco/tests/working_copy.rs
@@ -29,3 +29,29 @@ async fn upstream_for_default() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn can_checkout() -> Result<(), Box<dyn std::error::Error>> {
+    init_logging();
+
+    let alice_tmp_dir = tempfile::tempdir()?;
+    let alice_repo_path = alice_tmp_dir.path().join("radicle");
+    let (alice_peer, alice_state) = build_peer(&alice_tmp_dir, RunConfig::default()).await?;
+    let alice = alice_state.init_owner("alice".to_string()).await?;
+
+    tokio::task::spawn(alice_peer.into_running());
+
+    let project = alice_state
+        .init_project(&alice, shia_le_pathbuf(alice_repo_path.clone()))
+        .await?;
+
+    let _ = alice_state
+        .checkout(project.urn(), None, alice_repo_path.join("checkout"))
+        .await?;
+
+    let _ = alice_state
+        .checkout(project.urn(), None, alice_repo_path.join("checkout"))
+        .await?;
+
+    Ok(())
+}
