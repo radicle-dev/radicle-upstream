b833765c13bb5aaedfba10413c7186201a68b093
Nuno Alexandre <hi@nunoalexandre.com>
2021-01-28 19:07:26 +0100

    Disable get started btn on ongoing onboarding tx
    We have to wait until the onboarding tx is confirmed. Meanwile, the only
    sensible thing to do is to show the 'GetStarted' screen but with its
    button disabled.

--- ui/Screen/Funding/Pool/Outgoing.svelte
+++ ui/Screen/Funding/Pool/Outgoing.svelte
@@ -5,14 +5,22 @@
   import Support from "./Outgoing/Support.svelte";
 
   import * as fundingPool from "../../../src/funding/pool";
+  import * as transaction from "../../../src/transaction";
 
   export let pool: fundingPool.Pool;
+
+  let ongoingOnboardingTx = false;
+  transaction.store.subscribe(_ => {
+    ongoingOnboardingTx = transaction.ongoing(
+      transaction.TxKind.SupportOnboarding
+    );
+  });
 </script>
 
 <Remote store={pool.data} let:data={poolData}>
   {#if fundingPool.isOnboarded(poolData)}
     <Support bind:pool />
   {:else}
-    <GetStarted bind:pool />
+    <GetStarted ongoingTx={ongoingOnboardingTx} bind:pool />
   {/if}
 </Remote>
