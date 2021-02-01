5ea05a37a56645aca1c52779756f9a2df6f33aaf
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-02-01 15:39:18 +0100

    Shuffle stuff around
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- scripts/release.js
+++ scripts/release.js
@@ -16,82 +16,96 @@ const PULL_REQUEST_MATCH =
 
 const SV_COMMAND = "yarn run standard-version --infile ./CHANGELOG.md";
 
-async function main() {
-  console.log();
-
-  const svResult = await exec(`${SV_COMMAND} --dry-run`);
-  const toVersion = `v${svResult.stdout.match(VERSION_MATCH)[2]}`;
-
+const checkPrerequisites = async () => {
   try {
     await exec("hub --version");
   } catch (error) {
     if (error.stderr.match("command not found")) {
-      console.log("Please install missing dependencies:");
-      console.log("  - https://github.com/github/hub");
+      printMissingDependenciesMsg();
     } else {
       throw error;
     }
   }
+};
 
-  if (process.argv[2] === "--finalize") {
-    const toVersion = process.argv[3];
-    const pullRequestId = process.argv[4];
+const printMissingDependenciesMsg = () =>
+  console.log(`
+  Please install missing dependencies:
+    - https://github.com/github/hub
+`);
 
-    if (toVersion === undefined || pullRequestId === undefined) {
-      console.log("This command should not be run stand-alone.");
-      console.log("You should run `yarn release` and follow the instructions.");
-      console.log();
-      return;
-    }
+const finalizeRelease = async () => {
+  const toVersion = process.argv[3];
+  const pullRequestId = process.argv[4];
 
-    console.log(`Finalizing release ${toVersion}:`);
-    console.log();
-
-    const mergeResult = await verboseExec(
-      `hub api -XPUT "repos/radicle-dev/radicle-upstream/pulls/${pullRequestId}/merge" --raw-field 'merge_method=squash'`
-    );
-    const releaseCommitSHA = JSON.parse(mergeResult.stdout).sha;
-
-    await verboseExec("git checkout master && git pull");
-
-    await verboseExec(`git tag ${toVersion} ${releaseCommitSHA}`);
-
-    await verboseExec(`git push --tags`);
-    console.log();
-    console.log(`Release ${toVersion} successfully completed! 👏 🎉 🚀`);
-    console.log();
-  } else {
-    console.log(`Cutting release ${toVersion}:\n`);
-
-    await verboseExec("git checkout master");
-
-    await verboseExec(
-      `git branch release-${toVersion} && git checkout release-${toVersion}`
-    );
-
-    await verboseExec(SV_COMMAND);
-    await verboseExec(`git push origin release-${toVersion}`);
-
-    const prResult = await verboseExec("hub pull-request -p --no-edit");
-
-    const prUrl = prResult.stdout.split("\n").slice(-2)[0];
-    const pullRequestId = prUrl.match(PULL_REQUEST_MATCH)[1];
-
-    console.log();
-    console.log("Now fix up CHANGELOG.md if necessary and update QA.md");
-    console.log("to cover the latest changes in functionality.");
-    console.log();
-    console.log("When everything is in shape, ask a peer to review the");
-    console.log("pull request, but don't merge it via the GitHub UI:");
-    console.log();
-    console.log(`  👉 ${prUrl}`);
-    console.log();
-    console.log("Finally, complete the release by running:");
-    console.log();
-    console.log(`  👉 yarn release --finalize ${toVersion} ${pullRequestId}`);
+  if (toVersion === undefined || pullRequestId === undefined) {
+    printWrongArgsMsg();
+    return;
   }
 
-  console.log();
+  console.log(`Finalizing release ${toVersion}:\n`);
+
+  const mergeResult = await verboseExec(
+    `hub api -XPUT ` +
+      `"repos/radicle-dev/radicle-upstream/pulls/${pullRequestId}/merge" ` +
+      `--raw-field 'merge_method=squash'`
+  );
+  const releaseCommitSHA = JSON.parse(mergeResult.stdout).sha;
+  await verboseExec("git checkout master && git pull");
+  await verboseExec(`git tag ${toVersion} ${releaseCommitSHA}`);
+  await verboseExec(`git push --tags`);
+
+  console.log(`\nRelease ${toVersion} successfully completed! 👏 🎉 🚀\n`);
+};
+
+const printWrongArgsMsg = () =>
+  console.log(`
+  This command should not be run stand-alone.
+  You should run \`yarn release\` and follow the instructions.
+`);
+
+const cutRelease = async toVersion => {
+  console.log(`\nCutting release ${toVersion}:\n`);
+
+  await verboseExec("git checkout master");
+  await verboseExec(
+    `git branch release-${toVersion} && git checkout release-${toVersion}`
+  );
+  await verboseExec(SV_COMMAND);
+  await verboseExec(`git push origin release-${toVersion}`);
+  const prResult = await verboseExec("hub pull-request -p --no-edit");
+  const prUrl = prResult.stdout.split("\n").slice(-2)[0];
+  const pullRequestId = prUrl.match(PULL_REQUEST_MATCH)[1];
+
+  printNextStepsMsg(prUrl, toVersion, pullRequestId);
+};
+
+const printNextStepsMsg = (prUrl, toVersion, pullRequestId) =>
+  console.log(`
+  Now fix up CHANGELOG.md if necessary and update QA.md
+  to cover the latest changes in functionality.
+
+  When everything is in shape, ask a peer to review the
+  pull request, but don't merge it via the GitHub UI:
+
+    👉 ${prUrl}
+
+  Finally, complete the release by running:
+
+    👉 yarn release --finalize ${toVersion} ${pullRequestId}
+`);
+
+async function main() {
+  checkPrerequisites();
+
+  const svResult = await exec(`${SV_COMMAND} --dry-run`);
+  const toVersion = `v${svResult.stdout.match(VERSION_MATCH)[2]}`;
+
+  if (process.argv[2] === "--finalize") {
+    finalizeRelease();
+  } else {
+    cutRelease(toVersion);
+  }
 }
 
 main();
