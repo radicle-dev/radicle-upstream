cef50d3c382fd6f891e667728627692311b63395
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-02-12 08:34:28 +0100

    Hard wrap text
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- cypress/plugins/nodeManager/shared.ts
+++ cypress/plugins/nodeManager/shared.ts
@@ -1,7 +1,8 @@
-// Types and constants shared between `plugin.ts` and `commands.ts`. These types can't be in
-// `plugin.ts`, because Cypress plugins run in a separate Nodejs environment
-// and can directly use Nodejs libraries, the Cypress tests don't have access
-// to those, so indlucing `plugin.ts` inside `commands.ts` leads to errors.
+// Types and constants shared between `plugin.ts` and `commands.ts`. These
+// types can't be in `plugin.ts`, because Cypress plugins run in a separate
+// Nodejs environment and can directly use Nodejs libraries, the Cypress tests
+// don't have access to those, so indlucing `plugin.ts` inside `commands.ts`
+// leads to errors.
 
 import * as path from "path";
 
@@ -25,7 +26,7 @@ export enum Commands {
 
 // A directory that can be used for temporary test data.
 //
-// It is located within this repository so that there is no extra setup necessary
-// when using it locally or on CI. To avoid committing any left-over temp data
-// this directory ignored via .gitignore.
+// It is located within this repository so that there is no extra setup
+// necessary when using it locally or on CI. To avoid committing any left-over
+// temp data this directory ignored via .gitignore.
 export const CYPRESS_WORKSPACE_PATH = path.join(__dirname, "../../workspace");
