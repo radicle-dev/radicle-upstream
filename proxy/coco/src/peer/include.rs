4b57f89e7827542dcbdb2540222d61041077d410
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-02-22 16:50:45 +0000

    Update include to use net::peer::Peer
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- proxy/coco/src/peer/include.rs
+++ proxy/coco/src/peer/include.rs
@@ -1,12 +1,12 @@
 //! Handling of include files
 
-use librad::identities::Urn;
+use librad::{identities::Urn, net::peer::Peer, signer::BoxedSigner};
 
-use crate::state::State;
+use crate::state;
 
 /// Update the include file for the given `RadUrn` and log the result.
-pub async fn update(state: State, urn: Urn) {
-    match state.update_include(urn.clone()).await {
+pub async fn update(peer: &Peer<BoxedSigner>, urn: Urn) {
+    match state::update_include(peer, urn.clone()).await {
         Ok(path) => log::debug!("Updated include file @ {}", path.display()),
         Err(err) => log::debug!("Failed to update include file for `{}`: {}", urn, err),
     }
