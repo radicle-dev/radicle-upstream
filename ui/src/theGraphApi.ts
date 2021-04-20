290304038b8b1f13c0c908df655de69c3ec9f9b5
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-04-20 16:52:35 +0200

    Fix "Module not found: Error: Can't resolve 'react'" errors
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/src/theGraphApi.ts
+++ ui/src/theGraphApi.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core";
 
 const gnosisSubgraphClient = new ApolloClient({
   uri:
