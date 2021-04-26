5be111430371d2875b1bbedaaafd3d5568c8f6ed
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-04-26 18:40:41 +0200

    Add empty state placeholder
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/Screen/Org/Projects.svelte
+++ ui/Screen/Org/Projects.svelte
@@ -1,1 +1,12 @@
-PROJECTS SCREEN
+<script>
+  import { EmptyState } from "../../DesignSystem/Component";
+  import { Variant as IllustrationVariant } from "../../src/illustration.ts";
+</script>
+
+<EmptyState
+  illustration={IllustrationVariant.Plant}
+  text="Get started by anchoring your organization’s first project with the radicle gnosis safe app."
+  primaryActionText="Anchor with Gnosis Safe"
+  on:primaryAction={() => {
+    window.location.href = 'https://gnosis-safe.io/';
+  }} />
