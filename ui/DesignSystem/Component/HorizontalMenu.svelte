83072581a8dd736a65f0ea1fe2d6b4b482c348f7
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-05-16 12:21:39 +0200

    Consolidate HorizontalMenu and MenuItem
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/DesignSystem/Component/HorizontalMenu.svelte
+++ ui/DesignSystem/Component/HorizontalMenu.svelte
@@ -3,8 +3,6 @@
 
   import type { HorizontalItem } from "../../src/menu";
 
-  import MenuItem from "./HorizontalMenu/MenuItem.svelte";
-
   export let items: HorizontalItem[];
   export let activeTab: "files" | "commit" | "commits";
   export let style: string = "";
@@ -39,20 +37,60 @@
     fill: var(--color-primary);
   }
 
+  .icon {
+    margin-right: 0.5rem;
+    align-items: center;
+    padding-top: 1px;
+  }
+
+  .tab {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .title {
+    line-height: 130%;
+    color: var(--color-foreground-level-6);
+  }
+
+  .title.active {
+    color: var(--color-primary);
+  }
+
+  .counter {
+    background-color: var(--color-foreground-level-2);
+    color: var(--color-foreground-level-6);
+    padding: 0.1875rem 0.5rem;
+    border-radius: 0.75rem;
+    margin-left: 0.5rem;
+  }
+
 </style>
 
 <nav data-cy="horizontal-menu" {style}>
   <ul class="menu-list">
     {#each items as item}
       <li class="menu-list-item">
-        <MenuItem
-          on:click={() => dispatch('select', item)}
-          icon={item.icon}
-          title={item.title}
-          dataCy={`${item.title.toLowerCase()}-tab`}
-          counter={item.counter}
-          tab={item.tab}
-          active={activeTab === item.tab} />
+        <div
+          class="tab"
+          data-cy={`${item.title.toLowerCase()}-tab`}
+          on:click={() => dispatch('select', item)}>
+          <div class="icon">
+            <svelte:component
+              this={item.icon}
+              style={activeTab === item.tab ? 'fill: var(--color-primary)' : ''} />
+          </div>
+
+          <p class="title typo-text-bold" class:active={activeTab === item.tab}>
+            {item.title}
+          </p>
+          {#if item.counter}
+            <span
+              class="counter typo-mono-bold"
+              data-cy="counter">{item.counter}</span>
+          {/if}
+        </div>
       </li>
     {/each}
   </ul>
