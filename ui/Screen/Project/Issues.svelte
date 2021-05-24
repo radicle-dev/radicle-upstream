7b73a821b4efc78a843e21a9bb4f4bc9c5b07626
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-05-24 10:49:20 +0200

    Reduce unrelated changes
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

---
+++ ui/Screen/Project/Issues.svelte
@@ -0,0 +1,97 @@
+<script>
+  import IssueCard from "./Issues/IssueCard.svelte";
+  import {
+    EmptyState,
+    List,
+    SegmentedControl,
+  } from "../../DesignSystem/Component";
+
+  const filterOptions = [
+    {
+      title: "Open",
+      value: "Open",
+    },
+    {
+      title: "Closed",
+      value: "Closed",
+    },
+    {
+      title: "All",
+      value: "All",
+    },
+  ];
+
+  const issues = [
+    {
+      hash: "blka",
+      open: true,
+      title: "Split server into CentralChain, and ChainApi",
+      author: "julien",
+      replies: 12,
+      created_at: "12 days",
+      updated_at: "1 day",
+      closed_at: null,
+    },
+    {
+      hash: "blkhjghfdga",
+      open: true,
+      title: "Feature request: Call JSON.stringify before displaying objects",
+      author: "xla",
+      replies: 0,
+      created_at: "20 days",
+      updated_at: "18 days",
+      closed_at: null,
+    },
+    {
+      hash: "blkfsfsa",
+      open: false,
+      title: "Return permissions via the session endpoint",
+      author: "rudolfs",
+      replies: 2,
+      created_at: "1 month",
+      updated_at: "27 days",
+      closed_at: "27 days",
+    },
+  ];
+  let currentFilter = "Open";
+  function updateFilter(newFilter) {
+    currentFilter = newFilter;
+  }
+  $: filteredIssues =
+    currentFilter === "All"
+      ? issues
+      : currentFilter === "Open"
+      ? issues.filter(issue => issue.open)
+      : issues.filter(issue => !issue.open);
+</script>
+
+<style>
+  .container {
+    max-width: var(--content-max-width);
+    margin: 0 auto;
+    min-width: var(--content-min-width);
+  }
+  .filters {
+    margin-bottom: var(--content-padding);
+    padding: 0 var(--content-padding);
+  }
+</style>
+
+<div class="container">
+  <div class="filters">
+    <SegmentedControl
+      active={"Open"}
+      options={filterOptions}
+      on:select={option => updateFilter(option.detail)} />
+  </div>
+  {#if issues.length > 0}
+    <List dataCy="issue-list" items={filteredIssues} let:item={issue}>
+      <IssueCard {issue} />
+    </List>
+  {:else}
+    <EmptyState
+      emoji="🔭"
+      text="There’s nothing here yet, get started by creating your first issue."
+      primaryActionText="Open a new issue" />
+  {/if}
+</div>
---
+++ ui/Screen/Project/Issues/IssueCard.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { Icon } from "../../../DesignSystem/Primitive";
+
+  export let issue = null;
+
+  const issueCaption = issue => {
+    if (issue.open) {
+      return `Opened ${issue.created_at} ago by ${issue.author}`;
+    } else {
+      return `Created by ${issue.author} closed ${issue.closed_at} ago`;
+    }
+  };
+
+  const issueIconColor = issueOpen => {
+    return issueOpen ? "--color-positive" : "--color-negative";
+  };
+</script>
+
+<style>
+  .issue-card {
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+    padding: 16px;
+  }
+
+  .title {
+    display: flex;
+  }
+
+  .right {
+    display: flex;
+    align-items: center;
+  }
+  .reply-count {
+    display: flex;
+    margin-right: 24px;
+  }
+</style>
+
+<div class="issue-card">
+  <div class="title">
+    <Icon.ExclamationCircle
+      style="margin-right: 12px; fill: var({issueIconColor(issue.open)})" />
+    <div>
+      <p class="typo-text-bold">{issue.title}</p>
+      <p style="color: var(--color-foreground-level-5); padding-top: 6px">
+        {issueCaption(issue)}
+      </p>
+    </div>
+  </div>
+  <div class="right">
+    {#if issue.replies > 0}
+      <div class="reply-count">
+        <Icon.Chat />
+        <p
+          class="typo-text-bold"
+          style="margin-left: 4px; color: var(--color-foreground-level-5);">
+          {issue.replies}
+        </p>
+      </div>
+    {/if}
+    <p style="color: var(--color-foreground-level-5);">
+      Updated
+      {issue.updated_at}
+      ago
+    </p>
+  </div>
+</div>
