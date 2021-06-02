7d1e75a50af658f482929173992b1ce241ebca10
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-06-02 20:22:47 +0200

    Open anchor modal directly
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/Screen/Org/ProjectsMenu.svelte
+++ ui/Screen/Org/ProjectsMenu.svelte
@@ -1,53 +1,16 @@
 <script lang="typescript">
-  import ModalAnchorProject from "ui/Modal/Org/AnchorProject.svelte";
-  import { Button, Icon } from "../../DesignSystem/Primitive";
-  import { Overlay } from "../../DesignSystem/Component";
+  import { Button, Icon } from "ui/DesignSystem/Primitive";
   import * as modal from "ui/src/modal";
+  import ModalAnchorProject from "ui/Modal/Org/AnchorProject.svelte";
 
-  let expanded = false;
-
-  const hide = () => (expanded = false);
-  const toggleModal = () => {
-    expanded = !expanded;
+  const openAnchorProjectModal = () => {
+    modal.toggle(ModalAnchorProject, () => {}, {});
   };
 </script>
 
-<style>
-  .modal {
-    margin-top: 3rem;
-    right: 0;
-    position: absolute;
-    border-radius: 0.5rem;
-    background: var(--color-background);
-    border: 1px solid var(--color-foreground-level-3);
-    box-shadow: var(--elevation-medium);
-    padding: 1rem;
-    width: 25rem;
-  }
-
-  p {
-    color: var(--color-foreground-level-6);
-    user-select: none;
-  }
-</style>
-
-<Overlay {expanded} on:hide={hide} style="position: relative;">
-  <div class="modal" hidden={!expanded}>
-    <p style="margin-bottom: 0.5rem;">
-      Anchoring is done through the radicle gnosis safe app.
-    </p>
-
-    <Button
-      on:click={() => {
-        modal.toggle(ModalAnchorProject);
-        toggleModal();
-      }}
-      style="margin-top: 1rem; width: 100%; display: block; text-align: center;">
-      Anchor with Gnosis Safe
-    </Button>
-  </div>
-
-  <Button variant="transparent" icon={Icon.Anchor} on:click={toggleModal}>
-    Anchor a project
-  </Button>
-</Overlay>
+<Button
+  variant="transparent"
+  icon={Icon.Anchor}
+  on:click={openAnchorProjectModal}>
+  Anchor a project
+</Button>
