cff439cc942f36ba2c0b993a6e219127a142a507
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-06-09 10:23:52 +0200

    Show follow button only on hover
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/Screen/Org/AnchorList.svelte
+++ ui/Screen/Org/AnchorList.svelte
@@ -1,4 +1,6 @@
 <script lang="typescript">
+  import { fade } from "svelte/transition";
+
   import type * as theGraphApi from "ui/src/theGraphApi";
   import * as notification from "ui/src/notification";
   import * as search from "ui/src/search";
@@ -6,6 +8,7 @@
   import {
     AnchorMetadataModal,
     FollowToggle,
+    Hoverable,
     List,
     RadicleId,
   } from "ui/DesignSystem/Component";
@@ -31,15 +34,13 @@
     padding: 1.375rem 1.5rem;
     align-items: center;
     min-width: 0;
+    height: 84px;
   }
   .anchor-row {
     display: flex;
     white-space: nowrap;
     width: -webkit-fill-available;
     color: var(--color-foreground-level-6);
-  }
-  .reset-cursor {
-    cursor: default;
   }
   .header {
     display: flex;
@@ -61,17 +62,22 @@
   dataCy="project-list"
   items={anchors}
   let:item={anchor}
+  styleHoverState={false}
   style="margin: 0 auto;">
-  <div
-    class="reset-cursor list-item"
-    data-cy={`project-list-entry-${anchor.id}`}>
-    <div class="typo-text anchor-row">
-      <RadicleId urn={anchor.projectId} showIcon={false} />
-      <AnchorMetadataModal {anchor} {orgAddress} />
+  <Hoverable let:hovering={hover} style="flex: 1">
+    <div class="list-item" data-cy={`project-list-entry-${anchor.id}`}>
+      <div class="typo-text anchor-row">
+        <RadicleId urn={anchor.projectId} showIcon={false} />
+        <AnchorMetadataModal {anchor} {orgAddress} />
+      </div>
+      {#if hover}
+        <div transition:fade={{ duration: 200 }}>
+          <FollowToggle
+            on:follow={() => {
+              onFollow(anchor.projectId);
+            }} />
+        </div>
+      {/if}
     </div>
-    <FollowToggle
-      on:follow={() => {
-        onFollow(anchor.projectId);
-      }} />
-  </div>
+  </Hoverable>
 </List>
