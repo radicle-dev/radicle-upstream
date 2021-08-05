110cdffc87f888708921cea9286f7c01147e4127
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-08-05 10:00:38 +0200

    Inform user about pending Gnosis transaction
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/Modal/EnsSetupFlow/steps/LinkOrgToNameSuccess.svelte
+++ ui/Modal/EnsSetupFlow/steps/LinkOrgToNameSuccess.svelte
@@ -9,19 +9,35 @@
   import Emoji from "ui/DesignSystem/Emoji.svelte";
 
   import type { EnsConfiguration } from "../ens-flow.types";
+  import * as org from "ui/src/org";
 
   import ButtonRow from "./shared/ButtonRow.svelte";
   import HeadlineAndDescription from "./shared/HeadlineAndDescription.svelte";
 
   export let onSubmit: () => void = () => {};
+  export let safeAddress: string | undefined = undefined;
 
   export let ensConfiguration: EnsConfiguration;
 </script>
 
 <div>
-  <Emoji emoji="🎉" size="huge" style="margin-bottom: 16px" />
-  <HeadlineAndDescription
-    headline="That's it!"
-    description={`Great, your organization now points to your new name ${ensConfiguration.name}.radicle.eth. Shortly, your name will start appearing across Radicle in place of your organization address!`} />
-  <ButtonRow {onSubmit} canCancel={false} confirmCopy="Amazing, thanks!" />
+  {#if safeAddress}
+    <Emoji emoji="🤝" size="huge" style="margin-bottom: 16px" />
+    <HeadlineAndDescription
+      headline="Approve on Gnosis"
+      description={"As a final step your organisation will have to confirm the transaction on Gnosis. After it's been approved and executed your newly registered name will start appearing across Radicle in place of your organization address!"} />
+    <ButtonRow
+      onSubmit={() => {
+        safeAddress && org.openOnGnosisSafe(safeAddress, "transactions");
+        onSubmit();
+      }}
+      canCancel={false}
+      confirmCopy="View proposal on Gnosis" />
+  {:else}
+    <Emoji emoji="🎉" size="huge" style="margin-bottom: 16px" />
+    <HeadlineAndDescription
+      headline="That's it!"
+      description={`Great, your organization now points to your new name ${ensConfiguration.name}.radicle.eth. Shortly, your name will start appearing across Radicle in place of your organization address!`} />
+    <ButtonRow {onSubmit} canCancel={false} confirmCopy="Amazing, thanks!" />
+  {/if}
 </div>
