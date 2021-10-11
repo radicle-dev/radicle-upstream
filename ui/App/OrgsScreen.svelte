2a05a711cda0d7b96174c0f0e8802ade5c501378
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-10-11 18:09:47 +0200

    Show loading/loaded/error states
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/App/OrgsScreen.svelte
+++ ui/App/OrgsScreen.svelte
@@ -6,24 +6,43 @@
  LICENSE file.
 -->
 <script lang="typescript">
+  import { onMount } from "svelte";
   import lodash from "lodash";
 
-  import * as theGraphApi from "ui/src/org/theGraphApi";
+  import { push } from "ui/src/router";
+  import { unreachable } from "ui/src/unreachable";
   import * as Org from "ui/src/org";
   import * as ensResolver from "ui/src/org/ensResolver";
-  import { push } from "ui/src/router";
+  import * as error from "ui/src/error";
+  import * as theGraphApi from "ui/src/org/theGraphApi";
 
   import { Avatar, Badge, CopyableIdentifier, Icon } from "ui/DesignSystem";
   import ScreenLayout from "ui/App/ScreenLayout.svelte";
+  import EmptyState from "ui/App/ScreenLayout/EmptyState.svelte";
 
   let resolvedOrgs: ResolvedOrg[] = [];
+
+  let state: "loading" | "loaded" | "error" = "loading";
 
   interface ResolvedOrg {
     org: Org.Org;
     owner: Org.Owner;
   }
 
-  async function fetchOrgs() {
+  onMount(async () => {
+    state = "loading";
+    try {
+      resolvedOrgs = await fetchOrgs();
+      state = "loaded";
+    } catch (err: unknown) {
+      error.show(
+        new error.Error({ message: "Failed to fetch orgs", source: err })
+      );
+      state = "error";
+    }
+  });
+
+  async function fetchOrgs(): Promise<ResolvedOrg[]> {
     const unresolvedOrgs = await theGraphApi.getAllOrgs();
     await Promise.all(
       unresolvedOrgs.map(async org => {
@@ -33,16 +52,14 @@
       })
     );
 
-    resolvedOrgs = await Promise.all(
+    const resolvedOrgs = await Promise.all(
       unresolvedOrgs.map(async org => {
         const owner = await Org.getOwner(org.id);
         return { owner, org };
       })
     );
-    resolvedOrgs = lodash.orderBy(resolvedOrgs, "org.registration.domain");
+    return lodash.orderBy(resolvedOrgs, "org.registration.domain");
   }
-
-  fetchOrgs();
 </script>
 
 <style>
@@ -100,59 +117,67 @@
 <ScreenLayout dataCy="network-page">
   <div class="container">
     <h1 style="padding: 0 0.75rem;">Explore orgs</h1>
-    <div class="grid">
-      {#each resolvedOrgs as { org, owner }}
-        <div
-          class="box"
-          on:click={() =>
-            push({
-              type: "org",
-              params: { address: org.id, view: "projects" },
-            })}>
-          <header class="row">
-            <Avatar
-              style="margin-right: 1rem;"
-              size="large"
-              kind={org.registration?.avatar
-                ? { type: "orgImage", url: org.registration.avatar }
-                : { type: "orgEmoji", uniqueIdentifier: org.id }} />
-            <div class="title-meta">
-              <h3 class="typo-overflow-ellipsis">
-                {org.registration?.domain ? org.registration.domain : org.id}
-              </h3>
-              <Badge
-                style="align-self: flex-start;"
-                caption={owner.type === "wallet"
-                  ? "Single signer org"
-                  : "Multi signer org"} />
-            </div>
-          </header>
-          <ul class="metadata">
-            <li class="row">
-              {#if owner.type === "gnosis-safe"}
-                <Icon.Gnosis />
-              {:else}
-                <Icon.Ethereum />
-              {/if}
-              <CopyableIdentifier
-                style="margin-left: 0.5rem;"
-                value={owner.address}
-                kind="ethAddress"
-                name="owner address"
-                showIcon={false} />
-            </li>
-            {#if owner.type === "gnosis-safe"}
+    {#if state === "loading"}
+      <EmptyState emoji="⌛" text="Loading orgs…" />
+    {:else if state === "loaded"}
+      <div class="grid">
+        {#each resolvedOrgs as { org, owner }}
+          <div
+            class="box"
+            on:click={() =>
+              push({
+                type: "org",
+                params: { address: org.id, view: "projects" },
+              })}>
+            <header class="row">
+              <Avatar
+                style="margin-right: 1rem;"
+                size="large"
+                kind={org.registration?.avatar
+                  ? { type: "orgImage", url: org.registration.avatar }
+                  : { type: "orgEmoji", uniqueIdentifier: org.id }} />
+              <div class="title-meta">
+                <h3 class="typo-overflow-ellipsis">
+                  {org.registration?.domain ? org.registration.domain : org.id}
+                </h3>
+                <Badge
+                  style="align-self: flex-start;"
+                  caption={owner.type === "wallet"
+                    ? "Single signer org"
+                    : "Multi signer org"} />
+              </div>
+            </header>
+            <ul class="metadata">
               <li class="row">
-                <Icon.Proposals />
-                <p style="margin-left: .5rem;">
-                  {owner.threshold} of {owner.members.length}
-                  {owner.members.length === 1 ? "signature" : "signatures"} required
-                </p>
+                {#if owner.type === "gnosis-safe"}
+                  <Icon.Gnosis />
+                {:else}
+                  <Icon.Ethereum />
+                {/if}
+                <CopyableIdentifier
+                  style="margin-left: 0.5rem;"
+                  value={owner.address}
+                  kind="ethAddress"
+                  name="owner address"
+                  showIcon={false} />
               </li>
-            {/if}
-          </ul>
-        </div>
-      {/each}
-    </div>
+              {#if owner.type === "gnosis-safe"}
+                <li class="row">
+                  <Icon.Proposals />
+                  <p style="margin-left: .5rem;">
+                    {owner.threshold} of {owner.members.length}
+                    {owner.members.length === 1 ? "signature" : "signatures"} required
+                  </p>
+                </li>
+              {/if}
+            </ul>
+          </div>
+        {/each}
+      </div>
+    {:else if state === "error"}
+      <EmptyState emoji="😬" text="Could not get org list." />
+    {:else}
+      {unreachable(state)}
+    {/if}
   </div>
 </ScreenLayout>
