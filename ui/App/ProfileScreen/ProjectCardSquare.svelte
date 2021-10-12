4be4f1a9a850e49ddfbe2eab7933e067183366fd
Julien Donck <hello@juliendonck.com>
2021-10-12 16:34:03 +0200

    border tweak (#2476)
    Signed-off-by: Julien Donck <hello@juliendonck.com>

--- ui/App/ProfileScreen/ProjectCardSquare.svelte
+++ ui/App/ProfileScreen/ProjectCardSquare.svelte
@@ -18,7 +18,7 @@
 
 <style>
   .project-card {
-    border: 1px solid var(--color-foreground-level-3);
+    border: 1px solid var(--color-foreground-level-2);
     border-radius: 0.5rem;
     padding: 2rem;
     height: 15rem;
