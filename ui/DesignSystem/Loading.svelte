7d5aa603811c59b49544412dc623f433f7c10d92
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-10-15 17:17:22 +0200

    chore: resolve svelte-check warning
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/DesignSystem/Loading.svelte
+++ ui/DesignSystem/Loading.svelte
@@ -5,7 +5,7 @@
  with Radicle Linking Exception. For full terms see the included
  LICENSE file.
 -->
-<script lang="typescript">
+<script lang="ts">
   import Emoji from "./Emoji.svelte";
 </script>
 
