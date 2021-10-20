126ec7c7375f359fb9730081e6f958494ebeb31e
Rūdolfs Ošiņš <rudolfs@osins.org>
2021-10-20 19:01:46 +0200

    refactor: simplify ThreeDotsMenu component
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/DesignSystem/ThreeDotsMenu.svelte
+++ ui/DesignSystem/ThreeDotsMenu.svelte
@@ -6,13 +6,16 @@
  LICENSE file.
 -->
 <script lang="ts">
-  import { fade } from "svelte/transition";
   import type { SvelteComponent } from "svelte";
+  import { fade } from "svelte/transition";
 
   import Icon from "./Icon";
   import Overlay from "./Overlay.svelte";
   import Tooltip from "./Tooltip.svelte";
-  import Copyable from "./Copyable.svelte";
+
+  export let menuItems: MenuItem[];
+  export let dataCy: string | undefined = undefined;
+  export let style: string | undefined = undefined;
 
   interface MenuItem {
     title: string;
@@ -22,37 +25,18 @@
     dataCy?: string;
     disabled?: boolean;
   }
-  export let menuItems: MenuItem[];
-  export let headerTitle: string | undefined = undefined;
 
-  export let dataCy: string | undefined = undefined;
-  export let style: string | undefined = undefined;
-
-  let triggerEl: HTMLButtonElement;
   let expanded = false;
-
-  const toggleModal = () => {
-    expanded = !expanded;
-  };
-
-  const hideModal = () => {
-    expanded = false;
-  };
-
-  const handleItemSelection = (item: MenuItem) => {
-    hideModal();
-    item.event();
-  };
 </script>
 
 <style>
   .container {
     position: relative;
-    height: 40px;
-    width: 40px;
+    height: 2.5rem;
+    width: 2.5rem;
   }
 
-  .additional-actions-dropdown-button {
+  button {
     height: 100%;
     width: 100%;
     display: flex;
@@ -64,110 +48,89 @@
     border: 1px solid var(--color-foreground-level-3);
   }
 
-  .additional-actions-dropdown-button :global(svg) {
+  button :global(svg) {
     fill: var(--color-foreground-level-6);
   }
 
-  .additional-actions-dropdown-button:active :global(svg) {
-    fill: var(--color-foreground-level-5);
-  }
-
-  .additional-actions-dropdown-button:hover {
+  button:hover {
     background-color: var(--color-foreground-level-2);
   }
 
   .modal {
-    position: absolute;
-    top: 100%;
-    right: 0;
-    width: 240px;
-    margin-top: 15px;
     background-color: var(--color-background);
-    box-shadow: var(--elevation-medium);
     border-radius: 0.5rem;
-    cursor: pointer;
     border: 1px solid var(--color-foreground-level-3);
-    overflow: hidden; /* hack to make inner option rounded corners */
-    z-index: 10;
-    user-select: none;
-  }
-
-  .header {
-    padding: 12px 16px;
-    color: var(--color-foreground-level-5);
-    display: flex;
-    justify-content: space-between;
-    border-bottom: solid 1px var(--color-foreground-level-3);
-  }
-
-  .header:hover {
-    color: var(--color-foreground-level-6);
-  }
-
-  .menu {
+    box-shadow: var(--elevation-medium);
     cursor: pointer;
+    margin-top: 1rem;
+    position: absolute;
+    right: 0;
+    top: 100%;
+    user-select: none;
+    width: 15rem;
+    z-index: 10;
+    /* Round corners of child elements. */
+    overflow: hidden;
   }
 
-  .menu-item {
+  .item {
+    cursor: pointer;
     display: flex;
-    padding: 8px 12px;
+    padding: 0.5rem 0.75rem;
     color: var(--color-foreground-level-6);
   }
 
-  .menu-item:hover {
+  .item:hover {
     background-color: var(--color-foreground-level-1);
   }
 
-  .menu-item.disabled {
+  .item.disabled {
     color: var(--color-foreground-level-4);
     cursor: not-allowed;
   }
 
-  .menu-item.disabled :global(svg) {
+  .item.disabled :global(svg) {
     fill: var(--color-foreground-level-4);
   }
 </style>
 
-<Overlay {expanded} on:hide={hideModal}>
+<Overlay
+  {expanded}
+  on:hide={() => {
+    expanded = false;
+  }}>
   <div data-cy={dataCy} class="container" {style}>
     <button
-      class="additional-actions-dropdown-button button-transition"
-      bind:this={triggerEl}
-      on:click|stopPropagation={toggleModal}>
-      <svelte:component this={Icon.Ellipsis} />
+      class="button-transition"
+      on:click|stopPropagation={() => {
+        expanded = !expanded;
+      }}>
+      <Icon.Ellipsis />
     </button>
-    {#if expanded}
-      <div out:fade={{ duration: 100 }} class="modal" hidden={!expanded}>
-        {#if headerTitle}
-          <div class="header">
-            <Copyable name={headerTitle}>
-              {headerTitle}
-            </Copyable>
-          </div>
-        {/if}
-
-        {#if menuItems}
-          <div class="menu" data-cy="dropdown-menu">
-            {#each menuItems as item}
-              {#if item !== undefined}
-                <Tooltip value={item.tooltip} position="left">
-                  <div
-                    data-cy={item.dataCy}
-                    class="menu-item"
-                    class:disabled={item.disabled}
-                    on:click={!item.disabled
-                      ? () => handleItemSelection(item)
-                      : undefined}>
-                    <svelte:component
-                      this={item.icon}
-                      style="margin-right: 12px" />
-                    <p>{item.title}</p>
-                  </div>
-                </Tooltip>
-              {/if}
-            {/each}
-          </div>
-        {/if}
+    {#if expanded && menuItems.length > 0}
+      <div
+        out:fade|local={{ duration: 100 }}
+        class="modal"
+        data-cy="dropdown-menu">
+        {#each menuItems as item}
+          <Tooltip value={item.tooltip} position="left">
+            <div
+              data-cy={item.dataCy}
+              class="item"
+              class:disabled={item.disabled}
+              on:click={!item.disabled
+                ? () => {
+                    expanded = false;
+                    item.event();
+                  }
+                : undefined}>
+              <svelte:component
+                this={item.icon}
+                style="margin-right: 0.75rem;" />
+              <div>{item.title}</div>
+            </div>
+          </Tooltip>
+        {/each}
       </div>
     {/if}
   </div>
