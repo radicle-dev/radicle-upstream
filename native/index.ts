455ab3f9c5283bd6e8a0b30531b04a98d54b6c6a
Thomas Scholtes <geigerzaehler@axiom.fm>
2021-11-01 16:16:42 +0100

    control proxy args through environment variables
    We make it possible to control proxy arguments with environment
    variables. This makes it easier to modify proxy behavior when running
    `yarn start`.
    
    To accomplish this we switch from `argh` to `structopt`.
    
    Signed-off-by: Thomas Scholtes <geigerzaehler@axiom.fm>

--- native/index.ts
+++ native/index.ts
@@ -35,10 +35,6 @@ let proxyArgs: string[] = [];
 if (isDev) {
   proxyPath = path.join(__dirname, "../target/debug/radicle-proxy");
 
-  if (process.env.RADICLE_UPSTREAM_PROXY_ARGS) {
-    proxyArgs =
-      process.env.RADICLE_UPSTREAM_PROXY_ARGS.split(/[, ]/).filter(Boolean);
-  }
   proxyArgs.push(
     "--default-seed",
     "hybz9gfgtd9d4pd14a6r66j5hz6f77fed4jdu7pana4fxaxbt369kg@setzling.radicle.xyz:12345",
