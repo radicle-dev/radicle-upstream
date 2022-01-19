76e1e1e466d295f1be9f59171eee195468dbd374
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-01-19 11:46:29 +0100

    feat: add file diff collapse button
    This is a workaround to make reviewing patches with lock file changes
    easier to navigate. Enabling scrollbars would also help in this
    situation, but requires more effort to get right.
    
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- ui/App/ProjectScreen/Source/SourceBrowser/FileDiff.svelte
+++ ui/App/ProjectScreen/Source/SourceBrowser/FileDiff.svelte
@@ -9,9 +9,13 @@
   import type { ModifiedFile } from "ui/src/source/diff";
   import { lineNumberL, lineNumberR, lineSign } from "ui/src/source/diff";
 
+  import ChevronDownIcon from "design-system/icons/ChevronDown.svelte";
+  import ChevronRightIcon from "design-system/icons/ChevronRight.svelte";
   import FileIcon from "design-system/icons/File.svelte";
 
   export let file: ModifiedFile;
+
+  let collapsed: boolean = false;
 </script>
 
 <style>
@@ -23,32 +27,31 @@
     margin-bottom: 2rem;
   }
   .changeset-file header {
+    cursor: pointer;
     height: 3rem;
     display: flex;
     align-items: center;
     background: none;
-    border-bottom: 1px solid var(--color-foreground-level-3);
     border-radius: 0;
     padding: 0.75rem;
   }
-
+  main {
+    border-top: 1px solid var(--color-foreground-level-3);
+  }
   .changeset-file main {
     overflow-x: auto;
   }
-
   .binary {
     padding: 1rem;
     color: var(--color-foreground-level-4);
     text-align: center;
     background-color: var(--color-foreground-level-1);
   }
-
   table.diff {
     table-layout: fixed;
     border-collapse: collapse;
     margin: 0.5rem 0;
   }
-
   tr.diff-line[data-type="+"] > * {
     background: var(--color-positive-level-1);
   }
@@ -84,7 +87,6 @@
     padding-right: 1rem;
     text-align: center;
   }
-
   td.diff-expand-action {
     text-align: center;
     user-select: none;
@@ -96,49 +98,66 @@
     background: var(--color-background);
     color: var(--color-foreground-level-4);
   }
-
   td.diff-line-number {
     color: var(--color-foreground-level-4);
+  }
+  .collapse-button {
+    display: flex;
+  }
+  .collapse-button:hover :global(svg) {
+    fill: var(--color-foreground-level-6);
   }
 </style>
 
 <article class="changeset-file">
-  <header>
-    <FileIcon style="margin-right: 8px;" />
+  <header
+    on:click={() => {
+      collapsed = !collapsed;
+    }}>
+    <div class="collapse-button">
+      {#if collapsed}
+        <ChevronRightIcon style="margin-right: 0.5rem; user-select: none;" />
+      {:else}
+        <ChevronDownIcon style="margin-right: 0.5rem; user-select: none;" />
+      {/if}
+    </div>
+    <FileIcon style="margin-right: 0.5rem;" />
     <p class="typo-text-bold">{file.path}</p>
   </header>
-  <main>
-    {#if file.diff.type === "plain" && file.diff.hunks.length > 0}
-      <table class="diff">
-        {#each file.diff.hunks as hunk}
-          <tr class="diff-line">
-            <td colspan={2} class="diff-expand-action typo-mono" />
-            <td colspan={2} class="diff-expand-header typo-mono">
-              {hunk.header}
-            </td>
-          </tr>
-          {#each hunk.lines as line}
-            <tr class="diff-line" data-expanded data-type={lineSign(line)}>
-              <td
-                class="diff-line-number typo-mono left"
-                data-type={lineSign(line)}>
-                {lineNumberL(line)}
+  {#if !collapsed}
+    <main>
+      {#if file.diff.type === "plain" && file.diff.hunks.length > 0}
+        <table class="diff">
+          {#each file.diff.hunks as hunk}
+            <tr class="diff-line">
+              <td colspan={2} class="diff-expand-action typo-mono" />
+              <td colspan={2} class="diff-expand-header typo-mono">
+                {hunk.header}
               </td>
-              <td
-                class="diff-line-number typo-mono right"
-                data-type={lineSign(line)}>
-                {lineNumberR(line)}
-              </td>
-              <td class="diff-line-type typo-mono" data-type={line.type}>
-                {lineSign(line)}
-              </td>
-              <td class="diff-line-content typo-mono">{line.line}</td>
             </tr>
+            {#each hunk.lines as line}
+              <tr class="diff-line" data-expanded data-type={lineSign(line)}>
+                <td
+                  class="diff-line-number typo-mono left"
+                  data-type={lineSign(line)}>
+                  {lineNumberL(line)}
+                </td>
+                <td
+                  class="diff-line-number typo-mono right"
+                  data-type={lineSign(line)}>
+                  {lineNumberR(line)}
+                </td>
+                <td class="diff-line-type typo-mono" data-type={line.type}>
+                  {lineSign(line)}
+                </td>
+                <td class="diff-line-content typo-mono">{line.line}</td>
+              </tr>
+            {/each}
           {/each}
-        {/each}
-      </table>
-    {:else}
-      <div class="binary">Binary file</div>
-    {/if}
-  </main>
+        </table>
+      {:else}
+        <div class="binary">Binary file</div>
+      {/if}
+    </main>
+  {/if}
 </article>
