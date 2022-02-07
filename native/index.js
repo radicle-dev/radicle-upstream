5ec5349008c270f7dd83387cf038c802ba3b6595
Thomas Scholtes <geigerzaehler@axiom.fm>
2022-02-07 14:46:12 +0100

    run app in dev mode during development
    When running the app from the command line during development with `yarn
    run electron ./native/index.js` we ensure that the NODE_ENV environment
    variable which configures the app is set to "development".
    
    The NODE_ENV variable was properly set when running the app with `yarn
    start` but not when running the devnet instantces.
    
    We also enable the `transpileOnly` flag for `ts-node`. This makes the
    app start quicker.
    
    Signed-off-by: Thomas Scholtes <geigerzaehler@axiom.fm>

--- native/index.js
+++ native/index.js
@@ -4,6 +4,10 @@
 // with Radicle Linking Exception. For full terms see the included
 // LICENSE file.
 
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = "development";
+}
+
 /* eslint-disable @typescript-eslint/no-var-requires */
-require("ts-node").register();
+require("ts-node").register({ transpileOnly: true });
 require("./index.ts");
