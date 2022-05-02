b12a16807e6378f4fd996a677dde192e0272289a
Thomas Scholtes <geigerzaehler@axiom.fm>
2022-05-02 11:57:17 +0200

    fix web links for browsing projects
    Signed-off-by: Thomas Scholtes <geigerzaehler@axiom.fm>

--- ui/App/ProjectScreen.svelte
+++ ui/App/ProjectScreen.svelte
@@ -70,22 +70,32 @@
   let peerSelectorExpanded: boolean = false;
 
   function menuItems(project: Project): MenuItem[] {
-    return [
-      {
-        title: "View in browser",
-        icon: GlobeIcon,
-        disabled: project.seed === null,
-        tooltip:
-          project.seed === null
-            ? "Couldn't find the project on any of the official seeds yet"
-            : undefined,
-        event: () => {
-          ipc.openUrl(
-            `https://app.radicle.network/seeds/${project.seed}/${project.urn}`
-          );
+    if (project.seed) {
+      const seedUrl = new URL(project.seed);
+      return [
+        {
+          title: "View in browser",
+          icon: GlobeIcon,
+          disabled: false,
+          tooltip: undefined,
+          event: () => {
+            ipc.openUrl(
+              `https://app.radicle.network/seeds/${seedUrl.hostname}/${project.urn}`
+            );
+          },
         },
-      },
-    ];
+      ];
+    } else {
+      return [
+        {
+          title: "View in browser",
+          icon: GlobeIcon,
+          disabled: true,
+          tooltip: "This project isn’t on a seed yet",
+          event: () => {},
+        },
+      ];
+    }
   }
 </script>
 
