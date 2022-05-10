e78fd4cbfc30dd65910eb6cac74bedb5b5d60c23
Rūdolfs Ošiņš <rudolfs@osins.org>
2022-05-10 15:42:45 +0200

    refactor: simplify lock test
    Signed-off-by: Rūdolfs Ošiņš <rudolfs@osins.org>

--- test/e2e/lock.test.ts
+++ test/e2e/lock.test.ts
@@ -9,64 +9,38 @@ import { test, expect } from "test/support/playwright/fixtures";
 import * as PeerRunner from "test/support/peerRunner";
 import * as Support from "test/support";
 
-test("opens on LockScreen when an identity exists", async ({
-  page,
-}, testInfo) => {
+let peer: PeerRunner.UpstreamPeer;
+
+test.beforeEach(async ({ page }, testInfo) => {
   const stateDir = await Support.prepareStateDir(testInfo.file, testInfo.title);
 
-  const peer = await PeerRunner.UpstreamPeer.create({
+  peer = await PeerRunner.UpstreamPeer.create({
     dataPath: stateDir,
     name: "peer",
   });
   await peer.start();
   await page.goto(peer.uiUrl);
+});
 
+test("show LockScreen when an identity exists", async ({ page }) => {
   // Expect to be on the LockScreen.
   await expect(page).toHaveURL(new RegExp("#/lock"));
 });
 
-test("shows an error notification if an incorrect passphrase is entered", async ({
-  page,
-}, testInfo) => {
-  const stateDir = await Support.prepareStateDir(testInfo.file, testInfo.title);
-
-  const peer = await PeerRunner.UpstreamPeer.create({
-    dataPath: stateDir,
-    name: "peer",
-  });
-  await peer.start();
-  await page.goto(peer.uiUrl);
-
-  // Click [placeholder="Enter your passphrase"]
+test("show error notification if passphrase is incorrect", async ({ page }) => {
   await page.locator('[placeholder="Enter your passphrase"]').click();
-  // Fill [placeholder="Enter your passphrase"]
   await page
     .locator('[placeholder="Enter your passphrase"]')
     .fill("wrong-password");
-  // Click button:has-text("Unlock")
   await page.locator('button:has-text("Unlock")').click();
 
   // Expect a notification showing an error message.
   await expect(page.locator("text=That’s the wrong passphrase.")).toBeVisible();
 });
 
-test("opens the ProfileScreen on successful unlock", async ({
-  page,
-}, testInfo) => {
-  const stateDir = await Support.prepareStateDir(testInfo.file, testInfo.title);
-
-  const peer = await PeerRunner.UpstreamPeer.create({
-    dataPath: stateDir,
-    name: "peer",
-  });
-  await peer.start();
-  await page.goto(peer.uiUrl);
-
-  // Click [placeholder="Enter your passphrase"]
+test("open ProfileScreen after successful unlock", async ({ page }) => {
   await page.locator('[placeholder="Enter your passphrase"]').click();
-  // Fill [placeholder="Enter your passphrase"]
   await page.locator('[placeholder="Enter your passphrase"]').fill("asdf");
-  // Press Enter
   await page.locator('[placeholder="Enter your passphrase"]').press("Enter");
 
   // Expect to be on the ProfileScreen.
