298254de1f8532ecf210e49112bdb9b9c5da752a
Julien Donck <hello@juliendonck.com>
2022-05-11 15:44:35 +0200

    add new pause icon (#2832)
    Signed-off-by: Julien Donck <hello@juliendonck.com>

---
+++ design-system/icons/Pause.svelte
@@ -0,0 +1,24 @@
+<!--
+ Copyright © 2021 The Radicle Upstream Contributors
+
+ This file is part of radicle-upstream, distributed under the GPLv3
+ with Radicle Linking Exception. For full terms see the included
+ LICENSE file.
+-->
+<script lang="ts">
+  import IconWrapper from "./IconWrapper.svelte";
+
+  export let dataCy: string | undefined = undefined;
+  export let style: string | undefined = undefined;
+</script>
+
+<IconWrapper on:click {dataCy} {style}>
+  <path
+    d="M10 8C10.5523 8 11 8.44772 11 9V15C11 15.5523 10.5523 16 10 16C9.44772 16 9 15.5523 9 15V9C9 8.44772 9.44772 8 10 8Z" />
+  <path
+    d="M15 9C15 8.44772 14.5523 8 14 8C13.4477 8 13 8.44772 13 9V15C13 15.5523 13.4477 16 14 16C14.5523 16 15 15.5523 15 15V9Z" />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z" />
+</IconWrapper>
